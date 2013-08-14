c0e7d9ca1e045506b9d9a7d4aa9af2f7d3f1f2d2
andrew martin <almartin@gmail.com>
2013-08-14 16:50:27 -0400

    move novoid/dedupe logic to main AR data feed.  cant read it twice!

--- AR (accelerated reader)/v_ar$test_event_detail.sql
+++ AR (accelerated reader)/v_ar$test_event_detail.sql
@@ -1,118 +1,129 @@
 USE KIPP_NJ
 GO
 
-CREATE VIEW AR$test_event_detail AS
+ALTER VIEW AR$test_event_detail AS
 
---Rise
-SELECT arsp.[iUserID]
-      ,rluser.[vchPreviousIDNum]
-      ,arsp.[iQuizNumber]
-      ,arsp.[vchContentTitle]
-      ,arsp.[vchAuthor] 
-      ,arsp.[iWordCount]
-      ,arsp.[chFictionNonFiction] 
-      ,arsp.[vchInterestLevel] 
-      ,arsp.[dBookLevel] 
-      ,arsp.[iQuestionsPresented] 
-      ,arsp.[iQuestionsCorrect] 
-      ,arsp.[dAlternateBookLevel_1] 
-      ,arsp.[iAlternateBookLevel_2] 
-      ,arsp.[dPointsPossible] 
-      ,arsp.[dPointsEarned] 
-      ,arsp.[dPassingPercentage] 
-      ,arsp.[dPercentCorrect] 
-      ,arsp.[tiPassed] 
-      ,arsp.[chTWI] 
-      ,arsp.[tiBookRating] 
-      ,arsp.[dtTaken] 
-FROM [RM9-DSCHEDULER\SQLEXPRESS].[RL_RISE].[dbo].[ar_StudentPractice] arsp
-LEFT OUTER JOIN [RM9-DSCHEDULER\SQLEXPRESS].[RL_RISE].[dbo].[rl_User]  rluser
-  ON arsp.iUserID = rluser.iUserID
-WHERE arsp.[iContentTypeID] = 31
+--CTE to enable manipulation of this guy w/o ugly self join
+WITH ar_detail AS
+    --Rise
+    (SELECT arsp.[iUserID] AS student_number
+          ,rluser.[vchPreviousIDNum]
+          ,arsp.[iQuizNumber]
+          ,arsp.[vchContentTitle]
+          ,arsp.[vchAuthor] 
+          ,arsp.[iWordCount]
+          ,arsp.[chFictionNonFiction] 
+          ,arsp.[vchInterestLevel] 
+          ,arsp.[dBookLevel] 
+          ,arsp.[iQuestionsPresented] 
+          ,arsp.[iQuestionsCorrect] 
+          ,arsp.[dAlternateBookLevel_1] 
+          ,arsp.[iAlternateBookLevel_2] 
+          ,arsp.[dPointsPossible] 
+          ,arsp.[dPointsEarned] 
+          ,arsp.[dPassingPercentage] 
+          ,arsp.[dPercentCorrect] 
+          ,arsp.[tiPassed] 
+          ,arsp.[chTWI] 
+          ,arsp.[tiBookRating] 
+          ,arsp.[dtTaken] 
+    FROM [RM9-DSCHEDULER\SQLEXPRESS].[RL_RISE].[dbo].[ar_StudentPractice] arsp
+    LEFT OUTER JOIN [RM9-DSCHEDULER\SQLEXPRESS].[RL_RISE].[dbo].[rl_User]  rluser
+      ON arsp.iUserID = rluser.iUserID
+    WHERE arsp.[iContentTypeID] = 31
 
-UNION ALL
+    UNION ALL
 
---SPARK
-SELECT arsp.[iUserID]
-      ,rluser.[vchPreviousIDNum]
-      ,arsp.[iQuizNumber]
-      ,arsp.[vchContentTitle]
-      ,arsp.[vchAuthor] 
-      ,arsp.[iWordCount]
-      ,arsp.[chFictionNonFiction] 
-      ,arsp.[vchInterestLevel] 
-      ,arsp.[dBookLevel] 
-      ,arsp.[iQuestionsPresented] 
-      ,arsp.[iQuestionsCorrect] 
-      ,arsp.[dAlternateBookLevel_1] 
-      ,arsp.[iAlternateBookLevel_2] 
-      ,arsp.[dPointsPossible] 
-      ,arsp.[dPointsEarned] 
-      ,arsp.[dPassingPercentage] 
-      ,arsp.[dPercentCorrect] 
-      ,arsp.[tiPassed] 
-      ,arsp.[chTWI] 
-      ,arsp.[tiBookRating] 
-      ,arsp.[dtTaken] 
-FROM [RM9-DSCHEDULER\SQLEXPRESS].[RL_SPARK].[dbo].[ar_StudentPractice] arsp
-LEFT OUTER JOIN [RM9-DSCHEDULER\SQLEXPRESS].[RL_SPARK].[dbo].[rl_User]  rluser
-  ON arsp.iUserID = rluser.iUserID
-WHERE arsp.[iContentTypeID] = 31
+    --SPARK
+    SELECT arsp.[iUserID]
+          ,rluser.[vchPreviousIDNum]
+          ,arsp.[iQuizNumber]
+          ,arsp.[vchContentTitle]
+          ,arsp.[vchAuthor] 
+          ,arsp.[iWordCount]
+          ,arsp.[chFictionNonFiction] 
+          ,arsp.[vchInterestLevel] 
+          ,arsp.[dBookLevel] 
+          ,arsp.[iQuestionsPresented] 
+          ,arsp.[iQuestionsCorrect] 
+          ,arsp.[dAlternateBookLevel_1] 
+          ,arsp.[iAlternateBookLevel_2] 
+          ,arsp.[dPointsPossible] 
+          ,arsp.[dPointsEarned] 
+          ,arsp.[dPassingPercentage] 
+          ,arsp.[dPercentCorrect] 
+          ,arsp.[tiPassed] 
+          ,arsp.[chTWI] 
+          ,arsp.[tiBookRating] 
+          ,arsp.[dtTaken] 
+    FROM [RM9-DSCHEDULER\SQLEXPRESS].[RL_SPARK].[dbo].[ar_StudentPractice] arsp
+    LEFT OUTER JOIN [RM9-DSCHEDULER\SQLEXPRESS].[RL_SPARK].[dbo].[rl_User]  rluser
+      ON arsp.iUserID = rluser.iUserID
+    WHERE arsp.[iContentTypeID] = 31
 
-UNION ALL
+    UNION ALL
 
---TEAM
-SELECT arsp.[iUserID]
-      ,rluser.[vchPreviousIDNum]
-      ,arsp.[iQuizNumber]
-      ,arsp.[vchContentTitle]
-      ,arsp.[vchAuthor] 
-      ,arsp.[iWordCount]
-      ,arsp.[chFictionNonFiction] 
-      ,arsp.[vchInterestLevel] 
-      ,arsp.[dBookLevel] 
-      ,arsp.[iQuestionsPresented] 
-      ,arsp.[iQuestionsCorrect] 
-      ,arsp.[dAlternateBookLevel_1] 
-      ,arsp.[iAlternateBookLevel_2] 
-      ,arsp.[dPointsPossible] 
-      ,arsp.[dPointsEarned] 
-      ,arsp.[dPassingPercentage] 
-      ,arsp.[dPercentCorrect] 
-      ,arsp.[tiPassed] 
-      ,arsp.[chTWI] 
-      ,arsp.[tiBookRating] 
-      ,arsp.[dtTaken] 
-FROM [RM9-DSCHEDULER\SQLEXPRESS].[RL_TEAM].[dbo].[ar_StudentPractice] arsp
-LEFT OUTER JOIN [RM9-DSCHEDULER\SQLEXPRESS].[RL_TEAM].[dbo].[rl_User]  rluser
-  ON arsp.iUserID = rluser.iUserID
-WHERE arsp.[iContentTypeID] = 31
+    --TEAM
+    SELECT arsp.[iUserID]
+          ,rluser.[vchPreviousIDNum]
+          ,arsp.[iQuizNumber]
+          ,arsp.[vchContentTitle]
+          ,arsp.[vchAuthor] 
+          ,arsp.[iWordCount]
+          ,arsp.[chFictionNonFiction] 
+          ,arsp.[vchInterestLevel] 
+          ,arsp.[dBookLevel] 
+          ,arsp.[iQuestionsPresented] 
+          ,arsp.[iQuestionsCorrect] 
+          ,arsp.[dAlternateBookLevel_1] 
+          ,arsp.[iAlternateBookLevel_2] 
+          ,arsp.[dPointsPossible] 
+          ,arsp.[dPointsEarned] 
+          ,arsp.[dPassingPercentage] 
+          ,arsp.[dPercentCorrect] 
+          ,arsp.[tiPassed] 
+          ,arsp.[chTWI] 
+          ,arsp.[tiBookRating] 
+          ,arsp.[dtTaken] 
+    FROM [RM9-DSCHEDULER\SQLEXPRESS].[RL_TEAM].[dbo].[ar_StudentPractice] arsp
+    LEFT OUTER JOIN [RM9-DSCHEDULER\SQLEXPRESS].[RL_TEAM].[dbo].[rl_User]  rluser
+      ON arsp.iUserID = rluser.iUserID
+    WHERE arsp.[iContentTypeID] = 31
 
-UNION ALL
+    UNION ALL
 
---NCA
-SELECT arsp.[iUserID]
-      ,rluser.[vchPreviousIDNum]
-      ,arsp.[iQuizNumber]
-      ,arsp.[vchContentTitle]
-      ,arsp.[vchAuthor] 
-      ,arsp.[iWordCount]
-      ,arsp.[chFictionNonFiction] 
-      ,arsp.[vchInterestLevel] 
-      ,arsp.[dBookLevel] 
-      ,arsp.[iQuestionsPresented] 
-      ,arsp.[iQuestionsCorrect] 
-      ,arsp.[dAlternateBookLevel_1] 
-      ,arsp.[iAlternateBookLevel_2] 
-      ,arsp.[dPointsPossible] 
-      ,arsp.[dPointsEarned] 
-      ,arsp.[dPassingPercentage] 
-      ,arsp.[dPercentCorrect] 
-      ,arsp.[tiPassed] 
-      ,arsp.[chTWI] 
-      ,arsp.[tiBookRating] 
-      ,arsp.[dtTaken] 
-FROM [RM9-DSCHEDULER\SQLEXPRESS].[RL_NCA].[dbo].[ar_StudentPractice] arsp
-LEFT OUTER JOIN [RM9-DSCHEDULER\SQLEXPRESS].[RL_NCA].[dbo].[rl_User]  rluser
-  ON arsp.iUserID = rluser.iUserID
-WHERE arsp.[iContentTypeID] = 31
+    --NCA
+    SELECT arsp.[iUserID]
+          ,rluser.[vchPreviousIDNum]
+          ,arsp.[iQuizNumber]
+          ,arsp.[vchContentTitle]
+          ,arsp.[vchAuthor] 
+          ,arsp.[iWordCount]
+          ,arsp.[chFictionNonFiction] 
+          ,arsp.[vchInterestLevel] 
+          ,arsp.[dBookLevel] 
+          ,arsp.[iQuestionsPresented] 
+          ,arsp.[iQuestionsCorrect] 
+          ,arsp.[dAlternateBookLevel_1] 
+          ,arsp.[iAlternateBookLevel_2] 
+          ,arsp.[dPointsPossible] 
+          ,arsp.[dPointsEarned] 
+          ,arsp.[dPassingPercentage] 
+          ,arsp.[dPercentCorrect] 
+          ,arsp.[tiPassed] 
+          ,arsp.[chTWI] 
+          ,arsp.[tiBookRating] 
+          ,arsp.[dtTaken] 
+    FROM [RM9-DSCHEDULER\SQLEXPRESS].[RL_NCA].[dbo].[ar_StudentPractice] arsp
+    LEFT OUTER JOIN [RM9-DSCHEDULER\SQLEXPRESS].[RL_NCA].[dbo].[rl_User]  rluser
+      ON arsp.iUserID = rluser.iUserID
+    WHERE arsp.[iContentTypeID] = 31
+    )
+
+SELECT ar_base.*
+FROM ar_detail ar_base
+--self join, same book, in the future
+JOIN ar_detail ar_future
+  ON ar_base.vchPreviousIDNum = ar_future.vchPreviousIDNum
+ AND ar_base.iQuizNumber = ar_future.iQuizNumber
+ AND (ar_future.dtTaken > ar_base.dtTaken)
