c7269333841cbd3de76a66defe8badbb5f9dfb57
andrew martin <almartin@gmail.com>
2013-08-19 16:43:25 -0400

    simple student response detail long

---
+++ ILLUMINATE/v_illuminate$student_response_detail_long.sql
@@ -0,0 +1,14 @@
+CREATE VIEW ILLUMINATE$student_response_detail_long AS
+SELECT *
+FROM OPENQUERY(ILLUMINATE, '
+  SELECT sa.student_id
+        ,f.field_id
+        ,f.sheet_label
+        ,r.response
+  FROM dna_assessments.students_assessments_responses sar
+  JOIN dna_assessments.students_assessments sa USING (student_assessment_id)
+  JOIN dna_assessments.responses r USING (response_id)
+  JOIN dna_assessments.fields f USING (field_id)
+  --WHERE sa.assessment_id = 530
+  --AND sa.student_id = 20488
+')
