3332047b8dc871367ea48f671df9e9a237609f97
andrew martin <almartin@gmail.com>
2013-08-28 15:14:40 -0400

    add job failure count

--- EMAIL (notifications)/sp_EMAIL$daily_qa_audit.sql
+++ EMAIL (notifications)/sp_EMAIL$daily_qa_audit.sql
@@ -19,24 +19,49 @@ BEGIN
 	 --global, entire email
 	 @email_body										NVARCHAR(MAX)
 
- ,@sql_audit           NVARCHAR(MAX)
- ,@html_roster	        NVARCHAR(MAX)
+ --variables that store various queries as text
+ ,@sql_failed_jobs     NVARCHAR(MAX)
+ ,@sql_views           NVARCHAR(MAX)
+ 
+ --variables that catch the output of those queries in HTML format
+ ,@html_failed_jobs	   NVARCHAR(MAX)
+ ,@html_views	         NVARCHAR(MAX)
+ 
+ --needed for querying view refresh stats
  ,@return_value        INT
 	
+
+  --use the return value to get the results from the QA table
+  SET @sql_failed_jobs = '
+    SELECT job_name
+          ,COUNT(*) AS failure_count
+    FROM
+          (SELECT j.name AS job_name
+                ,msdb.dbo.agent_datetime(run_date, run_time) AS run_date_format
+                ,h.*
+          FROM msdb.dbo.sysjobs j 
+          JOIN msdb.dbo.sysjobhistory h 
+           ON j.job_id = h.job_id 
+          WHERE j.enabled = 1 
+            AND msdb.dbo.agent_datetime(run_date, run_time) >= DATEADD(day, -1, GETDATE())
+            AND h.step_id = 0
+            AND h.run_status = 0 
+          ) sub
+    GROUP BY job_name'
+
   --run the view timing test
   EXEC	@return_value = [dbo].sp_QA$view_query_time
 
-  --use the return value to get the results from the QA table
-  SET @sql_audit = '
+  SET @sql_views = '
     SELECT object_name AS "non-cached views"
           ,CAST(CAST(refresh_time / 1000.0 AS NUMERIC(4,2)) AS NVARCHAR(MAX)) AS "seconds to refresh"
     FROM KIPP_NJ.dbo.QA$response_time_results
     WHERE batch_id =' + CAST(@return_value AS VARCHAR)
 
   --sp_TableToHTML is a procedure that TAKES a SQL statement and returns HTML
-  EXECUTE AlumniMirror.dbo.sp_TableToHTML @sql_audit, @html_roster OUTPUT
+  EXECUTE AlumniMirror.dbo.sp_TableToHTML @sql_views, @html_views OUTPUT
+  EXECUTE AlumniMirror.dbo.sp_TableToHTML @sql_failed_jobs, @html_failed_jobs OUTPUT
   
-
 SET @email_body = 
 '<html>
 <head>
@@ -70,9 +95,17 @@ SET @email_body =
   
 		<br>
 		<br>
-' +
-	@html_roster +
-'</body>
+  <span style="med_text">Failed SQL Server Agent Jobs (past 24 hours)</span>
+'
++ @html_failed_jobs + 
+'
+  <br>
+  <span style="med_text">Refresh time for non-cached views</span>
+' 
+ --non cached view refresh time
+ + @html_views +
+'
+</body>
 </html>
 '
 
@@ -80,7 +113,6 @@ SET @email_body =
 EXEC [msdb].[dbo].sp_send_dbmail @profile_name = 'DataRobot'
 								,@body = @email_body
 								,@body_format ='HTML'
-								--,@recipients = 'efisher@teamschools.org;kgarnes@teamschools.org;amartin@teamschools.org;ldesimon@teamschools.org;nmadigan@teamschools.org;vshastry@teamschools.org'
-								,@recipients = 'amartin@teamschools.org'
-								,@subject = 'Daily Audit'
+								,@recipients = 'amartin@teamschools.org;ldesimon@teamschools.org;nmadigan@teamschools.org;cbini@teamschools.org;smircovich@teamschools.org;plebre@teamschools.org;kswearingen@teamschools.org'
+								,@subject = 'SQL Server: Audit/QA Results'
 END
