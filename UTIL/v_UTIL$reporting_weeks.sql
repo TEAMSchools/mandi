dce302ccc20559728dd3c2597f1e0df17eb570ad
andrew martin <almartin@gmail.com>
2013-08-29 13:38:12 -0400

    easy utility view with reporting weeks

---
+++ UTIL/v_UTIL$reporting_weeks.sql
@@ -0,0 +1,25 @@
+USE KIPP_NJ
+GO
+
+CREATE VIEW UTIL$reporting_weeks AS
+WITH weeks AS 
+    (SELECT RIGHT('00' + CAST(n AS VARCHAR(2)), 2) AS week
+     FROM UTIL$row_generator
+     WHERE n > 0 
+       AND n <= 52)
+    
+    ,years AS
+    (SELECT 2002 + n AS year
+     FROM UTIL$row_generator
+     WHERE n < 40
+    )
+
+SELECT TOP 100000000
+       years.year
+      ,weeks.week
+      ,CAST(CAST(years.year AS NVARCHAR) + CAST(weeks.week AS NVARCHAR) AS INT) AS reporting_hash
+FROM weeks
+JOIN years 
+  ON 1=1
+ORDER BY year
+        ,week
