fa1834d90721c9cb7c5584ab29f22a7be7a00aac
andrew martin <almartin@gmail.com>
2013-08-29 12:56:37 -0400

    row generator view

---
+++ UTIL/v_UTIL$row_generator.sql
@@ -0,0 +1,15 @@
+USE KIPP_NJ
+GO
+
+CREATE VIEW UTIL$row_generator AS
+WITH Nbrs_3(n) AS (SELECT 1 UNION SELECT 0)
+    ,Nbrs_2(n) AS (SELECT 1 FROM Nbrs_3 n1 CROSS JOIN Nbrs_3 n2)
+    ,Nbrs_1(n) AS (SELECT 1 FROM Nbrs_2 n1 CROSS JOIN Nbrs_2 n2)
+    ,Nbrs_0(n) AS (SELECT 1 FROM Nbrs_1 n1 CROSS JOIN Nbrs_1 n2)
+    ,Nbrs (n)  AS (SELECT 1 FROM Nbrs_0 n1 CROSS JOIN Nbrs_0 n2)
+    
+SELECT n
+FROM 
+      (SELECT ROW_NUMBER() OVER (ORDER BY n)
+       FROM Nbrs) D (n)
+
