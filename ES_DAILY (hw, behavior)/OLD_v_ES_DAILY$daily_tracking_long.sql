81b87283896dc64b37cfa4541287a39a08efd34f
cbini <cbini87@gmail.com>
2013-09-10 11:00:18 -0400

    keeping this around in case of emergency and STUDENTS hangs upon server

---
+++ ES_DAILY (hw, behavior)/OLD_v_ES_DAILY$daily_tracking_long.sql
@@ -0,0 +1,98 @@
+USE KIPP_NJ
+GO
+
+ALTER VIEW ES_DAILY$daily_tracking_long AS
+
+SELECT schoolid
+	  ,att_date
+	  ,studentid
+	  ,student_number
+	  ,lastfirst
+	  ,grade_level
+	  ,team
+	  ,hw
+	  ,color_day
+	  ,thrive_am
+	  ,thrive_mid
+	  ,thrive_pm
+	  ,CAST(student_number AS VARCHAR(20)) + '_' + att_date AS hash
+FROM
+       (SELECT schoolid
+			  ,CAST(REPLACE(CONVERT(VARCHAR(11),ATT_DATE,6),' ','-') AS VARCHAR(50)) AS att_date
+			  ,studentid
+			  ,student_number
+			  ,lastfirst
+			  ,grade_level
+			  ,team
+			  ,hw
+			  ,color_day
+			  ,thrive_am
+			  ,thrive_mid
+			  ,thrive_pm
+		FROM OPENQUERY(PS_TEAM, '
+				SELECT schoolid
+					  ,att_date
+					  ,studentid
+					  ,student_number
+					  ,lastfirst
+					  ,grade_level
+					  ,team
+					  ,hw
+					  ,color_day
+					  ,thrive_am
+					  ,thrive_mid
+					  ,thrive_pm
+				FROM
+					  (SELECT schoolid
+							 ,att_date
+							 ,studentid
+							 ,student_number
+							 ,lastfirst
+							 ,grade_level
+							 ,team
+							 ,hw
+							 ,CASE
+							   WHEN schoolid = 73255 THEN NULL
+							   ELSE color
+							 END AS color_day
+							 ,CASE
+							   WHEN schoolid = 73255 THEN color
+							 END AS thrive_am
+							 ,CASE
+							   WHEN schoolid = 73255 THEN color_mid
+							 END AS thrive_mid
+							 ,CASE
+							   WHEN schoolid = 73255 THEN color_pm
+							 END AS thrive_pm     
+					  FROM
+							(SELECT scores.schoolid
+								   ,scores.user_defined_date AS att_date
+								   ,scores.foreignkey AS studentid
+								   ,s.student_number
+								   ,s.lastfirst
+								   ,s.grade_level
+								   ,s.team
+								   --,scores.created_by
+								   --,scores.created_on       
+								   --,scores.related_to_table
+								   ,PS_CUSTOMFIELDS.GETCF(''dailytracking'',scores.unique_id,''field1'') hw       
+								   ,PS_CUSTOMFIELDS.GETCF(''dailytracking'',scores.unique_id,''field2'') color
+								   ,PS_CUSTOMFIELDS.GETCF(''dailytracking'',scores.unique_id,''field3'') color_mid
+								   ,PS_CUSTOMFIELDS.GETCF(''dailytracking'',scores.unique_id,''field4'') color_pm
+								   /*
+								   ,PS_CUSTOMFIELDS.GETCF(''dailytracking'',scores.unique_id,''field5'') field5
+								   ,PS_CUSTOMFIELDS.GETCF(''dailytracking'',scores.unique_id,''field6'') field6
+								   ,PS_CUSTOMFIELDS.GETCF(''dailytracking'',scores.unique_id,''field6'') field7
+								   ,PS_CUSTOMFIELDS.GETCF(''dailytracking'',scores.unique_id,''field6'') field8
+								   ,PS_CUSTOMFIELDS.GETCF(''dailytracking'',scores.unique_id,''field6'') field9
+								   ,PS_CUSTOMFIELDS.GETCF(''dailytracking'',scores.unique_id,''field6'') field10
+								   */
+							FROM virtualtablesdata2 scores
+							JOIN students s ON s.id = scores.foreignKey
+							WHERE scores.related_to_table = ''dailytracking''					  					  
+							ORDER BY scores.schoolid, scores.user_defined_date, scores.foreignKey
+							)
+					  )
+				GROUP BY schoolid, att_date, studentid, student_number, lastfirst, grade_level, team, hw, color_day, thrive_am, thrive_mid, thrive_pm
+				')
+		) sub
