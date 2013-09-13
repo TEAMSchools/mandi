41caa46ef5519702acbaac43ec9c3a1bb31080d1
cbini <cbini87@gmail.com>
2013-09-13 18:30:58 -0400

    cosmetic

--- REPORTING (report cards, reading logs)/v_REPORTING$promo_status#Rise.sql
+++ REPORTING (report cards, reading logs)/v_REPORTING$promo_status#Rise.sql
@@ -1,8 +1,8 @@
 USE KIPP_NJ
 GO
 
---ALTER VIEW REPORTING$promo_status#Rise AS
-select studentid
+ALTER VIEW REPORTING$promo_status#Rise AS
+SELECT studentid
       ,student_number
       ,attendance_points
       ,y1_att_pts_pct
@@ -10,85 +10,86 @@ select studentid
       ,GPA_Promo_Status_Grades
       ,promo_status_att
       ,promo_status_hw
-      ,case 
-         when GPA_Promo_Status_Grades  + promo_status_att + promo_status_hw like 'HonorsHonorsHonors' then 'Honors'
-         when GPA_Promo_Status_Grades  + promo_status_att + promo_status_hw like 'High HonorsHonorsHonors' then 'Honors'
-         when GPA_Promo_Status_Grades  + promo_status_att + promo_status_hw like '%Off Track%' then 'Off Track'
-         when GPA_Promo_Status_Grades  + promo_status_att + promo_status_hw like '%Warning%' then 'Warning'
-         else 'Satisfactory' 
-      end as promo_status_overall
-from
-      (select studentid
+      ,CASE 
+        WHEN GPA_Promo_Status_Grades  + promo_status_att + promo_status_hw LIKE 'HonorsHonorsHonors' THEN 'Honors'
+        WHEN GPA_Promo_Status_Grades  + promo_status_att + promo_status_hw LIKE 'High HonorsHonorsHonors' THEN 'Honors'
+        WHEN GPA_Promo_Status_Grades  + promo_status_att + promo_status_hw LIKE '%Off Track%' THEN 'Off Track'
+        WHEN GPA_Promo_Status_Grades  + promo_status_att + promo_status_hw LIKE '%Warning%' THEN 'Warning'
+        ELSE 'Satisfactory' 
+       END AS promo_status_overall
+FROM 
+     (SELECT studentid
             ,student_number
             ,attendance_points
             ,y1_att_pts_pct
             ,att_string
             ,promo_status_grades
-            ,case 
-               when y1_att_pts_pct < 90 then 'Off Track'
-               when y1_att_pts_pct < 92 then 'Warning'
-               when y1_att_pts_pct >= 98 then 'Honors'
-               else 'Satisfactory' 
-             end as promo_status_att
-            ,case 
-               when simple_avg < 65 then 'Off Track'
-               when simple_avg < 70 then 'Warning'
-               when simple_avg >= 90 then 'Honors'
-               else 'Satisfactory' 
-             end as promo_status_hw
-            ,case 
-               when GPA >= 3   and GPA <3.5 and promo_status_grades not like '%Warning%' then 'Honors' 
-               when GPA >= 3.5 and promo_status_grades not like '%Warning%' then 'High Honors' 
-               else promo_status_grades 
-             end as GPA_Promo_Status_Grades
-      from
-           (select s.id as studentid
-                  ,s.student_number
-                  ,case 
-                     when (gr_wide.rc1_y1 < 65 or
-                           gr_wide.rc2_y1 < 65 or
-                           gr_wide.rc3_y1 < 65 or
-                           gr_wide.rc4_y1 < 65 or
-                           gr_wide.rc5_y1 < 65 or
-                           gr_wide.rc6_y1 < 65 or
-                           gr_wide.rc7_y1 < 65 or
-                           gr_wide.rc8_y1 < 65) 
-                     then 'Off Track'
-                     when (gr_wide.rc1_y1 < 70 or
-                           gr_wide.rc2_y1 < 70 or
-                           gr_wide.rc3_y1 < 70 or
-                           gr_wide.rc4_y1 < 70 or
-                           gr_wide.rc5_y1 < 70 or
-                           gr_wide.rc6_y1 < 70 or
-                           gr_wide.rc7_y1 < 70 or
-                           gr_wide.rc8_y1 < 70) 
-                     then 'Warning'
---                        when (nvl(gr_wide.rc1_y1,100) >= 85 and
---                              nvl(gr_wide.rc2_y1,100) >= 85 and
---                              nvl(gr_wide.rc3_y1,100) >= 85 and
---                              nvl(gr_wide.rc4_y1,100) >= 85 and 
---                              nvl(gr_wide.rc5_y1,100) >= 85 and
---                              nvl(gr_wide.rc6_y1,100) >= 85 and
---                              nvl(gr_wide.rc7_y1,100) >= 85 and
---                              nvl(gr_wide.rc8_y1,100) >= 85) then 'Honors'      
-                     else 'Satisfactory' 
-                   end as promo_status_grades
-                  ,Rise_GPA.GPA_Y1 as GPA
-                  ,CAST(mem.mem AS VARCHAR) + '-(' + CAST(ac.absences_total AS VARCHAR) + '+(' + CAST(ac.tardies_total AS VARCHAR) + '/3))' + '/' + CAST(mem.mem AS VARCHAR) as att_string
-                  ,ac.absences_total + floor(ac.tardies_total/3) as attendance_points
-                  ,round(
-                            ((mem.mem - (ac.absences_total + floor(ac.tardies_total/3))) / mem.mem) * 100
-                        ,2) as y1_att_pts_pct
-                  ,rise_hw.simple_avg
-            from STUDENTS s
-            left outer join GRADES$wide_all#MS gr_wide on s.id = gr_wide.studentid
-            left outer join GPA$detail#Rise Rise_GPA on s.id = Rise_GPA.studentid
-            left outer join ATT_MEM$attendance_counts ac on s.id = ac.id
-            left outer join ATT_MEM$membership_counts mem on s.id = mem.id
-            left outer join GRADES$elements rise_hw
-                         ON s.id = rise_hw.studentid
-                        AND rise_hw.pgf_type = 'H'
-                        AND rise_hw.course_number = 'all_courses'
-            where s.schoolid = 73252 and s.enroll_status = 0              
-            )sub
- )sub1
+            ,CASE 
+               WHEN y1_att_pts_pct < 90 THEN 'Off Track'
+               WHEN y1_att_pts_pct < 92 THEN 'Warning'
+               WHEN y1_att_pts_pct >= 98 THEN 'Honors'
+               ELSE 'Satisfactory' 
+             END AS promo_status_att
+            ,CASE 
+               WHEN simple_avg < 65 THEN 'Off Track'
+               WHEN simple_avg < 70 THEN 'Warning'
+               WHEN simple_avg >= 90 THEN 'Honors'
+               ELSE 'Satisfactory' 
+             END AS promo_status_hw
+            ,CASE 
+               WHEN GPA >= 3   AND GPA <3.5 AND promo_status_grades NOT LIKE '%Warning%' THEN 'Honors' 
+               WHEN GPA >= 3.5 AND promo_status_grades NOT LIKE '%Warning%' THEN 'High Honors' 
+               ELSE promo_status_grades 
+             END AS GPA_Promo_Status_Grades
+     FROM
+          (SELECT s.id AS studentid
+                 ,s.student_number
+                 ,CASE 
+                   WHEN (gr_wide.rc1_y1 < 65 OR
+                         gr_wide.rc2_y1 < 65 OR
+                         gr_wide.rc3_y1 < 65 OR
+                         gr_wide.rc4_y1 < 65 OR
+                         gr_wide.rc5_y1 < 65 OR
+                         gr_wide.rc6_y1 < 65 OR
+                         gr_wide.rc7_y1 < 65 OR
+                         gr_wide.rc8_y1 < 65) 
+                         THEN 'Off Track'
+                   WHEN (gr_wide.rc1_y1 < 70 OR
+                         gr_wide.rc2_y1 < 70 OR
+                         gr_wide.rc3_y1 < 70 OR
+                         gr_wide.rc4_y1 < 70 OR
+                         gr_wide.rc5_y1 < 70 OR
+                         gr_wide.rc6_y1 < 70 OR
+                         gr_wide.rc7_y1 < 70 OR
+                         gr_wide.rc8_y1 < 70) 
+                         THEN 'Warning'
+/*
+                   WHEN (NVL(gr_wide.rc1_y1,100) >= 85 AND
+                         NVL(gr_wide.rc2_y1,100) >= 85 AND
+                         NVL(gr_wide.rc3_y1,100) >= 85 AND
+                         NVL(gr_wide.rc4_y1,100) >= 85 AND 
+                         NVL(gr_wide.rc5_y1,100) >= 85 AND
+                         NVL(gr_wide.rc6_y1,100) >= 85 AND
+                         NVL(gr_wide.rc7_y1,100) >= 85 AND
+                         NVL(gr_wide.rc8_y1,100) >= 85)
+                         THEN 'Honors'
+--*/
+                   ELSE 'Satisfactory' 
+                  END AS promo_status_grades
+                 ,Rise_GPA.GPA_Y1 AS GPA
+                 ,CAST(mem.mem AS VARCHAR) + '-(' + CAST(ac.absences_total AS VARCHAR) + '+(' + CAST(ac.tardies_total AS VARCHAR) + '/3))' + '/' + CAST(mem.mem AS VARCHAR) AS att_string
+                 ,ac.absences_total + FLOOR(ac.tardies_total/3) AS attendance_points
+                 ,ROUND(((mem.mem - (ac.absences_total + FLOOR(ac.tardies_total/3))) / mem.mem) * 100,2) AS y1_att_pts_pct
+                 ,rise_hw.simple_avg
+           FROM STUDENTS s
+           LEFT OUTER JOIN GRADES$wide_all#MS gr_wide ON s.id = gr_wide.studentid
+           LEFT OUTER JOIN GPA$detail#Rise Rise_GPA ON s.id = Rise_GPA.studentid
+           LEFT OUTER JOIN ATT_MEM$attendance_counts ac ON s.id = ac.id
+           LEFT OUTER JOIN ATT_MEM$membership_counts mem ON s.id = mem.id
+           LEFT OUTER JOIN GRADES$elements rise_hw
+                        ON s.id = rise_hw.studentid
+                       AND rise_hw.pgf_type = 'H'
+                       AND rise_hw.course_number = 'all_courses'
+           WHERE s.schoolid = 73252 AND s.enroll_status = 0              
+           )sub
+     )sub1
--- REPORTING (report cards, reading logs)/v_REPORTING$promo_status#team.sql
+++ REPORTING (report cards, reading logs)/v_REPORTING$promo_status#team.sql
@@ -1,67 +1,61 @@
 USE KIPP_NJ
 GO
 
---ALTER VIEW REPORTING$promo_status#TEAM AS
-SELECT 
-         studentid
-        ,student_number
-        ,promo_status_grades
-        ,attendance_points
-        ,y1_att_pts_pct
-        ,att_string
-        ,promo_status_att         
-        ,case 
-                       when promo_status_grades  + promo_status_att  like 'On TrackOn Track' then 'On Track'
-                       when promo_status_grades  + promo_status_att  like 'On TrackOn Track' then 'On Track'
-                       when promo_status_grades  + promo_status_att  like '%Promotion In Doubt%' then 'Promotion In Doubt'
-                       when promo_status_grades  + promo_status_att  like '%Off Track%' then 'Promotion In Doubt'
-                       when promo_status_grades  + promo_status_att  like '%Warning%' then 'Promotion In Doubt'
-                       else 'Satisfactory' 
-         end as promo_status_overall
-         
-FROM(
-      SELECT
-                     studentid
-                    ,student_number
-                    ,promo_status_grades
-                    ,attendance_points
-                    ,y1_att_pts_pct
-                    ,att_string
-                    ,case 
-                             when y1_att_pts_pct < 90 then 'Off Track' --redundant
-                             when y1_att_pts_pct < 92 then 'Off Track'
-                             when y1_att_pts_pct >= 98 then 'On Track' --redundant
-                             else 'On Track' 
-                   end as promo_status_att   
-      FROM             
-      (select s.id as studentid
-            ,s.student_number
-      
-            ,case when ((gr_wide.rc1_y1 < 70 and gr_wide.rc1_credittype != 'COCUR') OR
-                        (gr_wide.rc2_y1 < 70 and gr_wide.rc2_credittype != 'COCUR') OR 
-                        (gr_wide.rc3_y1 < 70 and gr_wide.rc3_credittype != 'COCUR') OR
-                        (gr_wide.rc4_y1 < 70 and gr_wide.rc4_credittype != 'COCUR') OR
-                        (gr_wide.rc5_y1 < 70 and gr_wide.rc5_credittype != 'COCUR') OR
-                        (gr_wide.rc6_y1 < 70 and gr_wide.rc6_credittype != 'COCUR') OR
-                        (gr_wide.rc7_y1 < 70 and gr_wide.rc7_credittype != 'COCUR') OR
-                        (gr_wide.rc8_y1 < 70 and gr_wide.rc8_credittype != 'COCUR') 
-                        ) then 'Promotion In Doubt'
-                  else 'On Track' end as promo_status_grades 
-      
-            ,CAST(mem.mem AS VARCHAR) + '-(' + CAST(ac.absences_total AS VARCHAR) + '+(' + CAST(ac.tardies_total AS VARCHAR) + '/3))' + '/' + CAST(mem.mem AS VARCHAR) as att_string
-            ,ac.absences_total + floor(ac.tardies_total/3) as attendance_points
-            ,round(
-                      ((mem.mem - (ac.absences_total + floor(ac.tardies_total/3))) / mem.mem) * 100
-                  ,2) as y1_att_pts_pct
-      
-        from STUDENTS s
-        left outer join GRADES$wide_all#MS gr_wide on s.id = gr_wide.studentid
-        left outer join GPA$detail#TEAM team_GPA on s.id = TEAM_GPA.studentid
-        left outer join ATT_MEM$attendance_counts ac on s.id = ac.id
-        left outer join ATT_MEM$membership_counts mem on s.id = mem.id
-        --only used for Rise
-        --left outer join GRADES$elements team_hw on s.id = team_hw.studentid 
-        --            AND team_hw.pgf_type = 'H'
-        where s.schoolid = 133570965 and s.enroll_status = 0
-        )sub
- )sub1
+ALTER VIEW REPORTING$promo_status#TEAM AS
+SELECT studentid
+      ,student_number
+      ,promo_status_grades
+      ,attendance_points
+      ,y1_att_pts_pct
+      ,att_string
+      ,promo_status_att         
+      ,CASE 
+        WHEN promo_status_grades  + promo_status_att  LIKE 'On TrackOn Track' THEN 'On Track'
+        WHEN promo_status_grades  + promo_status_att  LIKE 'On TrackOn Track' THEN 'On Track'
+        WHEN promo_status_grades  + promo_status_att  LIKE '%Promotion In Doubt%' THEN 'Promotion In Doubt'
+        WHEN promo_status_grades  + promo_status_att  LIKE '%Off Track%' THEN 'Promotion In Doubt'
+        WHEN promo_status_grades  + promo_status_att  LIKE '%Warning%' THEN 'Promotion In Doubt'
+        ELSE 'Satisfactory' 
+       END AS promo_status_overall         
+FROM 
+     (SELECT studentid
+            ,student_number
+            ,promo_status_grades
+            ,attendance_points
+            ,y1_att_pts_pct
+            ,att_string
+            ,CASE 
+              WHEN y1_att_pts_pct < 90 THEN 'Off Track' --redundant
+              WHEN y1_att_pts_pct < 92 THEN 'Off Track'
+              WHEN y1_att_pts_pct >= 98 THEN 'On Track' --redundant
+              ELSE 'On Track' 
+             END AS promo_status_att   
+     FROM             
+           (SELECT s.id AS studentid
+                  ,s.student_number           
+                  ,CASE
+                    WHEN ((gr_wide.rc1_y1 < 70 AND gr_wide.rc1_credittype != 'COCUR') OR
+                          (gr_wide.rc2_y1 < 70 AND gr_wide.rc2_credittype != 'COCUR') OR 
+                          (gr_wide.rc3_y1 < 70 AND gr_wide.rc3_credittype != 'COCUR') OR
+                          (gr_wide.rc4_y1 < 70 AND gr_wide.rc4_credittype != 'COCUR') OR
+                          (gr_wide.rc5_y1 < 70 AND gr_wide.rc5_credittype != 'COCUR') OR
+                          (gr_wide.rc6_y1 < 70 AND gr_wide.rc6_credittype != 'COCUR') OR
+                          (gr_wide.rc7_y1 < 70 AND gr_wide.rc7_credittype != 'COCUR') OR
+                          (gr_wide.rc8_y1 < 70 AND gr_wide.rc8_credittype != 'COCUR'))
+                          THEN 'Promotion In Doubt'
+                    ELSE 'On Track'
+                   END AS promo_status_grades           
+                  ,CAST(mem.mem AS VARCHAR) + '-(' + CAST(ac.absences_total AS VARCHAR) + '+(' + CAST(ac.tardies_total AS VARCHAR) + '/3))' + '/' + CAST(mem.mem AS VARCHAR) AS att_string
+                  ,ac.absences_total + FLOOR(ac.tardies_total/3) AS attendance_points
+                  ,ROUND(((mem.mem - (ac.absences_total + FLOOR(ac.tardies_total/3))) / mem.mem) * 100,2) AS y1_att_pts_pct
+            FROM STUDENTS s
+            LEFT OUTER JOIN GRADES$wide_all#MS gr_wide ON s.id = gr_wide.studentid
+            LEFT OUTER JOIN GPA$detail#TEAM team_GPA ON s.id = TEAM_GPA.studentid
+            LEFT OUTER JOIN ATT_MEM$attendance_counts ac ON s.id = ac.id
+            LEFT OUTER JOIN ATT_MEM$membership_counts mem ON s.id = mem.id
+            --only used for Rise
+            --LEFT OUTER JOIN GRADES$elements team_hw ON s.id = team_hw.studentid 
+            --            AND team_hw.pgf_type = 'H'
+            WHERE s.schoolid = 133570965 AND s.enroll_status = 0
+            )sub
+     )sub1
