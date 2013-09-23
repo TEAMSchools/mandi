12aaec99bedc4924bbd8759e7d12b21bceababde
andrew martin <almartin@gmail.com>
2013-09-23 10:22:24 -0400

    restored previous email notification & wrote demographic test

--- EMAIL (notifications)/sp_EMAIL$daily_qa_audit.sql
+++ EMAIL (notifications)/sp_EMAIL$daily_qa_audit.sql
@@ -20,17 +20,23 @@ BEGIN
 	 @email_body										NVARCHAR(MAX)
 
  --variables that store various queries as text
+ ,@sql_audits          NVARCHAR(MAX)
  ,@sql_failed_jobs     NVARCHAR(MAX)
  ,@sql_views           NVARCHAR(MAX)
  
  --variables that catch the output of those queries in HTML format
+ ,@html_audits         NVARCHAR(MAX)
  ,@html_failed_jobs	   NVARCHAR(MAX)
  ,@html_views	         NVARCHAR(MAX)
  
  --needed for querying view refresh stats
  ,@return_value        INT
 	
-
+  SET @sql_audits = '
+    SELECT audit_type
+          ,result
+    FROM KIPP_NJ..QA$data_audit'
+                
   --use the return value to get the results from the QA table
   SET @sql_failed_jobs = '
     SELECT job_name
@@ -59,6 +65,7 @@ BEGIN
     WHERE batch_id =' + CAST(@return_value AS VARCHAR)
 
   --sp_TableToHTML is a procedure that TAKES a SQL statement and returns HTML
+  EXECUTE AlumniMirror.dbo.sp_TableToHTML @sql_audits, @html_audits OUTPUT  
   EXECUTE AlumniMirror.dbo.sp_TableToHTML @sql_views, @html_views OUTPUT
   EXECUTE AlumniMirror.dbo.sp_TableToHTML @sql_failed_jobs, @html_failed_jobs OUTPUT
   
@@ -95,6 +102,11 @@ SET @email_body =
   
 		<br>
 		<br>
+  <span style="med_text">Daily data audits</span> 
+'
++ @html_audits +
+'
+  <br>
   <span style="med_text">Failed SQL Server Agent Jobs (past 24 hours)</span>
 '
 + @html_failed_jobs + 
@@ -113,6 +125,7 @@ SET @email_body =
 EXEC [msdb].[dbo].sp_send_dbmail @profile_name = 'DataRobot'
 								,@body = @email_body
 								,@body_format ='HTML'
-								,@recipients = 'amartin@teamschools.org;ldesimon@teamschools.org;nmadigan@teamschools.org;cbini@teamschools.org;smircovich@teamschools.org;plebre@teamschools.org;kswearingen@teamschools.org'
+								,@recipients = 'amartin@teamschools.org'
+         --,@recipients = 'amartin@teamschools.org;ldesimon@teamschools.org;nmadigan@teamschools.org;cbini@teamschools.org;smircovich@teamschools.org;plebre@teamschools.org;kswearingen@teamschools.org'
 								,@subject = 'SQL Server: Audit/QA Results'
 END
--- QA (audits, tests)/v_QA$data_audit.sql
+++ QA (audits, tests)/v_QA$data_audit.sql
@@ -1,18 +1,51 @@
 USE KIPP_NJ
 GO
 
---ALTER VIEW QA$data_audit AS
+ALTER VIEW QA$data_audit AS
+
+
+SELECT '>>DEMOGRAPHIC AUDITS<<' AS audit_type
+      ,NULL AS result
+
+UNION ALL 
+
+--Added by AM2 on 9/23/2013
+--Any FARM status not in P, R, F?
+SELECT 'FARM data quality' AS audit_type
+      ,sub_2.assertion + ' | ' + CASE WHEN sub_2.N < 50 THEN sub_2.elements ELSE '50+ students' END AS result
+FROM
+     (SELECT sub_1.assertion
+            ,COUNT(*) AS N
+            ,dbo.GROUP_CONCAT(sub_1.hash) AS elements
+             --there I fixed it
+            ,ROW_NUMBER() OVER(ORDER BY sub_1.assertion ASC) AS rn
+      FROM
+            (SELECT s.first_name + ' ' + s.last_name + ' (' + sch.abbreviation + ')' AS hash
+                   ,CASE 
+                      WHEN s.lunchstatus IN ('F', 'P', 'R') THEN 'Pass'
+                      ELSE 'Fail'
+                    END AS assertion
+             FROM KIPP_NJ..STUDENTS s
+             JOIN KIPP_NJ..SCHOOLS sch
+               ON s.schoolid = sch.school_number
+             WHERE s.enroll_status = 0
+             ) sub_1
+      GROUP BY sub_1.assertion
+      ) sub_2
+WHERE rn = 1
 
 --Added by AM2 on 9/10/2013
 --testing to see if there are any *currently enrolled* students w/o a HR active on today's date.
+UNION ALL
+
 SELECT 'HR audit' AS audit_type
-      ,sub_2.assertion + ' | ' + sub_2.elements AS 'result'
+      ,sub_2.assertion + ' | ' + CASE WHEN sub_2.N < 50 THEN sub_2.elements ELSE '50+ students' END AS result
 FROM
      (SELECT sub.assertion
             ,COUNT(*) AS N
             ,dbo.GROUP_CONCAT(hash) AS elements
              --there I fixed it
-            ,ROW_NUMBER() OVER(ORDER BY sub.assertion ASC) AS Row
+            ,ROW_NUMBER() OVER(ORDER BY sub.assertion ASC) AS rn
       FROM
 	           (SELECT s.id
 		                 ,s.lastfirst
@@ -40,10 +73,7 @@ FROM
 	           ) sub
       GROUP BY sub.assertion
       ) sub_2
-WHERE row = 1
+WHERE rn = 1
 
-UNION ALL
-
-SELECT 'example_audit' AS audit_type
-      ,'Pass' AS result
+--UNION ALL
 
