b0913fc7f4041809771303b08dbe57a455d3dcdc
cbini <cbini87@gmail.com>
2013-09-23 23:04:04 -0400

    now featuring subject and scope from codes table

--- ILLUMINATE/v_illuminate$assessments.sql
+++ ILLUMINATE/v_illuminate$assessments.sql
@@ -4,68 +4,28 @@
 USE KIPP_NJ
 GO
 
---ALTER VIEW ILLUMINATE$assessments AS
+ALTER VIEW ILLUMINATE$assessments AS
 SELECT *
-FROM  
-     (SELECT assessment_id
-            ,title
-            ,description
-            ,user_id
-            ,created_at
-            ,updated_at
-            ,deleted_at
-            ,administered_at
-            ,code_scope_id
-            ,code_subject_area_id
-            ,reports_db_virtual_table_id
-            ,academic_year
-            ,local_assessment_id
-            ,intel_assess_guid
-            ,guid
-            ,tags
-            ,edusoft_guid
-            ,performance_band_set_id
-            ,als_guid
-            ,curriculum_associate_guid
-            ,allow_duplicates
-            ,itembank_assessment_id
-            ,locked
-            ,raw_score_performance_band_set_id
-            ,show_in_parent_portal
-            ,tag
-            ,CASE
-              WHEN tag IN ('k','1','2','3','4','5','6','7','8','9','10','11','12') THEN 'grade'
-              WHEN tag IN ('fsa','site assessment','unit assessments') THEN 'type'
-              WHEN tag IN ('reading','mathematics','english language arts','comprehension','phonics','english language development','grammar','science','historysocial science','arts music','arts dance','spanish','writing','world language') THEN 'subject'
-              WHEN tag IN ('measured progress','special education','kipp networkwide','teacher created') THEN 'other'
-              ELSE 'uncategorized'
-             END AS tag_type
-      FROM
-           (SELECT a.*
-                  ,tags.tag                  
-            FROM OPENQUERY(ILLUMINATE, '
-                   SELECT *
-                   FROM dna_assessments.assessments
-                 ') a
-            LEFT OUTER JOIN (SELECT *
-                             FROM OPENQUERY(ILLUMINATE,'
-                              SELECT assess.assessment_id
-                                    ,assess.title
-                                    ,assess.description
-                                    ,assess.code_scope_id
-                                    ,assess.code_subject_area_id
-                                    ,tags.tag_id
-                                    ,tag_index.tag
-                              FROM dna_assessments.assessments assess
-                              LEFT OUTER JOIN dna_assessments.assessments_tags tags
-                                ON assess.assessment_id = tags.assessment_id
-                              LEFT OUTER JOIN dna_assessments.tags tag_index
-                                ON tag_index.tag_id = tags.tag_id
-                             ')
-                            ) tags
-              ON a.assessment_id = tags.assessment_id
-            ) sub1
-     ) sub2
+      ,CASE
+        WHEN tag IN ('k','1','2','3','4','5','6','7','8','9','10','11','12') THEN 'grade'
+        ELSE 'other'
+       END AS tag_type
+FROM OPENQUERY(ILLUMINATE,'
+     SELECT a.*
+           ,subj.code_translation AS subject
+           ,scope.code_translation AS scope
+           ,tags.tag
+     FROM dna_assessments.assessments a
+     LEFT OUTER JOIN dna_assessments.assessments_tags tag_index
+       ON a.assessment_id = tag_index.assessment_id
+     LEFT OUTER JOIN dna_assessments.tags tags
+       ON tags.tag_id = tag_index.tag_id
+     LEFT OUTER JOIN codes.dna_subject_areas subj
+       ON a.code_subject_area_id = subj.code_id
+     LEFT OUTER JOIN codes.dna_scopes scope
+       ON a.code_scope_id = scope.code_id
+     ')     
+           
 /*
 PIVOT (
  MAX(tag)
@@ -83,7 +43,3 @@ PIVOT (
     ,tag6)
 ) AS piv
 --*/
---WHERE title LIKE 'Math - G2 - FSA%'
---WHERE type = 'fsa'
---WHERE assessment_id = 1251
---ORDER BY GRADE
