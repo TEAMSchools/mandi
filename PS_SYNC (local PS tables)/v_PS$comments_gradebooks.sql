7244a8714834abf6f1061a151d21aae40d675ea4
cbini <cbini87@gmail.com>
2013-09-25 11:33:23 -0400

    no changes, will work direct from PS but needs to be sp for practical use

--- PS_SYNC (local PS tables)/v_PS$comments_gradebooks.sql
+++ PS_SYNC (local PS tables)/v_PS$comments_gradebooks.sql
@@ -4,6 +4,24 @@ GO
 ALTER VIEW PS$comments_gradebooks AS
 SELECT *
 FROM OPENQUERY(KIPP_NWK,'
-     SELECT *
-     FROM local_gradebook_comments
-')
+       SELECT *
+       FROM local_gradebook_comments
+       ')
+
+/*
+SELECT *
+FROM OPENQUERY(PS_TEAM,'
+     SELECT s.id 
+           ,pgf.sectionid
+           ,pgf.studentid
+           ,pgf.finalgradename
+           ,CAST(SUBSTR(pgf.comment_value,1,4000) AS varchar2(4000)) AS teacher_comment
+     FROM STUDENTS s
+     LEFT OUTER JOIN pgfinalgrades pgf
+       ON s.id = pgf.studentid
+     WHERE (pgf.finalgradename LIKE ''T%'' OR pgf.finalgradename LIKE ''Q%'')
+       AND pgf.sectionid >= 4121
+       AND pgf.comment_value IS NOT NULL
+       AND s.enroll_status = 0
+     ')
+*/
