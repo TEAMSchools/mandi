81ee824fb829839be68559bfc9829f5a0299a038
cbini <cbini87@gmail.com>
2013-09-25 11:32:03 -0400

    Direct view into PS

--- PS_SYNC (local PS tables)/v_PS$comments_advisor.sql
+++ PS_SYNC (local PS tables)/v_PS$comments_advisor.sql
@@ -2,8 +2,18 @@ USE KIPP_NJ
 GO
 
 ALTER VIEW PS$comments_advisors AS
-SELECT *
-FROM OPENQUERY(KIPP_NWK,'
-     SELECT *
-     FROM y_rc_advisor_comments
-')
+SELECT s.id
+      ,sect.course_number
+      ,comments.finalgradename
+      ,comments.teacher_comment
+FROM STUDENTS s
+JOIN CC
+  ON cc.studentid = s.id
+ AND cc.termid >= 2300
+JOIN SECTIONS sect
+  ON sect.id = cc.sectionid
+ AND sect.course_number IN ('HR','Adv')
+JOIN PS$comments_gradebooks comments
+  ON comments.sectionid = sect.id
+ AND comments.studentid = s.id
+WHERE s.enroll_status = 0
