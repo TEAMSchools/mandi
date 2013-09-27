6cdd5e5448566f1bac9f5a71d851ee96fa6a9c67
cbini <cbini87@gmail.com>
2013-09-27 00:22:47 -0400

    still in testing

--- ES_DAILY (hw, behavior)/v_ES_DAILY$att_reporting#14.sql
+++ ES_DAILY (hw, behavior)/v_ES_DAILY$att_reporting#14.sql
@@ -1,18 +1,12 @@
 USE KIPP_NJ
 GO
 
-ALTER VIEW ES_DAILY$att_reporting_long#SY14 AS
-
+--ALTER VIEW ES_DAILY$att_reporting_long#SY14 AS
 SELECT *
 FROM OPENQUERY(PS_TEAM,'
-		SELECT id
-			  ,studentid
-			  ,schoolid
-			  ,att_date
-			  ,att_code
-			  ,presence_status_cd
-		FROM ps_attendance_daily
-		WHERE att_date >= ''2013-08-01''
-		  AND att_date <= ''2014-06-30''
-		ORDER BY att_date		
-')
+		     SELECT *
+		     FROM ps_attendance_daily
+		     WHERE att_date >= TO_DATE(''2013-08-01'',''YYYY-MM-DD'')
+		       AND att_date <= TO_DATE(''2014-06-30'',''YYYY-MM-DD'')
+		     ORDER BY att_date		
+       ')
