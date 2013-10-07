cb7450792d664d7586f9f27c70de41a40800f74e
cbini <cbini87@gmail.com>
2013-10-07 11:03:32 -0400

    dead, use PS#static

--- PS_SYNC (local PS tables)/v_PS$comments_advisor.sql
+++ PS_SYNC (local PS tables)/v_PS$comments_advisor.sql
@@ -2,18 +2,19 @@ USE KIPP_NJ
 GO
 
 ALTER VIEW PS$comments_advisors AS
-SELECT s.id
+SELECT 'DEPRECATED -- USE STATIC TABLE -- PS$comments#static' AS [DO NOT ENTER]
+
+/*
+CC.studentid
       ,sect.course_number
       ,comments.finalgradename
-      ,comments.teacher_comment
-FROM STUDENTS s
-JOIN CC
-  ON cc.studentid = s.id
- AND cc.termid >= 2300
+      ,comments.teacher_comment AS advisor_comment
+FROM CC  
 JOIN SECTIONS sect
   ON sect.id = cc.sectionid
  AND sect.course_number IN ('HR','Adv')
 JOIN PS$comments_gradebooks comments
   ON comments.sectionid = sect.id
- AND comments.studentid = s.id
-WHERE s.enroll_status = 0
+ AND comments.studentid = cc.studentid
+WHERE cc.termid >= dbo.fn_Global_Term_Id()
+*/
