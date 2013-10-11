ce849ce37bb747288c4bfd69239800fabcc3ae91
cbini <cbini87@gmail.com>
2013-10-10 20:39:44 -0400

    fixed a divide by zero problem with mem counts

--- ATT_MEM (attendance and membership)/v_ATT_MEM$membership_counts.sql
+++ ATT_MEM (attendance and membership)/v_ATT_MEM$membership_counts.sql
@@ -1,19 +1,19 @@
 USE KIPP_NJ
 GO
 
---ALTER VIEW ATT_MEM$membership_counts AS
+ALTER VIEW ATT_MEM$membership_counts AS
 SELECT s.id
       ,s.lastfirst
       ,s.schoolid
       ,s.grade_level
-      ,SUM(CONVERT(FLOAT,CASE WHEN RT != 'CUR' THEN membershipvalue ELSE 0 END)) AS mem
-      ,SUM(CONVERT(FLOAT,CASE WHEN RT  = 'RT1' THEN membershipvalue ELSE 0 END)) AS RT1_mem
-      ,SUM(CONVERT(FLOAT,CASE WHEN RT  = 'RT2' THEN membershipvalue ELSE 0 END)) AS RT2_mem
-      ,SUM(CONVERT(FLOAT,CASE WHEN RT  = 'RT3' THEN membershipvalue ELSE 0 END)) AS RT3_mem
-      ,SUM(CONVERT(FLOAT,CASE WHEN RT  = 'RT4' THEN membershipvalue ELSE 0 END)) AS RT4_mem
-      ,SUM(CONVERT(FLOAT,CASE WHEN RT  = 'RT5' THEN membershipvalue ELSE 0 END)) AS RT5_mem
-      ,SUM(CONVERT(FLOAT,CASE WHEN RT  = 'RT6' THEN membershipvalue ELSE 0 END)) AS RT6_mem
-      ,SUM(CONVERT(FLOAT,CASE WHEN RT  = 'CUR' THEN membershipvalue ELSE 0 END)) AS CUR_mem
+      ,SUM(CONVERT(FLOAT,CASE WHEN RT != 'CUR' THEN membershipvalue END)) AS mem
+      ,SUM(CONVERT(FLOAT,CASE WHEN RT  = 'RT1' THEN membershipvalue END)) AS RT1_mem
+      ,SUM(CONVERT(FLOAT,CASE WHEN RT  = 'RT2' THEN membershipvalue END)) AS RT2_mem
+      ,SUM(CONVERT(FLOAT,CASE WHEN RT  = 'RT3' THEN membershipvalue END)) AS RT3_mem
+      ,SUM(CONVERT(FLOAT,CASE WHEN RT  = 'RT4' THEN membershipvalue END)) AS RT4_mem
+      ,SUM(CONVERT(FLOAT,CASE WHEN RT  = 'RT5' THEN membershipvalue END)) AS RT5_mem
+      ,SUM(CONVERT(FLOAT,CASE WHEN RT  = 'RT6' THEN membershipvalue END)) AS RT6_mem
+      ,SUM(CONVERT(FLOAT,CASE WHEN RT  = 'CUR' THEN membershipvalue END)) AS CUR_mem
 FROM STUDENTS s
 LEFT OUTER JOIN
      (SELECT studentid
