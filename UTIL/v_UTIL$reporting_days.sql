ae96fb05e4a797fd54272cfc9240610048863f58
cbini <cbini87@gmail.com>
2013-10-15 17:58:45 -0400

    added day and month parts, day of week numeric

--- UTIL/v_UTIL$reporting_days.sql
+++ UTIL/v_UTIL$reporting_days.sql
@@ -1,7 +1,7 @@
 USE KIPP_NJ
 GO
 
-ALTER VIEW UTIL$reporting_days AS
+--ALTER VIEW UTIL$reporting_days AS
 WITH calendar AS
     (SELECT DATEADD(DAY, n, '01-AUG-02') AS date
      FROM UTIL$row_generator
@@ -12,8 +12,11 @@ SELECT TOP 10000000000 sub.*
       ,CAST(sub.year_part AS NVARCHAR) + CAST(sub.week_part AS NVARCHAR) AS reporting_hash
 FROM
    (SELECT date
-         ,DATENAME(dw, calendar.date) AS day_of_week
-         ,DATEPART(yyyy, calendar.date) AS year_part
+         ,DATENAME(DW, calendar.date) AS day_of_week
+         ,DATEPART(DW, calendar.date) AS dw_numeric
+         ,DATEPART(DD, calendar.date) AS day_part
+         ,DATEPART(MM, calendar.date) AS month_part         
+         ,DATEPART(YYYY, calendar.date) AS year_part
          ,RIGHT('00' + CAST(DATEPART(iso_week, calendar.date) AS VARCHAR(2)), 2) AS week_part
    FROM calendar
    ) sub
