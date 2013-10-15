f02739dab381e45452f92d8cbd3c85c4a62ac257
cbini <cbini87@gmail.com>
2013-10-15 18:07:12 -0400

    added IS NOT NULL to grades promo to protect against students not enrolled in core classes like social studies

--- REPORTING (report cards, reading logs)/v_REPORTING$promo_status#team.sql
+++ REPORTING (report cards, reading logs)/v_REPORTING$promo_status#team.sql
@@ -13,7 +13,6 @@ SELECT studentid
       ,promo_status_hw
       ,CASE 
         WHEN promo_status_grades  + promo_status_att  LIKE 'On TrackOn Track' THEN 'On Track'
-        WHEN promo_status_grades  + promo_status_att  LIKE 'On TrackOn Track' THEN 'On Track'
         WHEN promo_status_grades  + promo_status_att  LIKE '%Promotion In Doubt%' THEN 'Promotion In Doubt'
         WHEN promo_status_grades  + promo_status_att  LIKE '%Off Track%' THEN 'Promotion In Doubt'
         WHEN promo_status_grades  + promo_status_att  LIKE '%Warning%' THEN 'Promotion In Doubt'
@@ -46,18 +45,21 @@ FROM
                   ,mem.mem
                   ,team_hw.simple_avg
                   ,CASE
-                    WHEN ((gr_wide.rc1_y1 < 70 AND gr_wide.rc1_credittype != 'COCUR') OR
-                          (gr_wide.rc2_y1 < 70 AND gr_wide.rc2_credittype != 'COCUR') OR 
-                          (gr_wide.rc3_y1 < 70 AND gr_wide.rc3_credittype != 'COCUR') OR
-                          (gr_wide.rc4_y1 < 70 AND gr_wide.rc4_credittype != 'COCUR') OR
-                          (gr_wide.rc5_y1 < 70 AND gr_wide.rc5_credittype != 'COCUR') OR
-                          (gr_wide.rc6_y1 < 70 AND gr_wide.rc6_credittype != 'COCUR') OR
-                          (gr_wide.rc7_y1 < 70 AND gr_wide.rc7_credittype != 'COCUR') OR
-                          (gr_wide.rc8_y1 < 70 AND gr_wide.rc8_credittype != 'COCUR'))
+                    WHEN ((gr_wide.rc1_y1 IS NOT NULL AND gr_wide.rc1_y1 < 70 AND gr_wide.rc1_credittype != 'COCUR') OR
+                          (gr_wide.rc2_y1 IS NOT NULL AND gr_wide.rc2_y1 < 70 AND gr_wide.rc2_credittype != 'COCUR') OR 
+                          (gr_wide.rc3_y1 IS NOT NULL AND gr_wide.rc3_y1 < 70 AND gr_wide.rc3_credittype != 'COCUR') OR
+                          (gr_wide.rc4_y1 IS NOT NULL AND gr_wide.rc4_y1 < 70 AND gr_wide.rc4_credittype != 'COCUR') OR
+                          (gr_wide.rc5_y1 IS NOT NULL AND gr_wide.rc5_y1 < 70 AND gr_wide.rc5_credittype != 'COCUR') OR
+                          (gr_wide.rc6_y1 IS NOT NULL AND gr_wide.rc6_y1 < 70 AND gr_wide.rc6_credittype != 'COCUR') OR
+                          (gr_wide.rc7_y1 IS NOT NULL AND gr_wide.rc7_y1 < 70 AND gr_wide.rc7_credittype != 'COCUR') OR
+                          (gr_wide.rc8_y1 IS NOT NULL AND gr_wide.rc8_y1 < 70 AND gr_wide.rc8_credittype != 'COCUR'))
                           THEN 'Promotion In Doubt'
                     ELSE 'On Track'
                    END AS promo_status_grades           
-                  ,CAST(mem.mem AS VARCHAR) + '-(' + CAST(ac.absences_total AS VARCHAR) + '+(' + CAST(ac.tardies_total AS VARCHAR) + '/3))' + '/' + CAST(mem.mem AS VARCHAR) AS att_string
+                  ,CAST(mem.mem AS VARCHAR) + '-(' 
+                    + CAST(ac.absences_total AS VARCHAR) + '+(' 
+                    + CAST(ac.tardies_total AS VARCHAR) + '/3))' + '/' 
+                    + CAST(mem.mem AS VARCHAR) AS att_string
                   ,ac.absences_total + FLOOR(ac.tardies_total/3) AS attendance_points
                   ,ROUND(((mem.mem - (ac.absences_total + FLOOR(ac.tardies_total/3))) / mem.mem) * 100,2) AS y1_att_pts_pct
             FROM STUDENTS s
