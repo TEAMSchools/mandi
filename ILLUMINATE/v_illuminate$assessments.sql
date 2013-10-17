3490477119345597b3b75270e5ee25f092fc80c3
cbini <cbini87@gmail.com>
2013-10-17 08:36:49 -0400

    changed k to 0 to allow joins on grade level

--- ILLUMINATE/v_illuminate$assessments.sql
+++ ILLUMINATE/v_illuminate$assessments.sql
@@ -4,10 +4,37 @@
 USE KIPP_NJ
 GO
 
---ALTER VIEW ILLUMINATE$assessments AS
+ALTER VIEW ILLUMINATE$assessments AS
 SELECT *
 FROM
-     (SELECT oq.*
+     (SELECT oq.assessment_id
+            ,oq.title
+            ,oq.description
+            ,oq.user_id
+            ,oq.created_at
+            ,oq.updated_at
+            ,oq.deleted_at
+            ,oq.administered_at
+            ,oq.code_scope_id
+            ,oq.code_subject_area_id
+            ,oq.reports_db_virtual_table_id
+            ,oq.academic_year
+            ,oq.local_assessment_id
+            ,oq.intel_assess_guid
+            ,oq.guid
+            ,oq.tags
+            ,oq.edusoft_guid
+            ,oq.performance_band_set_id
+            ,oq.als_guid
+            ,oq.curriculum_associate_guid
+            ,oq.allow_duplicates
+            ,oq.itembank_assessment_id
+            ,oq.locked
+            ,oq.raw_score_performance_band_set_id
+            ,oq.show_in_parent_portal
+            ,oq.subject
+            ,oq.scope
+            ,CASE WHEN tag = 'k' THEN '0' ELSE tag END AS tag
             ,CASE
               WHEN tag IN ('k','1','2','3','4','5','6','7','8','9','10','11','12') 
                THEN 'grade' 
