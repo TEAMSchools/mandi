980b6b1cbc9fa4d2a8a04037ec81952ee6ce4f3a
cbini <cbini87@gmail.com>
2013-12-06 16:45:48 -0500

    filtered more standards, special case for writing SPARK

--- REPORTING (report cards, reading logs)/v_REPORTING$TA_results_by_standard.sql
+++ REPORTING (report cards, reading logs)/v_REPORTING$TA_results_by_standard.sql
@@ -3,14 +3,18 @@ GO
 
 ALTER VIEW REPORTING$TA_results_by_standard AS
 SELECT TOP (100) PERCENT
-       sub.*
-      --,week_num + '_' 
-       -- + 
-        ,CONVERT(VARCHAR,grade_level) + '_' 
-        + CONVERT(VARCHAR,fsa_std_rn) AS meta_hash
+       sub.*       
+      ,CONVERT(VARCHAR,grade_level) + '_' 
+        + CASE
+           WHEN standard = 'CCSS.LA.3.L.3.2' THEN CONVERT(VARCHAR,1)
+           ELSE CONVERT(VARCHAR,fsa_std_rn)
+          END AS meta_hash
       ,CONVERT(VARCHAR,student_number) + '_'  
         + CONVERT(VARCHAR,subject) + '_' 
-        + CONVERT(VARCHAR,fsa_std_rn) AS meta_stu_hash
+        + CASE
+           WHEN standard = 'CCSS.LA.3.L.3.2' THEN CONVERT(VARCHAR,1)
+           ELSE CONVERT(VARCHAR,fsa_std_rn)
+          END AS meta_stu_hash
 FROM
      (SELECT s.schoolid
             ,s.id AS studentid
@@ -60,6 +64,14 @@ FROM
                (PARTITION BY assessments.schoolid, assessments.grade_level, assessments.subject, s.id
                     ORDER BY assessments.standards_tested) AS fsa_std_rn
             /*
+            ,CASE WHEN assessments.subject = 'Writing' THEN
+               ROW_NUMBER() OVER
+                  (PARTITION BY assessments.schoolid, assessments.grade_level, assessments.subject, s.id
+                       ORDER BY assessments.standards_tested)
+              ELSE NULL
+             END AS writing_std_rn
+            */
+            /*
             --now using row number from assessment feed
             ,ROW_NUMBER() OVER(
                 PARTITION BY dates.time_per_name, assessments.grade_level, s.id
@@ -81,6 +93,18 @@ FROM
       WHERE s.schoolid IN (73254,73255,73256)
         AND s.enroll_status = 0        
         AND assessments.scope = 'District Benchmark'
-        AND assessments.standards_tested NOT IN ('CCSS.MA.4.4.NF.3.a','CCSS.MA.4.4.NF.3.b','CCSS.MA.4.4.NF.3.d','CCSS.MA.4.4.NF.4.a','CCSS.MA.4.4.NF.4.b','CCSS.MA.4.4.NF.4.c')
+        AND assessments.standards_tested NOT IN ('CCSS.MA.4.4.NF.3.a'
+                                                ,'CCSS.MA.4.4.NF.3.b'
+                                                ,'CCSS.MA.4.4.NF.3.d'
+                                                ,'CCSS.MA.4.4.NF.4.a'
+                                                ,'CCSS.MA.4.4.NF.4.b'
+                                                ,'CCSS.MA.4.4.NF.4.c'
+                                                ,'CCSS.LA.3.RI'
+                                                ,'CCSS.LA.3.RL'
+                                                ,'CCSS.LA.4.RI'
+                                                ,'CCSS.LA.4.RL'
+                                                ,'CCSS.LA.4.L.4.1'
+                                                ,'CCSS.LA.4.L.4.2'
+                                                ,'CCSS.LA.4.L.4.3')
       ) sub
 --ORDER BY schoolid, grade_level, week_num, team, studentid, subject, standard
