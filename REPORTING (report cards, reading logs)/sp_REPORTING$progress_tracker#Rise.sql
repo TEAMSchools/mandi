3a2e2f463c16446785972a6c007cc1538d2146f4
cbini <cbini87@gmail.com>
2013-12-10 18:31:21 -0500

    scoping these out to see if they were breaking the progress trackers, not guilty

--- REPORTING (report cards, reading logs)/sp_REPORTING$progress_tracker#Rise.sql
+++ REPORTING (report cards, reading logs)/sp_REPORTING$progress_tracker#Rise.sql
@@ -7,21 +7,21 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-
 ALTER PROCEDURE [dbo].[sp_REPORTING$progress_tracker#Rise_static|refresh] AS
+
 BEGIN
 
  DECLARE @sql AS VARCHAR(MAX)='';
 
  --STEP 1: make sure no temp table
-		IF OBJECT_ID(N'tempdb..#REPORTING$progress_tracker#Rise_static|refresh') IS NOT NULL
+		IF OBJECT_ID(N'tempdb..#REPORTING$progress_tracker#Rise_static|refresh1') IS NOT NULL
 		BEGIN
-						DROP TABLE [#REPORTING$progress_tracker#Rise_static|refresh]
+						DROP TABLE [#REPORTING$progress_tracker#Rise_static|refresh1]
 		END
 		
 		--STEP 2: load into a TEMPORARY staging table.  
   SELECT *
-		INTO [#REPORTING$progress_tracker#Rise_static|refresh]
+		INTO [#REPORTING$progress_tracker#Rise_static|refresh1]
 		FROM REPORTING$progress_tracker#Rise_refresh WITH(NOLOCK)
    
   --STEP 3: LOCK destination table exclusively load into a TEMPORARY staging table.
@@ -47,7 +47,7 @@ BEGIN
  -- step 6: insert into final destination
  INSERT INTO [dbo].[REPORTING$progress_tracker#Rise_static]
  SELECT *
- FROM [#REPORTING$progress_tracker#Rise_static|refresh];
+ FROM [#REPORTING$progress_tracker#Rise_static|refresh1];
 
  -- Step 4: rebuld all nonclustered indexes on table
  SELECT @sql = @sql + 
