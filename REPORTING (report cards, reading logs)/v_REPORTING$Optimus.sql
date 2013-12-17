67b554a259d92b251ca69b6cb3b60373d047f011
cbini <cbini87@gmail.com>
2013-12-17 17:22:12 -0500

    added school level, region, SPED, scope, title, department, all good things

--- REPORTING (report cards, reading logs)/v_REPORTING$Optimus.sql
+++ REPORTING (report cards, reading logs)/v_REPORTING$Optimus.sql
@@ -2,7 +2,17 @@ USE KIPP_NJ
 GO
 
 ALTER VIEW REPORTING$Optimus AS
-SELECT CASE
+SELECT 'KIPP NJ' AS Network
+      ,CASE
+        WHEN s.schoolid IN (73252,73253,73254,73255,73256,133570965) THEN 'Newark'
+        ELSE NULL 
+       END AS Region
+      ,CASE
+        WHEN s.schoolid IN (73254,73255,73256) THEN 'ES'
+        WHEN s.schoolid IN (73252,133570965) THEN 'MS'
+        WHEN s.schoolid IN (73253) THEN 'HS'
+       END AS school_level
+      ,CASE
         WHEN s.schoolid = 73252 THEN 'Rise'
         WHEN s.schoolid = 73253 THEN 'NCA'
         WHEN s.schoolid = 73254 THEN 'SPARK'
@@ -16,15 +26,23 @@ SELECT CASE
        END AS grade_level      
       ,s.team
       ,s.lastfirst
-      ,cs.SPEDLEP
-      --,a.assessment_id
-      --,a.title
-      ,a.scope
-      --,dates.time_per_name
-      ,a.subject      
+      ,cs.SPEDLEP      
+      ,a.title
+      ,a.scope      
+      ,CASE
+        WHEN a.subject IN ('Arts: Music','Arts: Visual Arts','Performing Arts') THEN 'Arts'
+        WHEN a.subject IN ('Comprehension','English Language Arts','Grammar','Phonics','Reading','Word Work','Writing') THEN 'Literacy'
+        WHEN a.subject IN ('Historical Arts','History-Social Science') THEN 'Social Studies'
+        WHEN a.subject IN ('Mathematics') THEN 'Math'
+        WHEN a.subject IN ('Science') THEN 'Science'
+        WHEN a.subject IN ('Spanish') THEN 'World Language'
+       END AS department
+      ,a.subject
+      ,a.term
       ,a.administered_at
-      ,a.standards_tested
-      --,a.parent_standard_id
+      ,CONVERT(NVARCHAR(255),a.standards_tested) AS standards_tested
+      ,a.standard_descr
+      --,CASE WHEN a.parent_standard IS NULL THEN a.standards_tested ELSE a.parent_standard END AS parent_standard
       ,ROUND(CONVERT(FLOAT,res.percent_correct),1) AS percent_correct
       ,CONVERT(FLOAT,res.mastered) AS mastered
 FROM ILLUMINATE$assessment_results_by_standard#static res WITH (NOLOCK)
@@ -38,11 +56,4 @@ JOIN ILLUMINATE$assessments#static a WITH (NOLOCK)
  AND res.standard_id = a.standard_id
  AND s.SCHOOLID = a.schoolid
  AND s.GRADE_LEVEL = a.grade_level
-/*
-JOIN REPORTING$dates dates WITH(NOLOCK)
-  ON res.administered_at >= dates.start_date
- AND res.administered_at <= dates.end_date
- AND dates.identifier = 'RT'
- AND a.schoolid = dates.schoolid
---*/
 --WHERE a.scope = 'District Benchmark' 
