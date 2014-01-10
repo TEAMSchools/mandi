8f156213fb63b62ab261f0d9352c35096d448442
cbini <cbini87@gmail.com>
2014-01-10 09:07:30 -0500

    removed unused code and streamlined subqueries

--- MAP/v_MAP$goals_wide.sql
+++ MAP/v_MAP$goals_wide.sql
@@ -8,13 +8,7 @@ SELECT grade_level
       ,course
       ,avg_baseline_rit
       ,avg_cohort_change
-      ,cohort_sd
-      /*
-      ,rit_change_winter
-      ,pctile_change_winter
-      ,ontrack_keepup_pct
-      ,ontrack_rr_pct
-      --*/
+      ,cohort_sd      
       ,CONVERT(FLOAT,ROUND((p01_zscore * cohort_sd) + (avg_baseline_rit + avg_cohort_change),1)) AS p01_goal_spr_2013
       ,CONVERT(FLOAT,ROUND((p05_zscore * cohort_sd) + (avg_baseline_rit + avg_cohort_change),1)) AS p05_goal_spr_2013
       ,CONVERT(FLOAT,ROUND((p10_zscore * cohort_sd) + (avg_baseline_rit + avg_cohort_change),1)) AS p10_goal_spr_2013
@@ -32,11 +26,11 @@ SELECT grade_level
       ,CONVERT(FLOAT,ROUND((p99_zscore * cohort_sd) + (avg_baseline_rit + avg_cohort_change),1)) AS p99_goal_spr_2013
 FROM
      (
-      SELECT sub_3.*
+      SELECT sub_2.*
             ,norms.mean AS avg_cohort_change
             ,norms.sd AS cohort_sd
             ,ROW_NUMBER() OVER
-               (PARTITION BY sub_3.grade_level, sub_3.measurementscale, sub_3.iep_status, sub_3.course
+               (PARTITION BY sub_2.grade_level, sub_2.measurementscale, sub_2.iep_status, sub_2.course
                     ORDER BY ABS(avg_baseline_rit - norms.rit) ASC) AS closest_match
       FROM
            (
@@ -51,119 +45,66 @@ FROM
                     ELSE 'Whole Grade'
                    END AS course
                   ,ROUND(AVG(testritscore),1) AS avg_baseline_RIT
-                  /*
-                  ,AVG(rit_change_winter) AS rit_change_winter
-                  ,AVG(pctile_change_winter) AS pctile_change_winter
-                  ,CONVERT(FLOAT,ROUND(((SUM(ontrack_keepup) / COUNT(*)) * 100),1)) AS ontrack_keepup_pct
-                  ,CONVERT(FLOAT,ROUND(((SUM(ontrack_rr) / COUNT(*)) * 100),1)) AS ontrack_rr_pct
-                  --*/
             FROM
-                 (
-                  SELECT sub_1.measurementscale
-                        ,sub_1.grade_level
-                        ,enrollments.course_name AS match_course
-                        ,enrollments.enr_hash AS match_section
+                 (                  
+                  SELECT s.ID AS studentid                              
+                        ,baseline.measurementscale
+                        ,baseline.grade_level
                         ,CASE
-                          WHEN sub_1.spedlep LIKE '%SPED%' THEN 'IEP'
+                          WHEN cs.spedlep LIKE '%SPED%' THEN 'IEP'
                           ELSE 'No IEP'
                          END AS iep_status
-                        ,sub_1.testritscore
-                        ,sub_1.typical_growth_goal
-                        ,sub_1.typical_growth_target                        
-                        /*
-                        ,rit_change_winter
-                        ,pctile_change_winter
-                        ,ontrack_keepup
-                        ,ontrack_rr                        
-                        --*/
-                  FROM
-                       (
-                        SELECT s.ID AS studentid                              
-                              ,baseline.measurementscale
-                              ,baseline.grade_level
-                              ,cs.SPEDLEP
-                              ,CASE
-                                WHEN baseline.measurementscale = 'Mathematics' THEN 'MATH'
-                                WHEN baseline.measurementscale = 'Reading' THEN 'ENG'
-                                WHEN baseline.measurementscale = 'Language Usage' THEN 'RHET'
-                                WHEN baseline.measurementscale IN ('Science - Concepts and Processes', 'Science - General Science') THEN 'SCI'
-                               END AS credit_join
-                              ,baseline.testritscore
-                              ,norms.r22 AS typical_growth_goal
-                              ,baseline.testritscore + norms.r22 AS typical_growth_target                              
-                              /*
-                              ,(map_winter.testritscore - rr.baseline_rit) AS rit_change_winter
-                              ,(map_winter.testpercentile - rr.baseline_percentile) AS pctile_change_winter
-                              ,CASE
-                                WHEN rr.baseline_rit IS NULL THEN NULL        
-                                WHEN rr.baseline_rit IS NOT NULL AND (map_winter.testritscore - rr.baseline_rit) >= rr.keep_up_goal THEN 1.0
-                                WHEN rr.baseline_rit IS NOT NULL AND (map_winter.testritscore - rr.baseline_rit) > 0 THEN 1.0
-                                ELSE 0.0
-                               END AS ontrack_keepup
-                              ,CASE
-                                WHEN rr.baseline_rit IS NULL THEN NULL
-                                WHEN rr.baseline_rit IS NOT NULL AND (map_winter.testritscore - rr.baseline_rit) >= rutgers_ready_goal THEN 1.0
-                                WHEN rr.baseline_rit IS NOT NULL AND (map_winter.testritscore - rr.baseline_rit) > 0 THEN 1.0
-                                ELSE 0.0
-                               END AS ontrack_rr
-                              --*/
-                        FROM STUDENTS s WITH(NOLOCK)
-                        LEFT OUTER JOIN CUSTOM_STUDENTS cs WITH(NOLOCK)
-                          ON s.id = cs.studentid  
-                        JOIN MAP$best_baseline#static baseline WITH(NOLOCK)
-                          ON s.id = baseline.studentid
-                         AND s.GRADE_LEVEL = baseline.grade_level
-                        /*
-                        LEFT OUTER JOIN MAP$rutgers_ready_student_goals rr WITH(NOLOCK)
-                          ON s.id = rr.studentid
-                         AND s.grade_level = rr.grade_level
-                         AND REPLACE(baseline.measurementscale, ' Usage', '') = rr.measurementscale
-                        LEFT OUTER JOIN MAP$comprehensive#identifiers map_winter WITH(NOLOCK)
-                          ON rr.studentid = map_winter.ps_studentid
-                         AND rr.grade_level = map_winter.grade_level
-                         AND rr.measurementscale = REPLACE(map_winter.measurementscale, ' Usage', '')
-                         AND map_winter.fallwinterspring = 'Winter'
-                         AND rr.year = map_winter.map_year_academic
-                        --*/
-                        LEFT OUTER JOIN MAP$growth_norms_data#2011 norms WITH(NOLOCK)
-                          ON baseline.measurementscale = norms.subject
-                         AND (baseline.grade_level - 1) = norms.startgrade
-                         AND (baseline.testritscore) = norms.startrit                        
-                        WHERE s.schoolid = 133570965
-                          AND s.enroll_status = 0
-                       ) sub_1
-                  LEFT OUTER JOIN
-                                 (
-                                  SELECT cc.studentid
-                                        ,courses.course_name
-                                        ,courses.credittype
-                                        ,courses.course_number
-                                        ,courses.course_name + ': ' + sections.section_number AS enr_hash
-                                  FROM CC WITH(NOLOCK)
-                                  JOIN COURSES WITH(NOLOCK)
-                                    ON cc.course_number = courses.course_number
-                                   AND courses.credittype != 'COCUR'
-                                   AND courses.credittype != 'WLANG'
-                                  JOIN SECTIONS WITH(NOLOCK)
-                                    ON cc.sectionid = sections.id
-                                  WHERE cc.schoolid = 133570965
-                                    AND cc.termid >= 2300
-                                 ) enrollments
-                    ON sub_1.studentid = enrollments.studentid
-                   AND sub_1.credit_join = enrollments.credittype
-                 ) sub_2
+                        ,CASE
+                          WHEN baseline.measurementscale = 'Mathematics' THEN 'MATH'
+                          WHEN baseline.measurementscale = 'Reading' THEN 'ENG'
+                          WHEN baseline.measurementscale = 'Language Usage' THEN 'RHET'
+                          WHEN baseline.measurementscale IN ('Science - Concepts and Processes', 'Science - General Science') THEN 'SCI'
+                         END AS credit_join
+                        ,baseline.testritscore                        
+                        ,enrollments.course_name AS match_course
+                  FROM STUDENTS s WITH(NOLOCK)
+                  LEFT OUTER JOIN CUSTOM_STUDENTS cs WITH(NOLOCK)
+                    ON s.id = cs.studentid  
+                  JOIN MAP$best_baseline#static baseline WITH(NOLOCK)
+                    ON s.id = baseline.studentid
+                   AND s.GRADE_LEVEL = baseline.grade_level                                          
+                  LEFT OUTER JOIN (
+                                   SELECT cc.studentid
+                                         ,courses.course_name
+                                         ,courses.credittype
+                                         ,courses.course_number                                      
+                                   FROM CC WITH(NOLOCK)
+                                   JOIN COURSES WITH(NOLOCK)
+                                     ON cc.course_number = courses.course_number
+                                    AND courses.credittype != 'COCUR'
+                                    AND courses.credittype != 'WLANG'                                  
+                                 /*--update for each school an term--*/
+                                   WHERE cc.schoolid = 133570965
+                                     AND cc.termid >= 2300
+                                  ) enrollments
+                    ON s.id = enrollments.studentid
+                   AND CASE
+                        WHEN baseline.measurementscale = 'Mathematics' THEN 'MATH'
+                        WHEN baseline.measurementscale = 'Reading' THEN 'ENG'
+                        WHEN baseline.measurementscale = 'Language Usage' THEN 'RHET'
+                        WHEN baseline.measurementscale IN ('Science - Concepts and Processes', 'Science - General Science') THEN 'SCI'
+                       END = enrollments.credittype
+                /*--update for each school an term--*/
+                  WHERE s.schoolid = 133570965
+                    AND s.enroll_status = 0
+                 ) sub_1
             WHERE testritscore IS NOT NULL
-              AND sub_2.match_course IS NOT NULL
-              AND sub_2.measurementscale NOT IN ('Science - General Science', 'Science - Concepts and Processes')
-            GROUP BY sub_2.grade_level, sub_2.measurementscale, CUBE(sub_2.iep_status,sub_2.match_course)      
-           ) sub_3 
+              AND sub_1.match_course IS NOT NULL
+              AND sub_1.measurementscale NOT IN ('Science - General Science', 'Science - Concepts and Processes')
+            GROUP BY sub_1.grade_level, sub_1.measurementscale, CUBE(sub_1.iep_status,sub_1.match_course)
+           ) sub_2
       LEFT OUTER JOIN MAP$rit_scale_school_norms norms WITH(NOLOCK)
-        ON norms.subject = sub_3.measurementscale
-       AND norms.grade = sub_3.grade_level
+        ON norms.subject = sub_2.measurementscale
+       AND norms.grade = sub_2.grade_level
        AND norms.term = 'Spring-to-Spring'
-      WHERE (sub_3.iep_status = 'IEP' AND sub_3.course = 'Whole Grade')
-         OR sub_3.iep_status = 'All Students'
-     ) sub_4
+      WHERE (sub_2.iep_status = 'IEP' AND sub_2.course = 'Whole Grade')
+         OR sub_2.iep_status = 'All Students'
+     ) sub_3
 LEFT OUTER JOIN (
                  SELECT *
                  FROM
@@ -220,5 +161,5 @@ LEFT OUTER JOIN (
                             )
                         ) p
                 ) norm_dist
-  ON 1=1
+  ON 1 = 1
 WHERE closest_match = 1
