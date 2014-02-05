dfe1e0697166660e3e53cd1cb2847c7e9d2de885
cbini <cbini87@gmail.com>
2014-02-05 14:06:35 -0500

    organized tables and views, added some basic tables

--- QA (audits, tests)/sp_QA$grant_permissions.sql
+++ QA (audits, tests)/sp_QA$grant_permissions.sql
@@ -2,26 +2,49 @@ USE KIPP_NJ
 GO
 
 ALTER PROCEDURE sp_QA$grant_permissions AS
+
+--DB objects
+GRANT SELECT ON REPORTING$dates TO db_data_tool_reader
+
+--Lit
 GRANT SELECT ON LIT$FP_test_events_long#identifiers#static TO db_data_tool_reader
+GRANT SELECT ON reporting$reading_log TO db_data_tool_reader
+GRANT SELECT ON reporting$reading_log#NCA TO db_data_tool_reader
+
+--Progress trackers
 GRANT SELECT ON REPORTING$progress_tracker#NCA#static TO db_data_tool_reader
 GRANT SELECT ON REPORTING$progress_detail#NCA#static TO db_data_tool_reader
-GRANT SELECT ON REPORTING$quick_lookup#NCA#static TO db_data_tool_reader
 GRANT SELECT ON REPORTING$progress_tracker#Rise_static TO db_data_tool_reader
 GRANT SELECT ON REPORTING$progress_tracker#TEAM_static TO db_data_tool_reader
 GRANT SELECT ON REPORTING$progress_tracker#NCA#static TO db_data_tool_reader
-GRANT SELECT ON reporting$reading_log TO db_data_tool_reader
+GRANT SELECT ON REPORTING$quick_lookup#NCA#static TO db_data_tool_reader
+
+--PS sync
 GRANT SELECT ON STUDENTS TO db_data_tool_reader
 GRANT SELECT ON CUSTOM_STUDENTS TO db_data_tool_reader
-GRANT SELECT ON ES_DAILY$daily_tracking_long#static TO db_data_tool_reader
+GRANT SELECT ON DISC$log#static TO db_data_tool_reader
+GRANT SELECT ON SCHOOLS TO db_data_tool_reader
+GRANT SELECT ON TEACHERS TO db_data_tool_reader
+GRANT SELECT ON CC TO db_data_tool_reader
+GRANT SELECT ON SECTIONS TO db_data_tool_reader
+
+--MAP
+GRANT SELECT ON MAP$cohort_performance_targets TO db_data_tool_reader
 GRANT SELECT ON MAP$rutgers_ready_student_goals TO db_data_tool_reader 
 GRANT SELECT ON MAP$comprehensive#identifiers TO db_data_tool_reader 
 GRANT SELECT ON MAP$best_baseline#static TO db_data_tool_reader
-GRANT SELECT ON DISC$log#static TO db_data_tool_reader
+GRANT SELECT ON REPORTING$MAP_tracker#static TO db_data_tool_reader
+
+--Gradebook
 GRANT SELECT ON GRADES$DETAIL#MS TO db_data_tool_reader
-GRANT SELECT ON REPORTING$dates TO db_data_tool_reader
 GRANT SELECT ON GPA$detail#Rise TO db_data_tool_reader
 GRANT SELECT ON GRADES$elements TO db_data_tool_reader
-GRANT SELECT ON REPORTING$MAP_tracker#static TO db_data_tool_reader
-GRANT SELECT ON MAP$cohort_performance_targets TO db_data_tool_reader
+
+--ES Reporting
+GRANT SELECT ON REPORTING$intervention_results_by_standard TO db_data_tool_reader
+GRANT SELECT ON ES_DAILY$daily_tracking_long#static TO db_data_tool_reader
+
+--DB functions
 GRANT EXECUTE ON fn_Global_Term_Id TO db_data_tool_reader
 GRANT EXECUTE ON GROUP_CONCAT_D TO db_data_tool_reader
+GRANT EXECUTE ON GROUP_CONCAT TO db_data_tool_reader
