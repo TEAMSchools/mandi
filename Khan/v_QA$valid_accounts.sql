17db244a614ea6c351b3bc26433788e6f44c7571
almartin82 <almartin@gmail.com>
2014-02-13 14:37:48 -0500

    valid account flag

--- Khan/v_QA$valid_accounts.sql
+++ Khan/v_QA$valid_accounts.sql
@@ -1,13 +1,17 @@
 USE Khan
 GO
 
-CREATE VIEW QA$valid_accounts AS
+ALTER VIEW QA$valid_accounts AS
 SELECT s.id
       ,s.lastfirst
       ,s.first_name + ' ' + s.last_name AS full_name
       ,s.schoolid
       ,s.grade_level
       ,d.nickname
+      ,CASE
+         WHEN d.nickname IS NOT NULL THEN 1
+         ELSE 0
+       END AS valid_account_flag
       ,replace(convert(varchar,convert(Money, d.points),1),'.00','') AS points
       ,CAST(ROUND((d.total_seconds_watched + 0.0) / 60, 1) AS float) AS min_watched
 FROM KIPP_NJ..STUDENTS s
