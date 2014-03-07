1adcd66138cc158fea05098795a7c4a997935c47
almartin82 <almartin@gmail.com>
2014-03-07 16:03:11 -0500

    item level responses

--- ILLUMINATE/v_illuminate$student_response_detail_long.sql
+++ ILLUMINATE/v_illuminate$student_response_detail_long.sql
@@ -3,12 +3,35 @@ GO
 ALTER VIEW ILLUMINATE$student_response_detail_long AS
 SELECT *
 FROM OPENQUERY(ILLUMINATE, '
-  SELECT sa.student_id
-        ,f.field_id
+  SELECT a.assessment_id
+        ,a.title
+        ,s.local_student_id
+        ,sa.date_taken
+        ,sar.field_id
+        ,f.order
         ,f.sheet_label
-        ,r.response
-  FROM dna_assessments.students_assessments_responses sar
-  JOIN dna_assessments.students_assessments sa USING (student_assessment_id)
-  JOIN dna_assessments.responses r USING (response_id)
-  JOIN dna_assessments.fields f USING (field_id)
+        ,f.is_rubric
+        ,sar.response_id
+        ,r.response AS student_response
+        ,CASE
+           WHEN fr.points IS NULL THEN 0
+           ELSE fr.points
+         END AS points
+        ,f.maximum AS out_of
+  FROM dna_assessments.assessments a
+  JOIN dna_assessments.students_assessments sa
+    ON a.assessment_id = sa.assessment_id
+  JOIN dna_assessments.students_assessments_responses sar
+    ON sa.student_assessment_id = sar.student_assessment_id
+  JOIN dna_assessments.fields f
+    ON sar.field_id = f.field_id
+  LEFT OUTER JOIN dna_assessments.field_responses fr
+    ON sar.field_id = fr.field_id
+   AND sar.response_id = fr.response_id
+  JOIN public.students s
+    ON sa.student_id = s.student_id
+  JOIN dna_assessments.responses r
+    ON sar.response_id = r.response_id
+  ORDER BY sa.student_id
+          ,f.order
 ')
