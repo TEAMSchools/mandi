a21ca3abf969551f330984ff329b682879306796
cbini <cbini87@gmail.com>
2014-03-26 08:40:21 -0400

    opened the floodgates--enrollment and attendance data back to 2004

--- DEVFIN (development and finance demographic views)/v_DEVFIN$att_demographics_long.sql
+++ DEVFIN (development and finance demographic views)/v_DEVFIN$att_demographics_long.sql
@@ -1,13 +1,13 @@
 USE KIPP_NJ
 GO
 
---ALTER VIEW DEVFIN$att_demographics_long AS
+ALTER VIEW DEVFIN$att_demographics_long AS
 
 WITH calendar AS(
   SELECT CONVERT(DATE,date) AS date
   FROM UTIL$reporting_days WITH(NOLOCK)
-  WHERE date >= '2013-07-01' 
-    AND date <= '2014-06-30'
+  WHERE date >= '2004-07-01'
+    AND date <= GETDATE()
  )
 
 ,roster AS(
@@ -15,9 +15,10 @@ WITH calendar AS(
         ,co.grade_level
         ,co.studentid
         ,co.lastfirst
-        ,s.lunchstatus
+        ,CASE WHEN co.HIGHEST_ACHIEVED = co.GRADE_LEVEL THEN s.lunchstatus ELSE lunch.lunch_status END AS lunchstatus
         ,CASE WHEN UPPER(cs.spedlep) LIKE '%SPEECH%' THEN 'SPEECH' ELSE cs.SPEDLEP END AS SPEDLEP
         ,s.ethnicity
+        ,co.year
         ,co.entrydate
         ,co.exitdate
         ,co.rn
@@ -26,8 +27,10 @@ WITH calendar AS(
     ON co.studentid = s.id
   LEFT OUTER JOIN CUSTOM_STUDENTS cs WITH(NOLOCK)
     ON co.studentid = cs.studentid
-  WHERE co.year = 2013
-    AND co.schoolid != 999999
+  LEFT OUTER JOIN PS$lunch_status_long lunch WITH(NOLOCK)
+    ON co.studentid = lunch.studentid
+   AND co.YEAR = lunch.year
+  WHERE co.schoolid != 999999
     AND co.grade_level != 99
  )
 
@@ -37,17 +40,20 @@ WITH calendar AS(
         ,mem.membershipvalue
         ,mem.attendancevalue      
   FROM MEMBERSHIP mem WITH(NOLOCK)
-  WHERE mem.calendardate >= '2013-07-01'
-    AND mem.calendardate <= '2014-06-30'
+  WHERE mem.calendardate >= '2004-07-01'
+    AND mem.calendardate <= GETDATE()
 )
 
 SELECT calendar.date
+      ,roster.YEAR AS academic_year
       ,roster.SCHOOLID
       ,roster.GRADE_LEVEL
       ,roster.STUDENTID
       ,roster.LASTFIRST
-      ,CONVERT(FLOAT,ISNULL(attendance.membershipvalue,0)) AS membershipvalue
-      ,CONVERT(FLOAT,ISNULL(attendance.attendancevalue,1)) AS attendancevalue
+      ,roster.ENTRYDATE
+      ,roster.EXITDATE
+      ,attendance.membershipvalue
+      ,attendance.attendancevalue      
       ,roster.SPEDLEP
       ,roster.lunchstatus
       ,roster.ethnicity
