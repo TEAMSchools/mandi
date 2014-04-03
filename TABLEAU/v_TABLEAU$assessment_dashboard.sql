73e7aa336ab3bd560fa1287a8629caef684d0c3c
cbini <cbini87@gmail.com>
2014-04-03 11:37:39 -0400

    dedicated library for tableu-specific views, duplicates of existing REPORTING$ views

---
+++ TABLEAU/v_TABLEAU$assessment_dashboard.sql
@@ -0,0 +1,33 @@
+USE KIPP_NJ
+GO
+
+ALTER VIEW TABLEAU$assessment_dashboard AS
+SELECT s.schoolid
+      ,s.grade_level
+      ,s.team
+      ,s.student_number
+      ,s.lastfirst
+      ,cs.spedlep      
+      ,a.title
+      ,a.scope            
+      ,a.subject
+      ,a.term
+      ,a.administered_at
+      ,CONVERT(VARCHAR,a.standards_tested) AS standards_tested
+      ,a.standard_descr      
+      ,ROUND(CONVERT(FLOAT,res.percent_correct),1) AS percent_correct
+      ,CONVERT(FLOAT,res.mastered) AS mastered
+      ,ROW_NUMBER() OVER(
+          PARTITION BY s.id, a.scope, a.standards_tested
+              ORDER BY a.administered_at DESC) AS rn_curr
+FROM ILLUMINATE$assessment_results_by_standard#static res WITH (NOLOCK)
+JOIN STUDENTS s WITH (NOLOCK)
+  ON res.local_student_id = s.student_number
+ AND s.enroll_status = 0
+LEFT OUTER JOIN CUSTOM_STUDENTS cs WITH(NOLOCK)
+  ON s.id = cs.studentid
+JOIN ILLUMINATE$assessments#static a WITH (NOLOCK)
+  ON res.assessment_id = a.assessment_id
+ AND res.standard_id = a.standard_id
+ AND s.schoolid = a.schoolid
+ AND s.grade_level = a.grade_level
---
+++ TABLEAU/v_TABLEAU$attendance_dashboard.sql
@@ -0,0 +1,48 @@
+USE KIPP_NJ
+GO
+
+ALTER VIEW TABLEAU$attendance_dashboard AS
+
+SELECT 'KIPP NJ' AS Network
+      ,CASE
+        WHEN s.schoolid IN (73252,73253,73254,73255,73256,133570965) THEN 'Newark'
+        ELSE NULL 
+       END AS Region
+      ,CASE
+        WHEN s.schoolid IN (73254,73255,73256) THEN 'ES'
+        WHEN s.schoolid IN (73252,133570965) THEN 'MS'
+        WHEN s.schoolid IN (73253) THEN 'HS'
+       END AS school_level      
+      ,s.schoolid
+      ,s.lastfirst
+      ,s.grade_level
+      ,s.team
+      ,cs.SPEDLEP
+      ,CONVERT(DATE,mem.calendardate) AS att_date
+      ,mem.membershipvalue
+      ,att.att_code
+      ,CASE WHEN att.att_code IS NULL OR att.att_code IN ('PLE','T','TLE','T10','ET','S','SE','NM') THEN 1 ELSE 0 END AS present
+      --tardies
+      ,CASE WHEN att.att_code IN ('T','TLE','T10','ET') THEN 1 ELSE 0 END AS tardy_all
+      ,CASE WHEN att.att_code IN ('T','TLE','ET') THEN 1 ELSE 0 END AS tardy
+      ,CASE WHEN att.att_code IN ('T10') THEN 1 ELSE 0 END AS tardy_10
+      --absences
+      ,CASE WHEN att.att_code IN ('AD','A','OS','X','D','AE') THEN 1 ELSE 0 END AS absent_all
+      ,CASE WHEN att.att_code IN ('AD','D','AE') THEN 1 ELSE 0 END AS absent_doc
+      ,CASE WHEN att.att_code IN ('A','X') THEN 1 ELSE 0 END AS absent_undoc
+      --suspensions
+      ,CASE WHEN att.att_code IN ('OS','S') THEN 1 ELSE 0 END AS suspension_all
+      ,CASE WHEN att.att_code = 'S' THEN 1 ELSE 0 END AS ISS
+      ,CASE WHEN att.att_code = 'OS' THEN 1 ELSE 0 END AS OSS
+      --other
+      ,CASE WHEN att.att_code IN ('TLE','PLE') THEN 1 ELSE 0 END AS early_dismissal
+FROM STUDENTS s WITH(NOLOCK)
+LEFT OUTER JOIN CUSTOM_STUDENTS cs WITH(NOLOCK)
+  ON s.id = cs.studentid
+JOIN MEMBERSHIP mem WITH(NOLOCK)
+  ON s.id = mem.studentid
+ AND s.schoolid = mem.schoolid
+LEFT OUTER JOIN ATTENDANCE att WITH(NOLOCK)
+  ON s.id = att.studentid
+ AND mem.CALENDARDATE = att.ATT_DATE 
+WHERE s.enroll_status = 0
