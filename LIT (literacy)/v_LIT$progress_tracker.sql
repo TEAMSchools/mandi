befc55b4e891f1236c264ed9398909c129989641
cbini <cbini87@gmail.com>
2014-04-23 08:44:20 -0400

    cleanup

--- LIT (literacy)/v_LIT$progress_tracker.sql
+++ LIT (literacy)/v_LIT$progress_tracker.sql
@@ -1,7 +1,14 @@
-USE KIPP_NJ
+USE [KIPP_NJ]
 GO
 
-ALTER VIEW LIT$progress_tracker AS
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+ALTER VIEW [dbo].[LIT$progress_tracker] AS
 SELECT sub.*
       ,CASE
         WHEN status = 'Did Not Achieve' THEN
@@ -400,12 +407,12 @@ FROM
               WHEN status = 'Achieved' THEN CONVERT(VARCHAR,scores.student_number) + '_'
                                              + CONVERT(VARCHAR,time_per_name) + '_' 
                                              + CONVERT(VARCHAR,status) + '_' 
-                                             + CONVERT(VARCHAR,year) + '_' 
+                                             + CASE WHEN year = 2013 AND time_per_name = 'EOY' THEN CONVERT(VARCHAR,(year - 1)) ELSE CONVERT(VARCHAR,year) END + '_' 
                                              + CONVERT(VARCHAR,achv_high_tri)
               WHEN status = 'Did Not Achieve' THEN CONVERT(VARCHAR,scores.student_number) + '_'
                                                     + CONVERT(VARCHAR,time_per_name) + '_' 
                                                     + CONVERT(VARCHAR,status) + '_' 
-                                                    + CONVERT(VARCHAR,year) + '_' 
+                                                    + CASE WHEN year = 2013 AND time_per_name = 'EOY' THEN CONVERT(VARCHAR,(year - 1)) ELSE CONVERT(VARCHAR,year) END + '_' 
                                                     + CONVERT(VARCHAR,dna_low_tri)
               ELSE NULL
              END AS reporting_hash
@@ -676,3 +683,4 @@ FROM
       WHERE s.enroll_status = 0
      ) sub
 WHERE curr_school != 73253     
+GO
