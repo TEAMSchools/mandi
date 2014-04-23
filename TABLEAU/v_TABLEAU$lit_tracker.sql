c2bd3efe4b87fb9084115ea889bccc55236534cc
cbini <cbini87@gmail.com>
2014-04-23 18:17:03 -0400

    long view for potential tableau tracker

---
+++ TABLEAU/v_TABLEAU$lit_tracker.sql
@@ -0,0 +1,32 @@
+USE KIPP_NJ
+GO
+
+ALTER VIEW TABLEAU$lit_tracker AS
+
+SELECT id.academic_year
+      ,id.schoolid
+      ,id.grade_level
+      ,id.cohort            
+      ,id.test_round      
+      ,id.test_date
+      ,rs.testid
+      ,rs.studentid      
+      ,rs.read_lvl
+      ,rs.status
+      ,id.GLEQ
+      ,id.lvl_num      
+      ,id.instruct_lvl
+      ,id.indep_lvl
+      ,id.color
+      ,id.genre
+      ,id.fp_keylever
+      ,id.fp_wpmrate            
+      ,rs.domain
+      ,rs.field
+      ,rs.score
+      ,rs.benchmark
+      ,rs.is_prof
+      ,rs.dna_reason      
+FROM LIT$readingscores_long rs WITH(NOLOCK)
+JOIN LIT$test_events#identifiers id WITH(NOLOCK)
+  ON rs.unique_id = id.unique_id
