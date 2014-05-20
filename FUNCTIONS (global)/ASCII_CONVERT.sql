3ddcc201b9074290c21a32d4551a7c6a3911771c
cbini <cbini87@gmail.com>
2014-05-20 08:36:11 -0400

    will return the original string if there arent any ASCII codes present, NULLs converted to blanks

--- FUNCTIONS (global)/ASCII_CONVERT.sql
+++ FUNCTIONS (global)/ASCII_CONVERT.sql
@@ -6,7 +6,6 @@ ALTER FUNCTION ASCII_CONVERT (@ascii_string NVARCHAR(MAX))
   AS
 
 BEGIN
-
   DECLARE @string NVARCHAR(MAX)
          ,@in_string NVARCHAR(MAX) 
          ,@out_string NVARCHAR(MAX)
@@ -25,21 +24,21 @@ BEGIN
   SET @ascii_start = NULL
   SET @ascii_end = NULL  
   SET @index = 1;
-
-  WHILE @index <= @len AND CHARINDEX('&#',@string, @index) > 0  -- walk the length of the string, but break if there aren't any remaining ASCII flags
-  BEGIN
   
-   SET @ascii_start = CHARINDEX('&#', @string, @index); -- find the start of the ASCII code
-   SET @ascii_end = CHARINDEX(';',@string,@ascii_start); -- find the end of the ASCII code, relative to the start
-   SET @in_string = SUBSTRING(@string, @index, @ascii_start - @index); -- extract the leading text string before the ASCII code
-   SET @char = CHAR(REPLACE(REPLACE(SUBSTRING(@string, @ascii_start, @ascii_end - @ascii_start + 1),'&#',''),';','')); -- convert ASCII code to CHAR
-   SET @out_string = @out_string + @in_string + @char; -- append to results string   
-   SET @index = (@ascii_end + 1); -- set index to character after ASCII code
+  IF CHARINDEX('&#',@string) > 0
+    BEGIN
+      WHILE @index <= @len AND CHARINDEX('&#',@string, @index) > 0  -- walk the length of the string, but break if there aren't any remaining ASCII flags
+      BEGIN    
+        SET @ascii_start = CHARINDEX('&#', @string, @index); -- find the start of the ASCII code
+        SET @ascii_end = CHARINDEX(';',@string,@ascii_start); -- find the end of the ASCII code, relative to the start
+        SET @in_string = SUBSTRING(@string, @index, @ascii_start - @index); -- extract the leading text string before the ASCII code
+        SET @char = CHAR(REPLACE(REPLACE(SUBSTRING(@string, @ascii_start, @ascii_end - @ascii_start + 1),'&#',''),';','')); -- convert ASCII code to CHAR
+        SET @out_string = ISNULL(@out_string,'') + ISNULL(@in_string,'') + ISNULL(@char,''); -- append to results string   
+        SET @index = (@ascii_end + 1); -- set index to character after ASCII code    
+      END      
+      SET @out_string = ISNULL(@out_string,'') + ISNULL(SUBSTRING(@string, @ascii_end + 1, @len - @ascii_end),''); -- when the loop breaks, append the residual string
+    END  
+  ELSE SET @out_string = @string  
   
-  END
-
-  SET @out_string = @out_string + SUBSTRING(@string, @ascii_end + 1, @len - @ascii_end); -- when the loop breaks, append the residual string
-
   RETURN @out_string;
-
 END
