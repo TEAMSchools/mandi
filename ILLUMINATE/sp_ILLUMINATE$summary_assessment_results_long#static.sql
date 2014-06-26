7fc71de2dc891154d5cc9f2b5b184aeec76928be
cbini <cbini87@gmail.com>
2014-06-26 17:38:46 -0400

    true merge now, had to add row number to avoid false matches, and of course :lipstick:

--- ILLUMINATE/sp_ILLUMINATE$summary_assessment_results_long#static.sql
+++ ILLUMINATE/sp_ILLUMINATE$summary_assessment_results_long#static.sql
@@ -8,7 +8,7 @@ BEGIN
   -- 1.) Drop and recreate the temp table --
   IF OBJECT_ID(N'tempdb..#ILLUMINATE$summary_assessment_results_long#static|refresh') IS NOT NULL
     DROP TABLE [#ILLUMINATE$summary_assessment_results_long#static|refresh]
-    CREATE TABLE [#ILLUMINATE$summary_assessment_results_long#static|refresh] (repository_id INT, student_id INT, field NVARCHAR(MAX), value NVARCHAR(MAX))
+    CREATE TABLE [#ILLUMINATE$summary_assessment_results_long#static|refresh] (repository_id INT, repository_row_id INT, student_id INT, field NVARCHAR(MAX), value NVARCHAR(MAX))
 
 
   -- 2.) Declare variables --
@@ -20,14 +20,14 @@ BEGIN
 
 
   -- 3.) Declare the cursor FOR the set of records it will loop over --
-      -- cursor name MUST be unique within schema
+  -- cursor name MUST be unique within schema
   DECLARE illuminate_cursor CURSOR FOR
     SELECT repository_id        
     FROM OPENQUERY(ILLUMINATE,'
       SELECT repository_id          
       FROM dna_repositories.repositories    
       WHERE deleted_at IS NULL      
-        AND repository_id < 53
+        --AND repository_id < 53
     ')
 
   		    
@@ -42,10 +42,8 @@ BEGIN
       IF @@FETCH_STATUS <> 0
         BEGIN
           BREAK
-        END
-  --
-
-    -- code block goes here
+        END  
+    
       -- grab column headers using GROUP_CONCAT to format them for the UNPIVOT statement
       SELECT @cols = '[' + dbo.GROUP_CONCAT_D(name, '], [') + ']'
       FROM OPENQUERY(ILLUMINATE,'
@@ -73,12 +71,15 @@ BEGIN
       SET @query = N'                  
         INSERT INTO [#ILLUMINATE$summary_assessment_results_long#static|refresh]
         SELECT ' + @repository_id + ' AS repository_id
+              ,repository_row_id
               ,student_id
               ,field
               ,value
         FROM OPENQUERY(ILLUMINATE,''
-          SELECT student_id ' + @converted_cols + ' AS TEXT)
-          FROM dna_repositories.repository_' + @repository_id + '
+          SELECT s.local_student_id AS student_id, repository_row_id' + @converted_cols + ' AS TEXT)
+          FROM dna_repositories.repository_' + @repository_id + ' repo
+          JOIN public.students s
+            ON repo.student_id = s.student_id
         '')
         
         UNPIVOT (
@@ -87,9 +88,10 @@ BEGIN
          ) unpiv           
       '        
       
+      -- print the query that has just been prepared for debugging
       RAISERROR(@query, 0, 1)
       
-      -- executes the query string above
+      -- execute the query string above
       EXEC(@query)
           
     END
@@ -98,59 +100,36 @@ BEGIN
   DEALLOCATE illuminate_cursor;
 
 
-  -- 5.) Clear out the results table
-  EXEC('TRUNCATE TABLE KIPP_NJ..ILLUMINATE$summary_assessment_results_long#static');
-
-
-  -- 6.) Disable all nonclustered indexes on table
-  SELECT @sql = @sql + 'ALTER INDEX ' + indexes.name + ' ON  dbo.' + objects.name + ' DISABLE;' + CHAR(13) + CHAR(10)
-  FROM sys.indexes
-  JOIN sys.objects 
-    ON sys.indexes.object_id = sys.objects.object_id
-  WHERE sys.indexes.type_desc = 'NONCLUSTERED'
-    AND sys.objects.type_desc = 'USER_TABLE'
-    AND sys.objects.name = 'ILLUMINATE$summary_assessment_results_long#static';
-  EXEC (@sql);
-
-
-  -- 7.) UPSERT, matching on repo, student, and field
+  -- 5.) UPSERT, matching on repo, row number, studentid, and field name
   MERGE ILLUMINATE$summary_assessment_results_long#static AS TARGET
-    USING (
-           SELECT *
-           FROM [#ILLUMINATE$summary_assessment_results_long#static|refresh]
-           ) AS SOURCE
-          (repository_id
-          ,student_id
-          ,field
-          ,value) 
-     ON target.repository_id = source.repository_id
-    AND target.student_id = source.student_id
-    AND target.field = source.field
-           
-      WHEN MATCHED THEN
-        UPDATE
-          SET target.value = source.value
-          
-      WHEN NOT MATCHED THEN
-        INSERT (repository_id
+  USING (SELECT repository_id
+               ,repository_row_id
                ,student_id
                ,field
-               ,value)
-        VALUES (source.repository_id
-               ,source.student_id
-               ,source.field
-               ,source.value);
-               
-               
-  -- 8.) Rebuld all nonclustered indexes on table
-  SELECT @sql = @sql + 'ALTER INDEX ' + indexes.name + ' ON  dbo.' + objects.name +' REBUILD;' + CHAR(13) + CHAR(10)
-  FROM sys.indexes
-  JOIN sys.objects 
-    ON sys.indexes.object_id = sys.objects.object_id
-  WHERE sys.indexes.type_desc = 'NONCLUSTERED'
-    AND sys.objects.type_desc = 'USER_TABLE'
-    AND sys.objects.name = 'ILLUMINATE$summary_assessment_results_long#static';
-  EXEC (@sql);
+               ,value
+         FROM [#ILLUMINATE$summary_assessment_results_long#static|refresh]) AS SOURCE  
+     (repository_id
+     ,repository_row_id
+     ,student_id
+     ,field,value)
+   ON target.repository_id = source.repository_id
+  AND target.repository_row_id = source.repository_row_id
+  AND target.student_id = source.student_id
+  AND target.field = source.field           
+  WHEN MATCHED THEN
+    UPDATE
+    SET target.value = source.value          
+  WHEN NOT MATCHED THEN
+    INSERT (repository_id
+           ,repository_row_id
+           ,student_id
+           ,field
+           ,value)
+    VALUES (source.repository_id
+           ,source.repository_row_id
+           ,source.student_id
+           ,source.field
+           ,source.value);
 
 END
 
