4ac9e1886fa1c233ea89efb795f0cacb7818beac
cbini <cbini87@gmail.com>
2014-07-10 19:02:26 -0400

    :squirrel: this is a big one, it will load all flat files in a given folder into a DB table

--- UTIL/sp_LoadFolder.sql
+++ UTIL/sp_LoadFolder.sql
@@ -5,136 +5,198 @@ ALTER PROCEDURE sp_LoadFolder
   @dir VARCHAR(8000)
 AS
 
-DECLARE @sql NVARCHAR(MAX),
-        @filename NVARCHAR(1024),
-        @ID INT;
+BEGIN
 
--- if the filepath ends with a \ then leave it alone
--- if not append it to the filepath
-IF RIGHT(@dir,1) = '\'
-    SET @dir = @dir
-  ELSE
-    SET @dir = @dir + '\'
-
--- we need a temp table holding the names of the files we want to import
--- if the table exists drop it
-IF OBJECT_ID('tempdb..#TEMP_import_files') IS NOT NULL
-    DROP TABLE #TEMP_import_files;
-
--- recreate the table with a primary key
--- it will be used in the WHILE loop
-CREATE TABLE #TEMP_import_files 
-  (       
-   id INT IDENTITY(1,1)
-  ,subdirectory NVARCHAR(512)
-  ,depth INT
-  ,isfile BIT
-  );
-
--- insert the filenames into the temp table
--- the folder path is specified as a parameter of the stored procedure
-PRINT 'Loading filenames into temp table...'
-INSERT INTO #TEMP_import_files
-  (
-   subdirectory
-  ,depth
-  ,isfile
-  )
-EXEC xp_dirtree @dir, 1, 1;
-
--- start the while loop
-WHILE EXISTS (SELECT id FROM #TEMP_import_files)
-  BEGIN
-
-    -- take the top record and eliminate the file extension  
-    SELECT TOP(1) 
-           @ID = id
-          ,@filename = REVERSE(SUBSTRING(REVERSE(subdirectory),CHARINDEX('.', REVERSE(subdirectory)) + 1, 999))        
-    FROM #TEMP_import_files;   
-
-    -- if a corresponding table exists, TRUNCATE it
-    -- if not, create it using SELECT * INTO via OPENROWSET and then TRUNCATE it so you don't get dupes
-    -- for new tables, add a primary key for indexing purposes
-    -- I called ours BINI_ID because my life is dope and I do dope shit
-    SET @sql = '    
-      IF OBJECT_ID(''KIPP_NJ..' + @filename + ''') IS NOT NULL
-          BEGIN
-            EXEC(''TRUNCATE TABLE KIPP_NJ..' + @filename + ''');
-            PRINT CHAR(13) + CHAR(13) + ''TRUNCATE ' + @filename + '''
-          END
-        ELSE        
-          BEGIN
-            PRINT ''CREATE TABLE ' + @filename + '''
-            SELECT *
-            INTO KIPP_NJ..' + @filename + '
-            FROM OPENROWSET(
-              ''MSDASQL''
-             ,''Driver={Microsoft Access Text Driver (*.txt, *.csv)};''
-             ,''SELECT * FROM ' + @dir + @filename  + '.csv'');
-        
-            ALTER TABLE ' + @filename + '
-            ADD BINI_ID INT IDENTITY(1,1);
-        
-            ALTER TABLE ' + @filename + '
-            ADD CONSTRAINT PK_' + @filename + ' PRIMARY KEY(BINI_ID);
-            
-            PRINT ''TRUNCATE ' + @filename + '''
-            EXEC(''TRUNCATE TABLE KIPP_NJ..' + @filename + ''');
-          END
-    '  
-    EXEC sp_executesql @sql;
-          
-
-        
-    -- load the new data into the tables via OPENROWSET
-    -- the CTE isn't totally necessary in this case, but it's 
-    -- kind of a nice way to organize the code
-    SET @sql = '      
-      DECLARE @inner_sql NVARCHAR(MAX)
-
-      PRINT ''Loading ' + @filename + '...'';
-
-      SELECT @inner_sql = ''ALTER INDEX '' 
-              + indexes.name + '' ON dbo.'' 
-              + objects.name + '' DISABLE;'' + CHAR(13) + CHAR(10)
-      FROM sys.indexes
-      JOIN sys.objects 
-        ON sys.indexes.object_id = sys.objects.object_id
-      WHERE sys.indexes.type_desc = ''NONCLUSTERED''
-        AND sys.objects.type_desc = ''USER_TABLE''
-        AND sys.objects.name = ''' + @filename + ''';
-      EXEC (@inner_sql);
-
-      WITH flatfile AS (
-        SELECT *
-        FROM OPENROWSET(
-          ''MSDASQL''
-         ,''Driver={Microsoft Access Text Driver (*.txt, *.csv)};''
-         ,''SELECT * FROM ' + @dir + @filename  + '.csv'')
-       )
-
-      INSERT INTO ' + @filename + '
-      SELECT *  
-      FROM flatfile;
+  DECLARE @sql NVARCHAR(MAX),
+          @filename NVARCHAR(1024),
+          @tablename NVARCHAR(1024),
+          @ID INT,
+          @CMD VARCHAR(256);
 
 
-      SELECT @inner_sql = ''ALTER INDEX '' 
-              + indexes.name + '' ON dbo.'' 
-              + objects.name + '' REBUILD;'' + CHAR(13) + CHAR(10)
-      FROM sys.indexes
-      JOIN sys.objects 
-        ON sys.indexes.object_id = sys.objects.object_id
-      WHERE sys.indexes.type_desc = ''NONCLUSTERED''
-        AND sys.objects.type_desc = ''USER_TABLE''
-        AND sys.objects.name = ''' + @filename + ''';
-      EXEC (@inner_sql);
-    '
-    EXEC sp_executesql @sql;
-  
-    -- remove it from the list
-    PRINT CHAR(13) + 'Removing ' + @filename + ' from import list...'
-    DELETE FROM #TEMP_import_files WHERE id = @ID;
+  -- set the command line for xp_cmdshell
+  SET @CMD = 'DIR "' + @dir + '" /A /S'
+
+
+  -- if the filepath ends with a '\' then leave it alone
+  -- if not append it to the filepath
+  IF RIGHT(@dir,1) = '\'
+      SET @dir = @dir
+    ELSE
+      SET @dir = @dir + '\'
+
+
+  -- we need temp tables to hold the command line response
+  -- as well as the names of the files we want to import
+  IF OBJECT_ID('tempdb..#TEMP_cmd_response') IS NOT NULL
+      DROP TABLE #TEMP_cmd_response
+
+  IF OBJECT_ID('tempdb..#TEMP_import_files') IS NOT NULL
+      DROP TABLE #TEMP_import_files;
+
+  CREATE TABLE #TEMP_cmd_response
+    (
+     returnval NVARCHAR(500)
+    )
+
+  CREATE TABLE #TEMP_import_files 
+    (       
+     id INT IDENTITY(1,1) -- the ID key is used for the WHILE loop
+    ,subdirectory NVARCHAR(512)
+    ,size INT
+    ,extension NVARCHAR(16)
+    );
+
+
+  -- populate temp table with the command line response
+  -- and delete rows with no file information
+  PRINT 'Getting list of files in folder...'
+  INSERT #TEMP_cmd_response EXEC master..xp_cmdshell @cmd;
+  PRINT CHAR(13) + 'Cleaning up the file list...'
+  DELETE FROM #TEMP_cmd_response 
+  WHERE returnval IS NULL
+    OR (ISNUMERIC(LEFT(returnval,1))=0 
+        AND returnval NOT LIKE '%Directory of%')
+    OR returnval LIKE '%<DIR>          .%';
+
+
+  -- parse the command line response and then
+  -- insert the file metadata into the temp table
+  PRINT CHAR(13) + 'Loading filenames into temp table...'
+  INSERT INTO #TEMP_import_files
+    (
+     subdirectory
+    ,size
+    ,extension
+    )
+  SELECT subdirectory
+        ,size
+        ,extension
+  FROM 
+      (
+       SELECT CASE 
+               WHEN SUBSTRING(returnval,22,17) LIKE '%<DIR>%' THEN NULL 
+               ELSE CONVERT(INT,REPLACE(SUBSTRING(returnval, 22, 17), ',', '')) 
+              END AS size
+             ,RIGHT(RTRIM([returnval]), LEN(RTRIM([returnval])) - 39) AS subdirectory
+             ,CASE 
+               WHEN SUBSTRING(returnval, 22, 17) LIKE '%<DIR>%' THEN NULL 
+               ELSE RIGHT(rtrim([returnval]), CHARINDEX('.', REVERSE(RTRIM([returnval])))) 
+              END AS extension
+       FROM #TEMP_cmd_response t
+       WHERE returnval NOT LIKE '%Directory of%'
+      ) sub
+  ;
+
+
+  -- start the loop!
+  PRINT CHAR(13) + 'Leg''go!'
+  WHILE EXISTS (
+                SELECT id 
+                FROM #TEMP_import_files
+                WHERE extension IN ('.csv','.txt')
+                  AND size > 0
+               )
+    BEGIN
     
-    PRINT CHAR(13) + 'Next!'
+      -- for debugging if this gets stuck in an infinite loop
+      --SELECT *
+      --FROM #TEMP_import_files
+      --WHERE extension IN ('.csv','.txt')
+      --  AND size > 0;
 
-  END
+      -- take the top record and eliminate the file extension  
+      SELECT TOP(1) 
+             @ID = id
+            ,@filename = subdirectory
+            ,@tablename = 'AUTOLOAD$' + REVERSE(SUBSTRING(REVERSE(subdirectory),CHARINDEX('.', REVERSE(subdirectory)) + 1, 999))        
+      FROM #TEMP_import_files
+      WHERE extension IN ('.csv','.txt')
+        AND size > 0;
+
+      -- if a corresponding table exists, TRUNCATE it
+      -- if not, create it using SELECT * INTO via OPENROWSET and then TRUNCATE it so you don't get dupes
+      -- for new tables, add a primary key for indexing purposes
+      -- I called ours BINI_ID because my life is dope and I do dope shit
+      SET @sql = '    
+        IF OBJECT_ID(''KIPP_NJ..' + @tablename + ''') IS NOT NULL
+            BEGIN
+              EXEC(''TRUNCATE TABLE KIPP_NJ..' + @tablename + ''');
+              PRINT CHAR(13) + ''TRUNCATE ' + @tablename + '''
+            END
+          ELSE        
+            BEGIN
+              PRINT CHAR(13) + CHAR(13) + ''CREATE TABLE ' + @tablename + '''
+              SELECT *
+              INTO KIPP_NJ..' + @tablename + '
+              FROM OPENROWSET(
+                ''MSDASQL''
+               ,''Driver={Microsoft Access Text Driver (*.txt, *.csv)};''
+               ,''SELECT * FROM ' + @dir + @filename  + ''');
+        
+              ALTER TABLE ' + @tablename + '
+              ADD BINI_ID INT IDENTITY(1,1);
+        
+              ALTER TABLE ' + @tablename + '
+              ADD CONSTRAINT PK_' + @tablename + ' PRIMARY KEY(BINI_ID);
+            
+              PRINT ''TRUNCATE ' + @tablename + '''
+              EXEC(''TRUNCATE TABLE KIPP_NJ..' + @tablename + ''');
+            END
+      '  
+      EXEC sp_executesql @sql;
+            
+      -- load the new data into the tables via OPENROWSET
+      -- the CTE isn't totally necessary in this case, but it's 
+      -- kind of a nice way to organize the code
+      SET @sql = '      
+        DECLARE @inner_sql NVARCHAR(MAX)
+
+        PRINT ''Loading ' + @filename + '...'';
+
+        SELECT @inner_sql = ''ALTER INDEX '' 
+                + indexes.name + '' ON dbo.'' 
+                + objects.name + '' DISABLE;'' + CHAR(13) + CHAR(10)
+        FROM sys.indexes
+        JOIN sys.objects 
+          ON sys.indexes.object_id = sys.objects.object_id
+        WHERE sys.indexes.type_desc = ''NONCLUSTERED''
+          AND sys.objects.type_desc = ''USER_TABLE''
+          AND sys.objects.name = ''' + @tablename + ''';
+        EXEC (@inner_sql);
+
+        WITH flatfile AS (
+          SELECT *
+          FROM OPENROWSET(
+            ''MSDASQL''
+           ,''Driver={Microsoft Access Text Driver (*.txt, *.csv)};''
+           ,''SELECT * FROM ' + @dir + @filename + ''')
+         )
+
+        INSERT INTO ' + @tablename + '
+        SELECT *  
+        FROM flatfile;
+
+
+        SELECT @inner_sql = ''ALTER INDEX '' 
+                + indexes.name + '' ON dbo.'' 
+                + objects.name + '' REBUILD;'' + CHAR(13) + CHAR(10)
+        FROM sys.indexes
+        JOIN sys.objects 
+          ON sys.indexes.object_id = sys.objects.object_id
+        WHERE sys.indexes.type_desc = ''NONCLUSTERED''
+          AND sys.objects.type_desc = ''USER_TABLE''
+          AND sys.objects.name = ''' + @tablename + ''';
+        EXEC (@inner_sql);
+      '
+      EXEC sp_executesql @sql;
+  
+      -- remove it from the list
+      PRINT CHAR(13) + 'Removing ' + @filename + ' from import list...'
+      DELETE FROM #TEMP_import_files WHERE id = @ID;
+    
+      PRINT CHAR(13) + 'Next!'
+
+    END
+
+END
