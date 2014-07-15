5ab7a86edb6e3f0301ab11be61082320b400f7f1
cbini <cbini87@gmail.com>
2014-07-15 08:33:17 -0400

    commented up

--- UTIL/sp_CacheView.sql
+++ UTIL/sp_CacheView.sql
@@ -8,12 +8,13 @@ USE KIPP_NJ
 GO
 
 ALTER PROCEDURE sp_CacheView
-  @db NVARCHAR(256),
-  @view NVARCHAR(256)
+  @db NVARCHAR(256), -- name of the destination DB
+  @view NVARCHAR(256) -- name of the view you want to cache
 AS
 
 BEGIN  
   
+  -- check that the object exists and is a view
   IF OBJECTPROPERTY(OBJECT_ID(@db + '..' + @view), 'ISVIEW') IS NULL OR OBJECTPROPERTY(OBJECT_ID(@db + '..' + @view), 'ISVIEW') = 0
       BEGIN
         PRINT 'You need a view, bro.  Try again.'
@@ -21,11 +22,13 @@ BEGIN
 
     ELSE
       BEGIN
+        -- check that there already isn't an existing object with the same name
         IF OBJECT_ID(@db + '..' + @view + '#static') IS NULL
             BEGIN
         
               DECLARE @sql NVARCHAR(MAX);
 
+              -- create the table structure
               BEGIN                
                 SET @sql = '
                   SELECT *
@@ -38,11 +41,15 @@ BEGIN
               END
 
               BEGIN                
+                -- make sure it's operating on the destination DB
+                -- this needed to be separated out from the rest of the procedure code
+                -- otherwise it throws a syntax error
                 SET @sql = '
                   USE ' + @db + '
                 ';
                 EXEC(@sql);
 
+                -- actual procedure code starts here
                 SET @sql = '                  
                   CREATE PROCEDURE [sp_' + @view + '#static|refresh] AS
                   BEGIN
@@ -99,6 +106,8 @@ BEGIN
   
                   END                  
                 ';                
+                -- print the generated code to console and then execute the creation
+                -- SAVE THIS TO mandi!
                 PRINT 'Copy and save this code as sp_' + @view + '#static_refresh:' + CHAR(13) + CHAR(10)
                 PRINT 'USE [' + @db + ']' + CHAR(13) + CHAR(10)
                         + 'GO'  + CHAR(13) + CHAR(10)  + CHAR(13) + CHAR(10)
@@ -106,9 +115,10 @@ BEGIN
                         + 'GO' + CHAR(13) + CHAR(10) + CHAR(13) + CHAR(10)
                         + 'SET QUOTED_IDENTIFIER ON' + CHAR(13) + CHAR(10)
                         + 'GO' + CHAR(13) + CHAR(10) + CHAR(13) + CHAR(10)
-                        + REPLACE(@sql,'CREATE','ALTER');
+                        + REPLACE(@sql,'CREATE','ALTER'); -- changes it to ALTER for you
                 EXEC(@sql);
 
+                -- finally, execute the newly created procedure
                 SET @sql = '
                   EXEC [sp_' + @view + '#static|refresh]
                 ';
@@ -120,6 +130,8 @@ BEGIN
 
           ELSE      
 
+            -- if the table exists, this will print and the procedure will terminate
+            -- I'm thinking about adding a 3rd parameter for rebuilding static tables that need to be adjusted
             BEGIN
               PRINT 'Static table already exists.  You''re gonna break something, ya dingus.  Aborting process...'
             END
