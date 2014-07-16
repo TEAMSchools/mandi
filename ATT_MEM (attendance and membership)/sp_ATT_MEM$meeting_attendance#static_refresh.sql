8022d68866d0cd19c5024960126fb6f165f876e2
cbini <cbini87@gmail.com>
2014-07-16 19:40:25 -0400

    meeting attendance and att codes now synced

---
+++ ATT_MEM (attendance and membership)/sp_ATT_MEM$meeting_attendance#static_refresh.sql
@@ -0,0 +1,64 @@
+USE [KIPP_NJ]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+                  
+ALTER PROCEDURE [sp_ATT_MEM$meeting_attendance#static|refresh] AS
+BEGIN
+
+  DECLARE @sql AS VARCHAR(MAX)='';
+
+  -- STEP 1: make sure no temp table
+		IF OBJECT_ID(N'tempdb..#ATT_MEM$meeting_attendance#static|refresh') IS NOT NULL
+		BEGIN
+						DROP TABLE [#ATT_MEM$meeting_attendance#static|refresh]
+		END
+
+
+  -- STEP 2: load into a temporary staging table.
+  SELECT *
+		INTO [#ATT_MEM$meeting_attendance#static|refresh]
+  FROM ATT_MEM$meeting_attendance;
+         
+
+  -- STEP 3: truncate destination table
+  EXEC('TRUNCATE TABLE KIPP_NJ..ATT_MEM$meeting_attendance#static');
+
+
+  -- STEP 4: disable all nonclustered indexes on table
+  SELECT @sql = @sql + 'ALTER INDEX ' 
+                 + indexes.name + ' ON dbo.' 
+                 + objects.name + ' DISABLE;' + CHAR(13) + CHAR(10)
+  FROM sys.indexes
+  JOIN sys.objects 
+    ON sys.indexes.object_id = sys.objects.object_id
+  WHERE sys.indexes.type_desc = 'NONCLUSTERED'
+    AND sys.objects.type_desc = 'USER_TABLE'
+    AND sys.objects.name = 'ATT_MEM$meeting_attendance#static';
+  EXEC (@sql);
+
+
+  -- STEP 5: insert into final destination
+  INSERT INTO [ATT_MEM$meeting_attendance#static]
+  SELECT *
+  FROM [#ATT_MEM$meeting_attendance#static|refresh];
+ 
+
+  -- STEP 6: rebuld all nonclustered indexes on table
+  SELECT @sql = @sql + 'ALTER INDEX ' 
+                  + indexes.name + ' ON dbo.' 
+                  + objects.name + ' REBUILD;' + CHAR(13) + CHAR(10)
+  FROM sys.indexes
+  JOIN sys.objects 
+    ON sys.indexes.object_id = sys.objects.object_id
+  WHERE sys.indexes.type_desc = 'NONCLUSTERED'
+    AND sys.objects.type_desc = 'USER_TABLE'
+    AND sys.objects.name = 'ATT_MEM$meeting_attendance#static';
+  EXEC (@sql);
+  
+END                  
