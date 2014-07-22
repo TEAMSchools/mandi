892401a8d73268ea659e1f3c08096976ae8566ee
cbini <cbini87@gmail.com>
2014-07-22 17:10:12 -0400

    python scripts for syncing google docs and naviance with the db

---
+++ DATA_INTEGRATION/load_gdocs.py
@@ -0,0 +1,111 @@
+__author__ = 'CBini'
+
+import csv
+import json
+import gspread
+import re
+import pymssql
+
+
+"""
+CONFIG
+"""
+# db config
+server_name = "WINSQL01\NARDO"
+
+# key files
+gdocs_secret = "W:\\data_robot\\logistical\\gdocs_secret.json"
+db_secret = "W:\\data_robot\\logistical\\nardo_secret.json"
+config_path = "C:\\Users\\CBini\\Desktop\\gdocs.csv"
+
+# keys
+keys = json.load(file(gdocs_secret))
+gdocs_user = keys['GDOCS_USERNAME']
+gdocs_pass = keys['GDOCS_PASSWORD']
+
+keys = json.load(file(db_secret))
+db_user = keys['NARDO_USERNAME']
+db_pass = keys['NARDO_PASSWORD']
+
+
+"""
+DOWNLOAD
+"""
+# open config file
+with open(config_path, 'rb') as f:
+    next(f)  # skip header
+    gdocs = csv.reader(f, delimiter=',')
+    wkbk_list = []
+    for record in gdocs:
+        print record
+        wkbk_list.append(record)
+    print
+
+client = gspread.login(gdocs_user, gdocs_pass)
+
+# iterate over urls in config csv
+for record in wkbk_list:
+    url = record[0]
+    tag = record[1]
+    db_name = record[2]
+    folder = record[3]
+    start_sheet = int(record[4])
+    end_sheet = int(record[5]) + 1  # range() will create a 0-indexed list of integers, so this adjusts for that
+    start_row = int(record[6])
+
+    # file paths
+    save_path = "W:\\data_robot\\" + folder + "\\"
+    save_path_server = "C:\\data_robot\\" + folder + "\\"
+
+    print 'Navigating to ' + url
+    workbook = client.open_by_url(url)
+    print 'Now saving worksheets ' + str(start_sheet + 1) + ' through ' + str(end_sheet) + '...'
+    print 'Getting worksheet names...'
+    sheet_names = workbook.worksheets()
+    print 'Let\'s go!'
+    print
+
+    # iterate over sequential worksheets
+    for i in range(start_sheet, end_sheet):
+        sheet_name = str(sheet_names[i])
+        # extract name between ' ' and removes spaces
+        clean_name = re.search("'(.*)'", sheet_name).group(1).replace(' ', '_')
+
+        print 'Opening worksheet ' + str(i + 1) + ': "' + str(clean_name) + '"'
+        worksheet = workbook.get_worksheet(i)
+        print 'Getting and cleaning data...'
+        data = worksheet.get_all_values()
+        # list comprehension quickly removes unwanted header rows
+        data = [line for x, line in enumerate(data) if x >= start_row]
+
+        filename = save_path + 'GDOCS_' + tag + '_' + clean_name + '.csv'
+        print 'Downloading file ' + filename + ' to server, starting at line ' + str(start_row + 1)
+        with open(filename, 'wb') as f:
+            writer = csv.writer(f)
+            writer.writerows(data)
+
+        print 'Done!'
+        print
+
+    """
+    LOAD INTO DB
+    """
+    print 'Download complete! Loading into NARDO...'
+    print
+
+    conn = pymssql.connect(server_name, db_user, db_pass, db_name)
+    cursor = conn.cursor()
+
+    query = "sp_LoadFolder '" + db_name + "', '" + save_path_server + "'"
+    print 'Running "EXEC ' + query + '"'
+    cursor.execute(query)
+
+    print 'NARDO say: ' + cursor.fetchall()[0][0]
+    conn.commit()
+    conn.close()
+
+    print
+    print 'Next workbook!'
+
+print
+print 'All done!'
