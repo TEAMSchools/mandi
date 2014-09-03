3ecd4db94de0ce2ff658d1932161ba8593b52e78
almartin82 <almartin@gmail.com>
2014-09-03 10:16:22 -0400

    refactor, handle missing 10th

--- MAP/v_MAP$growth_norms_data_extended.sql
+++ MAP/v_MAP$growth_norms_data_extended.sql
@@ -9,9 +9,27 @@ WITH master_growth_norms AS
 --stuff in the table
 SELECT *
 FROM master_growth_norms
---missing stuff
+WHERE startgrade < 10
 
---reading 11
+--slightly massage the 10th grade existing data
+--(because they report NULL for spring to spring)
+UNION ALL
+SELECT m.subject
+      ,m.startgrade
+      ,m.startrit
+      ,m.t41, m.t42, m.t44, back_1.t22, m.t12
+      ,m.r41, m.r42, m.r44, back_1.r22, m.r12
+      ,m.s41, m.s42, m.s44, back_1.s22, m.s12
+FROM master_growth_norms m
+JOIN master_growth_norms back_1 
+  ON m.subject = back_1.subject
+ AND m.startrit = back_1.startrit
+ AND m.startgrade - 1 = back_1.startgrade
+WHERE m.startgrade = 10
+
+--now do the missing stuff (grade 11 and 12)
+
+--grade 11
 UNION ALL
 SELECT m.subject
       ,11 AS startgrade
@@ -24,9 +42,8 @@ JOIN master_growth_norms back_1
   ON m.subject = back_1.subject
  AND m.startrit = back_1.startrit
  AND m.startgrade - 1 = back_1.startgrade
-WHERE m.subject = 'Reading'
-  AND m.startgrade = 10
---reading 12
+WHERE m.startgrade = 10
+--grade 12
 UNION ALL
 SELECT m.subject
       ,12 AS startgrade
@@ -39,95 +56,4 @@ JOIN master_growth_norms back_1
   ON m.subject = back_1.subject
  AND m.startrit = back_1.startrit
  AND m.startgrade - 1 = back_1.startgrade
-WHERE m.subject = 'Reading'
-  AND m.startgrade = 10
---mathematics 11
-UNION ALL
-SELECT m.subject
-      ,11 AS startgrade
-      ,m.startrit
-      ,m.t41, m.t42, m.t44, back_1.t22, m.t12
-      ,m.r41, m.r42, m.r44, back_1.r22, m.r12
-      ,m.s41, m.s42, m.s44, back_1.s22, m.s12
-FROM master_growth_norms m
-JOIN master_growth_norms back_1 
-  ON m.subject = back_1.subject
- AND m.startrit = back_1.startrit
- AND m.startgrade - 1 = back_1.startgrade
-WHERE m.subject = 'Mathematics'
-  AND m.startgrade = 10
---mathematics 12
-UNION ALL
-SELECT m.subject
-      ,12 AS startgrade
-      ,m.startrit
-      ,m.t41, m.t42, m.t44, back_1.t22, m.t12
-      ,m.r41, m.r42, m.r44, back_1.r22, m.r12
-      ,m.s41, m.s42, m.s44, back_1.s22, m.s12
-FROM master_growth_norms m
-JOIN master_growth_norms back_1 
-  ON m.subject = back_1.subject
- AND m.startrit = back_1.startrit
- AND m.startgrade - 1 = back_1.startgrade
-WHERE m.subject = 'Mathematics'
-  AND m.startgrade = 10
---language usage 11
-UNION ALL
-SELECT m.subject
-      ,11 AS startgrade
-      ,m.startrit
-      ,m.t41, m.t42, m.t44, back_1.t22, m.t12
-      ,m.r41, m.r42, m.r44, back_1.r22, m.r12
-      ,m.s41, m.s42, m.s44, back_1.s22, m.s12
-FROM master_growth_norms m
-JOIN master_growth_norms back_1 
-  ON m.subject = back_1.subject
- AND m.startrit = back_1.startrit
- AND m.startgrade - 1 = back_1.startgrade
-WHERE m.subject = 'Language Usage'
-  AND m.startgrade = 10
---language usage 12
-UNION ALL
-SELECT m.subject
-      ,12 AS startgrade
-      ,m.startrit
-      ,m.t41, m.t42, m.t44, back_1.t22, m.t12
-      ,m.r41, m.r42, m.r44, back_1.r22, m.r12
-      ,m.s41, m.s42, m.s44, back_1.s22, m.s12
-FROM master_growth_norms m
-JOIN master_growth_norms back_1 
-  ON m.subject = back_1.subject
- AND m.startrit = back_1.startrit
- AND m.startgrade - 1 = back_1.startgrade
-WHERE m.subject = 'Language Usage'
-  AND m.startgrade = 10
---general science 11
-UNION ALL
-SELECT m.subject
-      ,11 AS startgrade
-      ,m.startrit
-      ,m.t41, m.t42, m.t44, back_1.t22, m.t12
-      ,m.r41, m.r42, m.r44, back_1.r22, m.r12
-      ,m.s41, m.s42, m.s44, back_1.s22, m.s12
-FROM master_growth_norms m
-JOIN master_growth_norms back_1 
-  ON m.subject = back_1.subject
- AND m.startrit = back_1.startrit
- AND m.startgrade - 1 = back_1.startgrade
-WHERE m.subject = 'General Science'
-  AND m.startgrade = 10
---general science 12
-UNION ALL
-SELECT m.subject
-      ,12 AS startgrade
-      ,m.startrit
-      ,m.t41, m.t42, m.t44, back_1.t22, m.t12
-      ,m.r41, m.r42, m.r44, back_1.r22, m.r12
-      ,m.s41, m.s42, m.s44, back_1.s22, m.s12
-FROM master_growth_norms m
-JOIN master_growth_norms back_1 
-  ON m.subject = back_1.subject
- AND m.startrit = back_1.startrit
- AND m.startgrade - 1 = back_1.startgrade
-WHERE m.subject = 'General Science'
-  AND m.startgrade = 10
+WHERE m.startgrade = 10
