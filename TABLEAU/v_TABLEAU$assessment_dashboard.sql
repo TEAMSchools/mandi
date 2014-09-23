404141a7e8b0984f677d24f650bf6b28ff0bca25
cbini <cbini87@gmail.com>
2014-09-23 11:57:04 -0400

    added course and section

--- TABLEAU/v_TABLEAU$assessment_dashboard.sql
+++ TABLEAU/v_TABLEAU$assessment_dashboard.sql
@@ -18,7 +18,44 @@ WITH distinct_assessments AS (
   FROM ILLUMINATE$assessments#static WITH(NOLOCK)  
  )
 
+,enrollments AS (
+  SELECT cc.STUDENTID
+        ,cc.TERMID
+        ,cou.CREDITTYPE
+        ,cc.COURSE_NUMBER      
+        ,cou.COURSE_NAME
+        ,cc.SECTION_NUMBER
+        ,cc.SECTIONID      
+        ,CASE        
+          WHEN cc.SCHOOLID != 73253 THEN cc.EXPRESSION
+          WHEN cc.expression = '1(A)' THEN 'HR'
+          WHEN cc.expression = '2(A)' THEN '1'
+          WHEN cc.expression = '3(A)' THEN '2'
+          WHEN cc.expression = '4(A)' THEN '3'
+          WHEN cc.expression = '5(A)' THEN '4A'
+          WHEN cc.expression = '6(A)' THEN '4B'
+          WHEN cc.expression = '7(A)' THEN '4C'
+          WHEN cc.expression = '8(A)' THEN '4D'
+          WHEN cc.expression = '9(A)' THEN '5A'
+          WHEN cc.expression = '10(A)' THEN '5B'
+          WHEN cc.expression = '11(A)' THEN '5C'
+          WHEN cc.expression = '12(A)' THEN '5D'
+          WHEN cc.expression = '13(A)' THEN '6'
+          WHEN cc.expression = '14(A)' THEN '7'       
+         END AS period
+        ,ROW_NUMBER() OVER(
+           PARTITION BY cc.studentid, cou.credittype
+             ORDER BY cc.termid DESC, cc.course_number DESC) AS rn
+   FROM CC WITH(NOLOCK)
+   JOIN COURSES cou WITH(NOLOCK)
+    ON cc.COURSE_NUMBER = cou.COURSE_NUMBER
+   AND cou.CREDITTYPE IS NOT NULL
+   AND cou.CREDITTYPE NOT IN ('LOG', 'PHYSED', 'STUDY')
+   WHERE cc.TERMID >= dbo.fn_Global_Term_Id()
+ )
+
 SELECT co.schoolid
+      ,co.year AS academic_year
       ,co.grade_level
       ,s.team
       --,grp.group_name
@@ -29,6 +66,8 @@ SELECT co.schoolid
       ,a.scope            
       ,a.subject      
       ,a.credittype      
+      ,enr.COURSE_NAME
+      ,CASE WHEN co.schoolid = 73253 THEN enr.period ELSE enr.SECTION_NUMBER END AS section
       ,a.term
       ,a.administered_at
       ,CONVERT(VARCHAR,a.standards_tested) AS standards_tested
@@ -50,6 +89,10 @@ LEFT OUTER JOIN CUSTOM_STUDENTS cs WITH(NOLOCK)
 JOIN distinct_assessments a WITH (NOLOCK)
   ON res.assessment_id = a.assessment_id
  AND res.standard_id = a.standard_id  
+LEFT OUTER JOIN enrollments enr WITH(NOLOCK)
+  ON co.studentid = enr.studentid
+ AND a.credittype = enr.credittype
+ AND enr.rn = 1
 --LEFT OUTER JOIN ILLUMINATE$student_groups#static grp WITH(NOLOCK)
 --  ON co.STUDENT_NUMBER = grp.student_number
 -- AND res.academic_year = grp.academic_year
