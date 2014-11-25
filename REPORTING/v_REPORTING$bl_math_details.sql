d74e6af9f1466b7d06a2b0a8516185eaf7f98972
unknown <ldesimon@rm9-ldesimon1.teamschools.kipp.org>
2014-11-25 09:50:47 -0500

    new data feed for google doc bl dashboard, consisting of core roster and MAP data

--- REPORTING/v_REPORTING$bl_math_details.sql
+++ REPORTING/v_REPORTING$bl_math_details.sql
@@ -1,175 +1,159 @@
+--this view is used for the core data feed (consiting of roster, MAP info) in Rise/TEAM blended learning google doc dashboards
+
 USE KIPP_NJ
 GO
 
---ALTER VIEW REPORTING$bl_math_details AS
+--ALTER VIEW REPORTING$blended_learning_details AS
 
 WITH
 
---roster of students
+	--roster of students
  
 	roster AS
 	(
-      SELECT s.student_number AS base_student_number
-            ,s.id AS base_studentid
-            ,s.lastfirst AS stu_lastfirst
-            ,s.first_name AS stu_firstname
-            ,s.last_name AS stu_lastname
-            ,c.grade_level AS stu_grade_level
-            ,s.team AS travel_group            
-      FROM KIPP_NJ..COHORT$comprehensive_long#static c  WITH (NOLOCK)
-      JOIN KIPP_NJ..STUDENTS s WITH (NOLOCK)
-        ON c.studentid = s.id
-       AND s.enroll_status = 0       
-      
-	  --reportingdates
-	  
-	  WHERE year = 2014 -- dbo.fn_Global_Academic_Year()
-        AND c.rn = 1        
-        AND c.schoolid = 73252 
-	--	AND c.studentid = 5778       
+      SELECT c.student_number	AS base_student_number
+            ,c.studentid		AS base_studentid
+            ,c.lastfirst		AS stu_lastfirst
+            ,c.first_name		AS stu_firstname
+            ,c.last_name		AS stu_lastname
+            ,c.grade_level		AS stu_grade_level
+			,c.team				AS travel_group     
+			,c.spedlep			AS SPED
+			,c.gender      
+			,c.school_name		AS school
+      FROM KIPP_NJ..COHORT$identifiers_long#static c  WITH (NOLOCK)        
+	  WHERE year = 2014
+        AND c.enroll_status = 0
+		AND c.grade_level >= 5
+		AND c.grade_level <= 8
 	)
 
---ST Math data
-
-   ,stmath AS
+	,grades_math AS
 	(
-	  SELECT sub.*
-	  FROM
-		(
-		  SELECT stmath.student_number
-				,stmath.studentid
-				,ROUND(stmath.K_5_Mastery,0) AS K_5_Mastery
-				,ROUND(stmath.K_5_Progress,0) AS K_5_Progress
-				,stmath.objective_name
-				,stmath.cur_hurdle_num_tries
-				,stmath.teacher_name
-				,stmath.gcd
-				,CASE WHEN stmath.gcd = 'Kindergarten'	THEN 0
-					  WHEN stmath.gcd = 'First Grade'	THEN 1
-					  WHEN stmath.gcd = 'Second Grade'	THEN 2
-					  WHEN stmath.gcd = 'Third Grade'	THEN 3
-					  WHEN stmath.gcd = 'Fourth Grade'	THEN 4
-					  WHEN stmath.gcd = 'Fifth Grade'	THEN 5
-					  WHEN stmath.gcd = 'Sixth Grade'	THEN 6
-					  WHEN stmath.gcd = 'Seventh Grade' THEN 7
-					  WHEN stmath.gcd = 'Eigth Grade'	THEN 8
-					  WHEN stmath.gcd = 'Ninth Grade'	THEN 9
-					  WHEN stmath.gcd = 'Secondary Intervention' THEN 6
-				 ELSE stmath.gcd END AS stmath_level
-				,stmath.num_lab_logins
-				,stmath.fluency_time_spent
-				,stmath.num_homework_logins
-				,stmath.first_login_date
-				,stmath.last_login_date
-				,stmath.minutes_logged_last_week
-				,ROW_NUMBER() OVER
-						   (PARTITION BY stmath.student_number ORDER BY stmath.K_5_Progress DESC )
-							AS progress_order 
-		 FROM STMath..progress_completion#identifiers stmath WITH (NOLOCK)
-		)sub
-	 WHERE sub.progress_order = 1
+      SELECT grades_math.*
+			,sections.section_number
+      FROM KIPP_NJ..GRADES$DETAIL#MS grades_math  WITH (NOLOCK)
+	  JOIN KIPP_NJ..sections sections WITH (NOLOCK)
+	    ON grades_math.T1_ENR_SECTIONID = sections.id     
+	  WHERE grades_math.schoolid IN (73252,133570965) 
+  	    AND grades_math.credittype = 'MATH'
 	)
 
---Khan data
-
-    ,khan AS
+	,grades_sci AS
 	(
-	  SELECT sub.studentid
-            ,sub.lastfirst
-            ,COUNT(*) AS mastered
-      FROM
-			(SELECT e.studentid
-					,s.lastfirst
-					,s.first_name + ' ' + s.last_name AS full_name
-					,s.grade_level
-					,e.exercise
-					,e.proficient_date
-					,e.level
-					,e.total_done
-					,e.total_correct
-				FROM Khan..composite_exercises#identifiers e WITH (NOLOCK)
-				JOIN KIPP_NJ..STUDENTS s WITH (NOLOCK)
-				ON e.studentid = s.id               
-			WHERE e.mastered = 'True'
-				) sub
-    GROUP BY sub.studentid
-            ,sub.lastfirst
-	)
+      SELECT grades_sci.*
+			,sections.section_number
+      FROM KIPP_NJ..GRADES$DETAIL#MS grades_sci  WITH (NOLOCK)
+	  JOIN KIPP_NJ..sections sections WITH (NOLOCK)
+	    ON grades_sci.T1_ENR_SECTIONID = sections.id     
+	  WHERE grades_sci.schoolid IN (73252,133570965) 
+  	    AND grades_sci.credittype = 'SCI'
+  	)
 
+	--map math data
 
---Accelerated Math
-   
-	,am AS
+	,map_math AS
 	(
-	SELECT am.studentid
-		,am.grade_level
-		,am.year
-		,am.objectives
-		,am.avg_grade_level
-		,am.date
-				
-	FROM KIPP_NJ..AM$objectives_mastered#long#static am WITH (NOLOCK)
-		    
-	WHERE am.year = 2013
-	  AND am.date = '2014-06-20'
+	SELECT map_math.*
+	FROM TABLEAU$MAP_tracker map_math WITH (NOLOCK)
+	WHERE map_math.measurementscale LIKE 'Math%' 
+	 AND map_math.year IN (2014)
+	 AND map_math.fallwinterspring = 'Fall'
 	)
 
---map data
 
-	,map AS
+	--map science data
+
+	,map_sci AS
 	(
-	SELECT map.*
+	SELECT map_sci.*
+	FROM TABLEAU$MAP_tracker map_sci WITH (NOLOCK)
 
-	FROM MAP$comprehensive#identifiers map WITH (NOLOCK)
-
-	WHERE map.rn_curr = 1
-	  AND map.map_year IN (2014)
-	  AND map.discipline LIKE 'Math%'
-
+	WHERE map_sci.measurementscale LIKE 'Sci%' 
+	 AND map_sci.year IN (2014)
+	 AND map_sci.fallwinterspring = 'Fall'
 	)
+
+
+	--map reading data
+
+	,map_read AS
+	(
+	SELECT map_read.*
+	FROM TABLEAU$MAP_tracker map_read WITH (NOLOCK)
+
+	WHERE map_read.measurementscale LIKE 'Read%' 
+	 AND map_read.year IN (2014)
+	 AND map_read.fallwinterspring = 'Fall'
+	)
+
+	--map language data
+
+	,map_lang AS
+	(
+	SELECT map_lang.*
+	FROM TABLEAU$MAP_tracker map_lang WITH (NOLOCK)
+
+	WHERE map_lang.measurementscale LIKE 'lang%' 
+	 AND map_lang.year IN (2014)
+	 AND map_lang.fallwinterspring = 'Fall'
+	)
+
 
 --select fields for view
 
-SELECT roster.stu_lastfirst
-	  ,roster.stu_grade_level
-	  ,roster.base_student_number
-	  ,roster.base_studentid
-	  
-	  ,stmath.stmath_level
-      ,stmath.K_5_Progress AS stmath_progress
-	  ,CASE WHEN stmath.K_5_Progress > 0 THEN 1 
-			ELSE NULL 
-			END AS stmath_valid
-	  ,CASE WHEN stmath.K_5_Progress = 100 THEN 1 
-			ELSE NULL 
-			END AS stmath_achieved
-	  
-	  ,khan.mastered AS khan_obj
-	  ,CASE WHEN roster.stu_grade_level = 6 AND khan.mastered >= 100 THEN 1 
-			WHEN roster.stu_grade_level = 7 AND khan.mastered >= 200 THEN 1 
-			WHEN roster.stu_grade_level = 8 AND khan.mastered >= 200 THEN 1  
-			ELSE NULL 
-			END AS khan_achieved
-	  
-	  ,am.objectives AS am_obj
-	  
-	  ,map.testritscore AS map_rit
-	  ,map.testpercentile AS map_pctl
---	  ,map.discipline
---	  ,map.fallwinterspring
---	  ,map.map_year
+SELECT 
+	   roster.base_student_number	AS ID
+	  ,roster.stu_lastfirst			AS LastFirst
+	  ,roster.stu_grade_level		AS GR
+   	  ,roster.sped					AS SPED
+	  ,roster.travel_group			AS Travel
+	  ,roster.gender				AS MF
+
+	  ,grades_math.section_number	AS math_sec
+	  ,grades_math.Y1				AS math_y1
+	  ,grades_sci.section_number	AS sci_sec
+	  ,grades_sci.Y1				AS sci_y1
+
+	  ,roster.school			
+
+	  ,map_math.base_rit			AS  math_rit
+	  ,map_math.base_pct			AS	math_pctile
+	  ,map_math.keep_up_rit			AS	math_ku	
+	  ,map_math.rutgers_ready_rit	AS	math_rr
+
+	  ,map_sci.base_rit				AS  sci_rit
+	  ,map_sci.base_pct				AS	sci_pctile
+	  ,map_sci.keep_up_rit			AS	sci_ku
+	  ,map_sci.rutgers_ready_rit	AS	sci_rr
+
+	  ,map_read.base_rit			AS  read_rit
+	  ,map_read.base_pct			AS	read_pctile
+	  ,map_read.keep_up_rit			AS	read_ku
+	  ,map_read.rutgers_ready_rit	AS	read_rr
+
+	  ,map_lang.base_rit			AS  lang_rit
+	  ,map_lang.base_pct			AS	lang_pctile
+	  ,map_lang.keep_up_rit			AS	lang_ku	
+	  ,map_lang.rutgers_ready_rit	AS	lang_rr
 	
 	
 FROM roster 
 
-LEFT OUTER JOIN stmath
-  ON roster.base_student_number = stmath.student_number
+LEFT OUTER JOIN map_math
+  ON roster.base_studentid = map_math.studentid
 
-LEFT OUTER JOIN khan
-  ON roster.base_studentid = khan.studentid
+LEFT OUTER JOIN map_sci
+  ON roster.base_studentid = map_sci.studentid
 
-LEFT OUTER JOIN am
-  ON roster.base_studentid = am.studentid
+LEFT OUTER JOIN map_read
+  ON roster.base_studentid = map_read.studentid
 
-LEFT OUTER JOIN map
-  ON roster.base_student_number = map.studentid
+LEFT OUTER JOIN map_lang
+  ON roster.base_studentid = map_lang.studentid
+
+LEFT OUTER JOIN grades_math
+  ON roster.base_studentid = grades_math.studentid
+
+LEFT OUTER JOIN grades_sci
+  ON roster.base_studentid = grades_sci.studentid
