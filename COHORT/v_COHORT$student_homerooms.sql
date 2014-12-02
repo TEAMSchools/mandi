351a0062d48dfaad25d3ba7af6121ca075a20845
almartin82 <almartin@gmail.com>
2014-12-02 10:46:41 -0500

    one row per student per year

---
+++ COHORT/v_COHORT$student_homerooms.sql
@@ -0,0 +1,27 @@
+USE KIPP_NJ
+GO
+
+ALTER VIEW COHORT$student_homerooms AS
+WITH hr_rost AS
+    (SELECT KIPP_NJ.dbo.fn_TermToYear(sect.termid) AS year
+          ,cc.studentid
+          ,cc.dateenrolled
+          ,sch.abbreviation
+          ,t.first_name + ' ' + t.last_name AS teacher
+          ,sect.section_number
+    FROM KIPP_NJ..SECTIONS sect
+    JOIN KIPP_NJ..SCHOOLS sch
+      ON sect.schoolid = sch.school_number
+    JOIN KIPP_NJ..CC
+      ON sect.id = cc.sectionid
+    JOIN KIPP_NJ..TEACHERS t
+      ON sect.teacher = t.id
+    WHERE sect.course_number = 'HR'
+    )
+SELECT hr_rost.*
+      ,ROW_NUMBER() OVER
+        (PARTITION BY studentid
+                     ,year
+         ORDER BY dateenrolled
+        ) AS rn_stu_year
+FROM hr_rost
