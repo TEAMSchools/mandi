365a9badf5025ac01e8f03a5cd2398c6934d86fe
cbini <cbini87@gmail.com>
2015-01-12 15:05:03 -0500

    moved choices feed to server, AR goals script now a view

---
+++ AR/v_AR$goal_setting#NCA.sql
@@ -0,0 +1,117 @@
+USE KIPP_NJ
+GO
+
+ALTER VIEW AR$goal_setting#NCA AS
+
+WITH roster AS (
+  -- NCA
+  SELECT co.student_number      
+        ,co.studentid
+        ,co.lastfirst      
+        ,co.grade_level
+        ,co.advisor
+        ,terms.alt_name AS term 
+  FROM KIPP_NJ..COHORT$identifiers_long#static co WITH(NOLOCK)
+  JOIN KIPP_NJ..REPORTING$dates terms WITH(NOLOCK) 
+    ON co.schoolid = terms.schoolid
+   AND co.year = terms.academic_year
+   AND terms.identifier IN ('HEX', 'RT_IR')
+  WHERE co.year = KIPP_NJ.dbo.fn_Global_Academic_Year()
+    AND co.schoolid = 73253    
+    AND co.rn = 1
+ )
+
+,lexile AS (
+  SELECT lex.studentid
+        ,terms.term
+        ,CONVERT(INT,REPLACE(lex.lexile_score, 'BR', 0)) AS lexile_score
+  FROM KIPP_NJ..MAP$best_baseline#static lex WITH(NOLOCK)
+  JOIN (
+        SELECT 'Q1' term
+        UNION
+        SELECT 'Q2'
+       ) terms
+    ON 1 = 1
+  WHERE lex.year = KIPP_NJ.dbo.fn_Global_Academic_Year()
+    AND lex.measurementscale = 'Reading'
+  
+  UNION ALL
+
+  SELECT base.studentid
+        ,terms.term
+        ,CASE        
+          WHEN CONVERT(INT,REPLACE(map.rittoreadingscore, 'BR', 0)) > CONVERT(INT,REPLACE(base.lexile_score, 'BR', 0)) THEN CONVERT(INT,REPLACE(map.rittoreadingscore, 'BR', 0))
+          ELSE CONVERT(INT,REPLACE(base.lexile_score, 'BR', 0))
+         END AS lexile_score
+  FROM KIPP_NJ..MAP$best_baseline#static base WITH(NOLOCK)
+  LEFT OUTER JOIN KIPP_NJ..MAP$comprehensive#identifiers map WITH(NOLOCK)
+    ON base.studentid = map.ps_studentid
+   AND base.year = map.map_year_academic
+   AND base.measurementscale = map.measurementscale
+   AND map.fallwinterspring = 'Winter'
+   AND map.rn = 1
+  JOIN (
+        SELECT 'Q3' term
+        UNION
+        SELECT 'Q4'
+       ) terms
+    ON 1 = 1
+  WHERE base.year = KIPP_NJ.dbo.fn_Global_Academic_Year()
+    AND base.measurementscale = 'Reading'
+ )
+
+,google_doc AS (  
+  SELECT student_number
+        ,CONVERT(INT,points_goal) AS points_goal
+        ,term        
+        ,ROW_NUMBER() OVER(
+           PARTITION BY student_number, term
+             ORDER BY term) AS dupe_check
+  FROM [KIPP_NJ].[dbo].[AUTOLOAD$GDOCS_AR_NCA] WITH(NOLOCK)
+  WHERE student_number IS NOT NULL  
+ )
+
+,goals_long AS (
+  SELECT r.student_number
+        ,r.lastfirst
+        ,r.grade_level
+        ,r.advisor
+        ,r.term
+        ,lex.lexile_score      
+        ,CASE WHEN r.term IN ('Q3','Q4') THEN goal.points_goal ELSE gdoc.points_goal END AS points_goal
+  FROM roster r WITH(NOLOCK)
+  JOIN lexile lex WITH(NOLOCK)
+    ON r.studentid = lex.studentid
+   AND r.term = lex.term
+  LEFT OUTER JOIN KIPP_NJ..AR$tier_goals#HS goal WITH(NOLOCK)
+    ON lex.lexile_score >= goal.lexile_min 
+   AND lex.lexile_score <= goal.lexile_max
+  LEFT OUTER JOIN google_doc gdoc WITH(NOLOCK)
+    ON r.student_number = gdoc.student_number
+   AND r.term = gdoc.term
+   AND gdoc.dupe_check = 1  
+ )
+
+SELECT student_number
+      ,lastfirst
+      ,grade_level
+      ,ADVISOR
+      ,term
+      ,lexile_score
+      ,points_goal
+FROM goals_long
+
+UNION ALL
+
+SELECT student_number
+      ,lastfirst
+      ,grade_level
+      ,ADVISOR
+      ,'Y1' AS term
+      ,MAX(lexile_score) AS lexile_score
+      ,SUM(points_goal) AS points_goal
+FROM goals_long
+GROUP BY student_number
+        ,lastfirst
+        ,grade_level
+        ,ADVISOR
---
+++ REPORTING/v_REPORTING$choices_feed#NCA.sql
@@ -0,0 +1,27 @@
+USE KIPP_NJ
+GO
+
+ALTER VIEW REPORTING$choices_feed#NCA AS
+
+SELECT s.LASTFIRST AS Name
+      ,s.GRADE_LEVEL AS Gr
+      ,s.Advisor      
+      ,dates.alt_name AS Term
+      ,CONVERT(VARCHAR,disc.entry_date,101) AS [Log Date]
+      ,disc.entry_author AS [Log Author]
+      ,disc.logtype AS [Type]
+      ,disc.Subtype
+      ,disc.rn AS [Count]
+FROM KIPP_NJ..COHORT$identifiers_long#static s WITH (NOLOCK)
+JOIN KIPP_NJ..DISC$log#static disc WITH(NOLOCK)
+  ON s.studentid = disc.studentid
+ AND disc.logtype IS NOT NULL
+JOIN KIPP_NJ..REPORTING$dates dates WITH(NOLOCK)
+  ON disc.entry_date >= dates.start_date
+ AND disc.entry_date <= dates.end_date
+ AND dates.identifier = 'RT'
+ AND s.SCHOOLID = dates.schoolid
+WHERE s.SCHOOLID = 73253
+  AND s.ENROLL_STATUS = 0
+  AND s.year = KIPP_NJ.dbo.fn_Global_Academic_Year()
+  AND s.rn = 1
