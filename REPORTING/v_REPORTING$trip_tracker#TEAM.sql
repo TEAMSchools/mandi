c31528e751899e36961cd8b3ca8a710c53ea49be
cbini <cbini87@gmail.com>
2015-01-14 17:28:35 -0500

    optimizations

--- REPORTING/v_REPORTING$trip_tracker#TEAM.sql
+++ REPORTING/v_REPORTING$trip_tracker#TEAM.sql
@@ -9,7 +9,7 @@ SELECT s.student_number AS sn
       ,s.last_name AS lastname 
       ,s.grade_level AS stu_grade_level 
       ,s.team AS travel_group 
-      ,cs.ADVISOR
+      ,s.ADVISOR
       ,ROUND(team_gpa.gpa_y1_all,2) AS gpa_y1_all 
       ,ROUND(team_gpa.gpa_y1_core,2) AS gpa_y1_core 
       ,promo.promo_att_team AS promo_status_att
@@ -30,21 +30,21 @@ SELECT s.student_number AS sn
         + ISNULL(CONVERT(VARCHAR, incidents.DISC_04_DATE_REPORTED, 101) + '_', '') + ISNULL(incidents.DISC_04_SUBJECT, '') + ' '
         + ISNULL(CONVERT(VARCHAR, incidents.DISC_05_DATE_REPORTED, 101) + '_', '') + ISNULL(incidents.DISC_05_SUBJECT, '')
         AS list_of_infractions
-FROM students s WITH(NOLOCK)
-LEFT OUTER JOIN CUSTOM_STUDENTS cs WITH(NOLOCK)
-  ON s.id = cs.STUDENTID
-LEFT OUTER JOIN DISC$counts_wide counts WITH(NOLOCK)
-  ON s.id = counts.studentid
-LEFT OUTER JOIN DISC$recent_incidents_wide incidents WITH(NOLOCK)
-  ON s.id = incidents.studentid
-LEFT OUTER JOIN REPORTING$promo_status#MS promo WITH(NOLOCK)
-  ON s.id = promo.studentid  
-LEFT OUTER JOIN GPA$detail#MS team_gpa WITH(NOLOCK)
-  ON s.id = team_gpa.studentid
-LEFT OUTER JOIN GRADES$ELEMENTS hw WITH(NOLOCK)
-  ON s.id = hw.studentid
+FROM KIPP_NJ..COHORT$identifiers_long#static s WITH(NOLOCK)
+LEFT OUTER JOIN KIPP_NJ..DISC$counts_wide counts WITH(NOLOCK)
+  ON s.studentid = counts.studentid
+LEFT OUTER JOIN KIPP_NJ..DISC$recent_incidents_wide incidents WITH(NOLOCK)
+  ON s.studentid = incidents.studentid
+LEFT OUTER JOIN KIPP_NJ..REPORTING$promo_status#MS promo WITH(NOLOCK)
+  ON s.studentid = promo.studentid  
+LEFT OUTER JOIN KIPP_NJ..GPA$detail#MS team_gpa WITH(NOLOCK)
+  ON s.studentid = team_gpa.studentid
+LEFT OUTER JOIN KIPP_NJ..GRADES$ELEMENTS hw WITH(NOLOCK)
+  ON s.studentid = hw.studentid
  AND hw.pgf_type = 'H'
  AND hw.course_number = 'all_courses'
  AND hw.yearid = LEFT(dbo.fn_Global_Term_Id(), 2)
-WHERE s.schoolid = 133570965
+WHERE s.year = dbo.fn_Global_Academic_Year()
+  AND s.schoolid = 133570965  
   AND s.enroll_status = 0
+  AND s.rn = 1
