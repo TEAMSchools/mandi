70c60465b76c681756572ffb0c4fb3e33442fbf3
cbini <cbini87@gmail.com>
2015-02-25 11:04:49 -0500

    weekday_sun is the first day of the week, there was a messed up join on time_series_dense that employed it as the last day of the week

--- UTIL/v_UTIL$reporting_days_weeks.sql
+++ UTIL/v_UTIL$reporting_days_weeks.sql
@@ -1,6 +1,8 @@
 USE KIPP_NJ
 GO
+
 ALTER VIEW UTIL$reporting_weeks_days AS
+
 SELECT reporting_hash
       ,year
       ,academic_year
@@ -9,6 +11,7 @@ SELECT reporting_hash
       ,MAX(weekday_sun) AS weekday_sun
       ,MAX(weekday_start) AS weekday_start
       ,MAX(weekday_end) AS weekday_end      
+      ,MAX(weekday_sat) AS weekday_sat
 FROM
     (
      SELECT w.reporting_hash
@@ -21,17 +24,21 @@ FROM
            ,d_start.date AS weekday_start
            ,d_end.date AS weekday_end
            ,d_sun.date AS weekday_sun
-     FROM UTIL$reporting_weeks w WITH(NOLOCK)
+           ,d_sat.date AS weekday_sat
+     FROM KIPP_NJ..UTIL$reporting_weeks w WITH(NOLOCK)
      --last day of week
-     JOIN UTIL$reporting_days#static d_start WITH(NOLOCK)
+     JOIN KIPP_NJ..UTIL$reporting_days#static d_start WITH(NOLOCK)
        ON w.reporting_hash = d_start.reporting_hash
       AND d_start.day_of_week = 'Monday'
-     JOIN UTIL$reporting_days#static d_end WITH(NOLOCK)
+     JOIN KIPP_NJ..UTIL$reporting_days#static d_end WITH(NOLOCK)
        ON w.reporting_hash = d_end.reporting_hash
       AND d_end.day_of_week = 'Friday'
-     JOIN UTIL$reporting_days#static d_sun WITH(NOLOCK)
+     JOIN KIPP_NJ..UTIL$reporting_days#static d_sun WITH(NOLOCK)
        ON w.reporting_hash = d_sun.reporting_hash
       AND d_sun.day_of_week = 'Sunday'
+     JOIN KIPP_NJ..UTIL$reporting_days#static d_sat WITH(NOLOCK)
+       ON w.reporting_hash = d_sat.reporting_hash
+      AND d_sat.day_of_week = 'Saturday'
      ) sub
 GROUP BY reporting_hash
         ,year
