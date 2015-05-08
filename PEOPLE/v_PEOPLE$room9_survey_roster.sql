2077f732d07b7eb4a29270c1ddde33f20fdda173
Charlie Bini <cbini87@gmail.com>
2015-05-08 15:18:05 -0400

    asdf

--- PEOPLE/v_PEOPLE$room9_survey_roster.sql
+++ PEOPLE/v_PEOPLE$room9_survey_roster.sql
@@ -4,10 +4,12 @@ GO
 ALTER VIEW PEOPLE$room9_survey_roster AS
 
 SELECT COALESCE(ad.displayName, CONCAT(adp.first_name, ' ', adp.last_name)) AS name
+      ,first_name
       ,ad.mail AS email_addr      
       ,adp.location
       ,adp.department
       ,adp.job_title
+      ,adp.grade_taught
       ,adp.position_status
 FROM KIPP_NJ..PEOPLE$ADP_detail adp
 LEFT OUTER JOIN KIPP_NJ..PEOPLE$AD_users#static ad
@@ -15,4 +17,3 @@ LEFT OUTER JOIN KIPP_NJ..PEOPLE$AD_users#static ad
  AND ad.is_student = 0
  AND ad.is_active = 1
 WHERE adp.position_status = 'Active'
---ORDER BY location, last_name
