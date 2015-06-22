1bfaef847b0bfa8ab9e25f61dcfba8662f2c8853
almartin82 <almartin@gmail.com>
2015-06-22 11:59:06 -0400

    new responses from khan api

--- Khan/v_exercise_states#identifiers.sql
+++ Khan/v_exercise_states#identifiers.sql
@@ -2,14 +2,28 @@ USE Khan
 GO
 
 ALTER VIEW exercise_states#identifiers AS
+WITH states_munge AS 
+    (SELECT b.*,
+           REPLACE(stu_detail.identity_email, '@teamstudents.org', '') AS cleaned_email
+     FROM Khan..exercise_states b  WITH(NOLOCK)
+     JOIN Khan..stu_detail  WITH(NOLOCK)
+       ON b.student = stu_detail.student 
+    ),
+    add_id AS
+    (SELECT m.*
+           ,REPLACE(SUBSTRING(m.cleaned_email, 1, CHARINDEX(',', m.cleaned_email)-1) , 'norm:', '') + '.student' AS id_key
+     FROM states_munge m
+    )
 SELECT s.id AS studentid
-      ,sub.*
-FROM
-      (SELECT b.*
-             ,REPLACE(stu_detail.identity_email, '@teamstudents.org', '') + '.student' AS id_key
-       FROM Khan..exercise_states b  WITH(NOLOCK)
-       JOIN Khan..stu_detail  WITH(NOLOCK)
-         ON b.student = stu_detail.student 
-       ) sub
+      ,add_id.*
+FROM add_id
 JOIN KIPP_NJ..STUDENTS s WITH(NOLOCK)
-  ON sub.id_key = s.student_web_id
+  ON add_id.id_key = s.student_web_id
+
+
+
+
+
+
+
+
