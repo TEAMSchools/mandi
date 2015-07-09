093797b4a3d99b5b25fe13cd4c43271305476c47
Charlie Bini <cbini87@gmail.com>
2015-07-09 12:59:30 -0400

    new student login query for automation and prosperity

---
+++ PS/sp/sp_PS$SCHOOLS#static_refresh.sql
@@ -0,0 +1,64 @@
+USE [KIPP_NJ]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+                  
+ALTER PROCEDURE [sp_PS$SCHOOLS#static|refresh] AS
+BEGIN
+
+  DECLARE @sql AS VARCHAR(MAX)='';
+
+  -- STEP 1: make sure no temp table
+		IF OBJECT_ID(N'tempdb..#PS$SCHOOLS#static|refresh') IS NOT NULL
+		BEGIN
+						DROP TABLE [#PS$SCHOOLS#static|refresh]
+		END
+
+
+  -- STEP 2: load into a temporary staging table.
+  SELECT *
+		INTO [#PS$SCHOOLS#static|refresh]
+  FROM PS$SCHOOLS;
+         
+
+  -- STEP 3: truncate destination table
+  EXEC('TRUNCATE TABLE KIPP_NJ..PS$SCHOOLS#static');
+
+
+  -- STEP 4: disable all nonclustered indexes on table
+  SELECT @sql = @sql + 'ALTER INDEX ' 
+                 + indexes.name + ' ON dbo.' 
+                 + objects.name + ' DISABLE;' + CHAR(13) + CHAR(10)
+  FROM sys.indexes
+  JOIN sys.objects 
+    ON sys.indexes.object_id = sys.objects.object_id
+  WHERE sys.indexes.type_desc = 'NONCLUSTERED'
+    AND sys.objects.type_desc = 'USER_TABLE'
+    AND sys.objects.name = 'PS$SCHOOLS#static';
+  EXEC (@sql);
+
+
+  -- STEP 5: insert into final destination
+  INSERT INTO [PS$SCHOOLS#static]
+  SELECT *
+  FROM [#PS$SCHOOLS#static|refresh];
+ 
+
+  -- STEP 6: rebuld all nonclustered indexes on table
+  SELECT @sql = @sql + 'ALTER INDEX ' 
+                  + indexes.name + ' ON dbo.' 
+                  + objects.name + ' REBUILD;' + CHAR(13) + CHAR(10)
+  FROM sys.indexes
+  JOIN sys.objects 
+    ON sys.indexes.object_id = sys.objects.object_id
+  WHERE sys.indexes.type_desc = 'NONCLUSTERED'
+    AND sys.objects.type_desc = 'USER_TABLE'
+    AND sys.objects.name = 'PS$SCHOOLS#static';
+  EXEC (@sql);
+  
+END                  
---
+++ ROSTERS/v_ROSTERS$PS_access_accounts.sql
@@ -0,0 +1,89 @@
+USE KIPP_NJ
+GO
+
+ALTER VIEW ROSTERS$PS_access_accounts AS 
+
+WITH clean_names AS (
+  SELECT s.student_number
+        ,sch.ABBREVIATION AS school_name
+        ,s.grade_level
+        ,s.FIRST_NAME
+        ,s.LAST_NAME
+        ,KIPP_NJ.dbo.REMOVESPECIALCHARS(LOWER(s.FIRST_NAME)) AS first_name_clean
+        ,LEFT(LOWER(s.first_name),1) AS first_init           
+        ,KIPP_NJ.dbo.REMOVESPECIALCHARS(LOWER(
+           CASE
+            WHEN s.LAST_NAME LIKE 'St %' OR s.LAST_NAME LIKE 'St. %' THEN s.LAST_NAME
+            WHEN s.LAST_NAME LIKE '% II%' THEN LEFT(s.LAST_NAME,CHARINDEX(' I',s.LAST_NAME) - 1)
+            WHEN CHARINDEX('-',s.LAST_NAME) + CHARINDEX(' ',s.LAST_NAME) = 0 THEN REPLACE(s.LAST_NAME, ' JR', '')
+            WHEN CHARINDEX(' ',s.LAST_NAME) > 0 AND CHARINDEX('-',s.LAST_NAME) > 0 AND CHARINDEX(' ',s.LAST_NAME) < CHARINDEX('-',s.LAST_NAME) THEN LEFT(s.LAST_NAME,CHARINDEX(' ',s.LAST_NAME) - 1)        
+            WHEN CHARINDEX('-',s.LAST_NAME) > 0 AND CHARINDEX(' ',s.LAST_NAME) > 0 AND CHARINDEX('-',s.LAST_NAME) < CHARINDEX(' ',s.LAST_NAME) THEN LEFT(s.LAST_NAME,CHARINDEX('-',s.LAST_NAME) - 1)
+            WHEN s.LAST_NAME NOT LIKE 'De %' AND CHARINDEX(' ',s.LAST_NAME) > 0 THEN LEFT(s.LAST_NAME,CHARINDEX(' ',s.LAST_NAME) - 1)        
+            WHEN CHARINDEX('-',s.LAST_NAME) > 0 THEN LEFT(s.LAST_NAME,CHARINDEX('-',s.LAST_NAME) - 1)
+            ELSE REPLACE(s.LAST_NAME, ' JR', '')
+           END)) AS last_name_clean
+        ,CONVERT(VARCHAR,DATEPART(MONTH,s.DOB)) AS dob_month
+        ,CONVERT(VARCHAR,RIGHT(DATEPART(DAY,s.DOB),2)) AS dob_day
+        ,CONVERT(VARCHAR,RIGHT(DATEPART(YEAR,s.DOB),2)) AS dob_year                
+  FROM KIPP_NJ..STUDENTS s WITH(NOLOCK)
+  JOIN KIPP_NJ..PS$SCHOOLS#static sch WITH(NOLOCK)
+    ON s.SCHOOLID = sch.SCHOOL_NUMBER
+  WHERE s.ENROLL_STATUS != -1          
+ )
+
+SELECT STUDENT_NUMBER
+      ,base_username
+      ,alt_username
+      ,CASE 
+        WHEN alt_dupe_audit > 1 THEN first_init + last_name_clean + dob_month + dob_day
+        WHEN base_dupe_audit > 1 THEN alt_username 
+        ELSE base_username 
+       END AS student_web_id
+      ,CASE
+        WHEN GRADE_LEVEL >= 2 THEN last_name_clean + dob_year 
+        ELSE LOWER(school_name) + '1'
+       END AS student_web_password
+      ,uses_alt
+FROM
+    (
+     SELECT STUDENT_NUMBER
+           ,GRADE_LEVEL
+           ,school_name
+           ,first_name_clean
+           ,first_init
+           ,last_name_clean
+           ,dob_month
+           ,dob_day
+           ,dob_year
+           ,base_username
+           ,alt_username
+           ,base_dupe_audit
+           ,CASE WHEN base_dupe_audit > 1 THEN 1 ELSE 0 END AS uses_alt
+           ,ROW_NUMBER() OVER(
+             PARTITION BY CASE WHEN base_dupe_audit > 1 THEN alt_username ELSE base_username END
+               ORDER BY student_number) AS alt_dupe_audit
+     FROM
+         (         
+          SELECT STUDENT_NUMBER
+                ,GRADE_LEVEL
+                ,school_name
+                ,first_name_clean
+                ,first_init
+                ,last_name_clean
+                ,dob_month
+                ,dob_day
+                ,dob_year
+                ,last_name_clean
+                  + dob_month 
+                  + dob_day
+                  AS base_username
+                ,first_name_clean
+                  + dob_month 
+                  + dob_day
+                  AS alt_username
+                ,ROW_NUMBER() OVER(
+                  PARTITION BY last_name_clean + dob_month + dob_day
+                    ORDER BY first_name_clean) AS base_dupe_audit           
+          FROM clean_names         
+         ) sub
+    ) sub
