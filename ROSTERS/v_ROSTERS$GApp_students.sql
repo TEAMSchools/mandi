09adb533e9abf5cbb371a4c9d8163ba732e44615
Charlie Bini <cbini87@gmail.com>
2015-07-30 17:49:21 -0400

    tweaks to the login logic

--- ROSTERS/v_ROSTERS$GApp_students.sql
+++ ROSTERS/v_ROSTERS$GApp_students.sql
@@ -7,27 +7,11 @@ SELECT co.student_number
       ,co.first_name AS firstname
       ,co.last_name AS lastname      
       ,acct.student_web_id + '@teamstudents.org' AS email
-      ,acct.STUDENT_WEB_PASSWORD AS password 
+      ,acct.STUDENT_WEB_PASSWORD AS [password]
       ,CASE WHEN co.enroll_status = 0 THEN 'off' ELSE 'on' END AS suspended
-      ,'/Students/' + CASE WHEN co.enroll_status = 0 THEN co.school_name ELSE 'Disabled' END AS org      
+      ,'/Students/' + CASE WHEN co.enroll_status = 0 THEN co.school_name ELSE 'Disabled' END AS org
 FROM KIPP_NJ..COHORT$identifiers_long#static co WITH(NOLOCK)
-LEFT OUTER JOIN KIPP_NJ..PS$STUDENTS#static s WITH(NOLOCK)
-  ON co.student_number = s.STUDENT_NUMBER
 LEFT OUTER JOIN KIPP_NJ..ROSTERS$PS_access_accounts acct WITH(NOLOCK)
   ON co.student_number = acct.STUDENT_NUMBER
 WHERE co.year = 2015
   AND co.rn = 1
-  AND co.STUDENT_WEB_ID IS NOT NULL
-      
-/* -- TESTING
-SELECT DISTINCT 'Test' AS firstname
-      ,CASE WHEN enroll_status = 0 THEN school_name ELSE 'Disabled' END AS lastname      
-      ,LOWER(CASE WHEN enroll_status = 0 THEN school_name ELSE 'Disabled' END + 'GAMtest' + '@teamstudents.org') AS email
-      ,'testing123' AS password      
-      ,CASE WHEN enroll_status = 0 THEN 'off' ELSE 'on' END AS suspended
-      ,'/Students/' + CASE WHEN enroll_status = 0 THEN school_name ELSE 'Disabled' END AS org
-FROM KIPP_NJ..COHORT$identifiers_long#static WITH(NOLOCK)
-WHERE year = KIPP_NJ.dbo.fn_Global_Academic_Year()
-  AND rn = 1
-  AND STUDENT_WEB_ID IS NOT NULL
---*/
