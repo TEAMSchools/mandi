80004b52dde7e14c6a75e39da3fffb1f4cfcf98e
Charlie Bini <cbini87@gmail.com>
2015-08-11 16:21:26 -0400

    nothing to see here

--- LIT/v_LIT$achieved_by_round.sql
+++ LIT/v_LIT$achieved_by_round.sql
@@ -42,8 +42,8 @@ WITH roster AS (
         ,terms.start_date
   FROM roster r WITH(NOLOCK)
   JOIN terms WITH(NOLOCK)
-    ON (r.year = terms.academic_year AND r.schoolid = terms.schoolid) 
-    OR (r.grade_level <= 4 AND terms.schoolid IS NULL AND r.year <= 2012 AND r.year = terms.academic_year) -- ES rounds for archive years
+    ON ((r.year = terms.academic_year AND r.schoolid = terms.schoolid) 
+         OR (r.grade_level <= 4 AND terms.schoolid IS NULL AND r.year <= 2012 AND r.year = terms.academic_year)) -- ES rounds for archive years
  )
 
 -- highest acheived test per round for each student 
