6f6847f28db00949d84ef5c35c36cf33ec9080b5
almartin82 <almartin@gmail.com>
2015-08-19 13:06:46 -0400

    cohort and map code (stale)

--- MAP/v_MAP$enrollment_attribution.sql
+++ MAP/v_MAP$enrollment_attribution.sql
@@ -6,12 +6,16 @@ WITH es_hr AS
     (SELECT hr.studentid
           ,hr.year
           ,hr.teacher_name AS teacher
+          ,hr.course_number AS course_number
           ,hr.section_number AS enrollment
+          ,t.last_name AS teacher_last
     FROM KIPP_NJ..COHORT$student_homerooms hr
     JOIN KIPP_NJ..COHORT$comprehensive_long#static c
       ON hr.studentid = c.studentid
      AND hr.year = c.year
      AND c.rn = 1
+    JOIN KIPP_NJ..TEACHERS t
+      ON CAST(hr.teachernumber AS varchar) = t.teachernumber
     WHERE hr.rn_stu_year = 1
       AND c.grade_level < 5
     ),
@@ -35,6 +39,8 @@ WITH es_hr AS
          ,ce.academic_year
          ,ce.course_number
          ,ce.teacher_name AS teacher
+         ,ce.section_number
+         ,t.last_name AS teacher_last
          ,CASE 
             WHEN ce.credittype = 'MATH' THEN 'Mathematics'
             WHEN ce.credittype = 'ENG' THEN 'Reading'
@@ -52,14 +58,19 @@ WITH es_hr AS
    FROM ms_base
    JOIN PS$course_enrollments#static ce
      ON ms_base.studentid = ce.studentid
+    AND ms_base.year = ce.academic_year
     AND ce.credittype IN ('ENG', 'MATH', 'RHET', 'SCI')
     AND ce.drop_flags = 0
+    JOIN KIPP_NJ..TEACHERS t
+      ON CAST(ce.teachernumber AS varchar) = t.teachernumber
    ),
    ms_subj_slim AS 
   (SELECT studentid,
           year, 
           teacher,
-          course_number AS enrollment,
+          course_number,
+          course_number + ' ' + section_number AS enrollment,
+          teacher_last,
           join_subj
    FROM ms_subj
    WHERE ms_subj.rn_credittype = 1
