56c737a05018be591e7df2f7b2485bae8a52b76a
ldesimon <laszlo.desimon@gmail.com>
2015-12-04 00:02:36 -0500

    patch to reset BOLD points tracker to Q2

--- REPORTING/v_REPORTING$trip_tracker#BOLD.sql
+++ REPORTING/v_REPORTING$trip_tracker#BOLD.sql
@@ -49,7 +49,8 @@ FROM
      WHERE co.year = KIPP_NJ.dbo.fn_Global_Academic_Year()
        AND co.schoolid = 73258
        AND co.enroll_status = 0
-       AND co.date BETWEEN '2015-09-24' AND CONVERT(DATE,GETDATE())
+       AND co.date BETWEEN '2015-11-06' AND CONVERT(DATE,GETDATE())
+--	   AND co.date BETWEEN '2015-09-24' AND CONVERT(DATE,GETDATE())
        AND co.date IN (SELECT cal.date_value
                        FROM KIPP_NJ..PS$CALENDAR_DAY cal WITH(NOLOCK)
                        WHERE cal.academic_year = KIPP_NJ.dbo.fn_Global_Academic_Year()
