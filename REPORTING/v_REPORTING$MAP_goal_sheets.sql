29ffaa09d4f4cf931beb089ac6fd6b3d745a45ed
ldesimon <laszlo.desimon@gmail.com>
2015-12-04 15:25:51 -0500

    updates to show MAP goals for winter

--- REPORTING/v_REPORTING$MAP_goal_sheets.sql
+++ REPORTING/v_REPORTING$MAP_goal_sheets.sql
@@ -4,6 +4,37 @@ GO
 ALTER VIEW REPORTING$MAP_goal_sheets AS
 
 SELECT *
+	  ,Baseline_Reading_keep_up_goal / 2 AS Winter_Reading_keep_up_goal
+	  ,Baseline_Reading_keep_up_rit - (Baseline_Reading_keep_up_goal / 2) AS Winter_Reading_keep_up_rit
+	  ,Baseline_Reading_rutgers_ready_goal / 2 AS Winter_Reading_rutgers_ready_goal
+	  ,Baseline_Reading_rutgers_ready_rit - (Baseline_Reading_rutgers_ready_goal / 2) AS Winter_Reading_rutgers_ready_rit
+	  ,Baseline_Mathematics_keep_up_goal / 2 AS Winter_Mathematics_keep_up_goal
+	  ,Baseline_Mathematics_keep_up_rit - (Baseline_Mathematics_keep_up_goal / 2) AS Winter_Mathematics_keep_up_rit
+	  ,Baseline_Mathematics_rutgers_ready_goal / 2 AS Winter_Mathematics_rutgers_ready_goal
+	  ,Baseline_Mathematics_rutgers_ready_rit - (Baseline_Mathematics_rutgers_ready_goal / 2) AS Winter_Mathematics_rutgers_ready_rit
+	  ,Baseline_Language_keep_up_goal / 2 AS Winter_Language_keep_up_goal
+	  ,Baseline_Language_keep_up_rit - (Baseline_Language_keep_up_goal / 2) AS Winter_Language_keep_up_rit
+	  ,Baseline_Language_rutgers_ready_goal / 2 AS Winter_Language_rutgers_ready_goal
+	  ,Baseline_Language_rutgers_ready_rit - (Baseline_Language_rutgers_ready_goal / 2) AS Winter_Language_rutgers_ready_rit
+	  ,Baseline_Science_keep_up_goal / 2 AS Winter_Science_keep_up_goal
+	  ,Baseline_Science_keep_up_rit - (Baseline_Science_keep_up_goal / 2) AS Winter_Science_keep_up_rit
+	  ,Baseline_Science_rutgers_ready_goal / 2 AS Winter_Science_rutgers_ready_goal
+	  ,Baseline_Science_rutgers_ready_rit - (Baseline_Science_rutgers_ready_goal / 2) AS Winter_Science_rutgers_ready_rit
+	  ,CASE WHEN grade_level = 0 THEN 'Kindergarten'
+	        WHEN grade_level = 1 THEN '1st'
+			WHEN grade_level = 2 THEN '2nd'
+			WHEN grade_level = 3 THEN '3rd'
+			WHEN grade_level = 4 THEN '4th'
+			WHEN grade_level = 5 THEN '5th'
+			WHEN grade_level = 6 THEN '6th'
+			WHEN grade_level = 7 THEN '7th'
+			WHEN grade_level = 8 THEN '8th'
+			WHEN grade_level = 9 THEN '9th'
+			WHEN grade_level = 10 THEN '10th'
+			WHEN grade_level = 11 THEN '11th'
+	   ELSE NULL 
+	   END AS grade_level_name
+
 FROM
     (
      SELECT schoolid                  
@@ -101,64 +132,54 @@ FROM
     ) sub
 PIVOT(
   MAX(value)
-  FOR pivot_field IN ([Baseline_Language_TestPercentile]
-                     ,[Baseline_Language_TestRITScore]
+  FOR pivot_field IN ([Baseline_Reading_TestPercentile]
+                     ,[Baseline_Reading_TestRITScore]
                      ,[Baseline_Mathematics_TestPercentile]
                      ,[Baseline_Mathematics_TestRITScore]                     
-                     ,[Baseline_Reading_TestPercentile]
-                     ,[Baseline_Reading_TestRITScore]                     
+                     ,[Baseline_Language_TestPercentile]
+                     ,[Baseline_Language_TestRITScore]                     
                      ,[Baseline_Science_TestPercentile]
-                     ,[Baseline_Science_TestRITScore]                     
-                     ,[Fall_Language_TestPercentile]
-                     ,[Fall_Language_TestRITScore]                     
+                     ,[Baseline_Science_TestRITScore] 
+                     ,[Baseline_Mathematics_keep_up_goal]
+                     ,[Baseline_Mathematics_keep_up_rit]
+                     ,[Baseline_Mathematics_rutgers_ready_goal]
+                     ,[Baseline_Mathematics_rutgers_ready_rit]             
+                     ,[Baseline_Reading_keep_up_goal]
+                     ,[Baseline_Reading_keep_up_rit]
+                     ,[Baseline_Reading_rutgers_ready_goal]
+                     ,[Baseline_Reading_rutgers_ready_rit]         
+                     ,[Baseline_Language_keep_up_goal]
+                     ,[Baseline_Language_keep_up_rit]
+                     ,[Baseline_Language_rutgers_ready_goal]
+                     ,[Baseline_Language_rutgers_ready_rit]                             
+                     ,[Baseline_Science_keep_up_goal]
+                     ,[Baseline_Science_keep_up_rit]
+                     ,[Baseline_Science_rutgers_ready_goal]
+                     ,[Baseline_Science_rutgers_ready_rit]      
+					 ,[Fall_Reading_TestPercentile]
+                     ,[Fall_Reading_TestRITScore]                     
                      ,[Fall_Mathematics_TestPercentile]
                      ,[Fall_Mathematics_TestRITScore]                     
-                     ,[Fall_Reading_TestPercentile]
-                     ,[Fall_Reading_TestRITScore]                     
+                     ,[Fall_Language_TestPercentile]
+                     ,[Fall_Language_TestRITScore]                     
                      ,[Fall_Science_TestPercentile]
                      ,[Fall_Science_TestRITScore]                     
-                     ,[Spring_Language_TestPercentile]
-                     ,[Spring_Language_TestRITScore]
-                     ,[Spring_Mathematics_keep_up_goal]
-                     ,[Spring_Mathematics_keep_up_rit]
-                     ,[Spring_Mathematics_rutgers_ready_goal]
-                     ,[Spring_Mathematics_rutgers_ready_rit]
-                     ,[Spring_Mathematics_TestPercentile]
-                     ,[Spring_Mathematics_TestRITScore]
-                     ,[Spring_Reading_keep_up_goal]
-                     ,[Spring_Reading_keep_up_rit]
-                     ,[Spring_Reading_rutgers_ready_goal]
-                     ,[Spring_Reading_rutgers_ready_rit]
-                     ,[Spring_Reading_TestPercentile]
-                     ,[Spring_Reading_TestRITScore]
-                     ,[Spring_Science_keep_up_goal]
-                     ,[Spring_Science_keep_up_rit]
-                     ,[Spring_Science_rutgers_ready_goal]
-                     ,[Spring_Science_rutgers_ready_rit]
-                     ,[Spring_Science_TestPercentile]
-                     ,[Spring_Science_TestRITScore]
-                     ,[Winter_Language_keep_up_goal]
-                     ,[Winter_Language_keep_up_rit]
-                     ,[Winter_Language_rutgers_ready_goal]
-                     ,[Winter_Language_rutgers_ready_rit]
+                     
                      ,[Winter_Language_TestPercentile]
                      ,[Winter_Language_TestRITScore]                     
-                     ,[Winter_Mathematics_keep_up_goal]
-                     ,[Winter_Mathematics_keep_up_rit]
-                     ,[Winter_Mathematics_rutgers_ready_goal]
-                     ,[Winter_Mathematics_rutgers_ready_rit]
                      ,[Winter_Mathematics_TestPercentile]
                      ,[Winter_Mathematics_TestRITScore]
-                     ,[Winter_Reading_keep_up_goal]
-                     ,[Winter_Reading_keep_up_rit]
-                     ,[Winter_Reading_rutgers_ready_goal]
-                     ,[Winter_Reading_rutgers_ready_rit]
                      ,[Winter_Reading_TestPercentile]
                      ,[Winter_Reading_TestRITScore]
-                     ,[Winter_Science_keep_up_goal]
-                     ,[Winter_Science_keep_up_rit]
-                     ,[Winter_Science_rutgers_ready_goal]
-                     ,[Winter_Science_rutgers_ready_rit]
                      ,[Winter_Science_TestPercentile]
-                     ,[Winter_Science_TestRITScore])
+                     ,[Winter_Science_TestRITScore]
+					 
+ 					 ,[Spring_Reading_TestPercentile]
+                     ,[Spring_Reading_TestRITScore]
+                     ,[Spring_Mathematics_TestPercentile]
+                     ,[Spring_Mathematics_TestRITScore]
+                     ,[Spring_Language_TestPercentile]
+                     ,[Spring_Language_TestRITScore]
+                     ,[Spring_Science_TestPercentile]
+                     ,[Spring_Science_TestRITScore])
  ) p
