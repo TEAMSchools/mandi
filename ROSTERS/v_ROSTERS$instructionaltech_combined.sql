332f5fa19e3ee739b97a6f8c49ed09a244b43f3d
ldesimon <laszlo.desimon@gmail.com>
2016-01-14 10:14:59 -0500

    removed the convert to money for AR fields as quick patch as it was throwing 'Cannot convert a char value to money. The char value has incorrect syntax.'

--- ROSTERS/v_ROSTERS$instructionaltech_combined.sql
+++ ROSTERS/v_ROSTERS$instructionaltech_combined.sql
@@ -186,7 +186,7 @@ WHERE roster.boy_status IN ('Promoted')
 
 --LEXIA	STUDENTS
 
---/*
+/*
 
 SELECT roster.student_number AS "Lexia id"
       ,roster.first_name AS "First Name"
@@ -207,7 +207,7 @@ SELECT roster.student_number AS "Lexia id"
 	  
 FROM roster
 
-WHERE entrydate > '2015-10-25'
+WHERE entrydate > '2015-12-25'
   --AND grade_level < 6
 -- roster.schoolid IN (133570965,73252,73258,179902)
   -- WHERE entrydate >= '2015-09-08'
@@ -215,7 +215,7 @@ WHERE entrydate > '2015-10-25'
 
 ORDER BY roster.abbreviation, roster.grade_level, roster.teacherloginid, roster.lastfirst
 
---*/
+*/
 
 --LEXA STAFF
 
@@ -242,7 +242,7 @@ ORDER BY roster.ABBREVIATION, roster.section_number
 
 -- ST MATH STUDENTS
 
-/*
+--/*
 
 SELECT
 	 CASE 
@@ -293,7 +293,7 @@ SELECT
    ,roster.entrydate
 FROM roster
 
-WHERE entrydate > '2015-11-01'
+WHERE entrydate > '2015-12-25'
 	  --AND grade_level < 9
 	  --AND schoolid IN (133570965,73252,73258,179902)
 	  --AND schoolid = 73254
@@ -302,4 +302,4 @@ ORDER BY schoolid
         ,grade_level
 		,teacher_last_name
 
-*/
+--*/
