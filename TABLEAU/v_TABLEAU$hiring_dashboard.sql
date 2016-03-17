f1fa4a7e9ee91367f75bf77984b1b46f41cfec99
Charlie Bini <cbini87@gmail.com>
2016-03-17 12:01:47 -0400

    hiring dash

---
+++ TABLEAU/v_TABLEAU$hiring_dashboard.sql
@@ -0,0 +1,45 @@
+USE KIPP_NJ
+GO
+
+ALTER VIEW TABLEAU$hiring_dashboard AS
+
+SELECT pos.id AS position_id
+      ,pos.Position_Name__c
+      ,pos.CreatedDate AS position_createddate
+      ,KIPP_NJ.dbo.fn_DateToSY(CONVERT(DATE,pos.CreatedDate)) AS academic_year
+      ,pos.Date_Position_Filled__c
+      ,pos.Name AS position_name
+      ,pos.School__c
+      ,pos.Replacement_or_New_Position__c
+      ,pos.Status__c
+      ,pos.LastModifiedDate
+      
+      ,post.job_posting_job_posting_name
+      ,CONVERT(DATE,post.job_posting_created_date) AS job_posting_created_date
+      
+      ,post.primary_contact
+      ,post.job_posting_id
+      ,post.city
+      ,post.job_type
+      ,post.job_subtype
+      ,post.grade_level
+      ,post.job_posting_url
+
+      ,app.[Name] AS app_name
+      ,app.[Profile_Application__c]
+      ,app.[Contact_Name__c]
+      ,app.[CreatedDate] AS app_createddate
+      ,app.[Application_Review_Score__c]
+      ,app.[Average_Teacher_Phone_Score__c]
+      ,app.[Job_Posting__c]
+      ,app.[Job_Position__c]
+      ,app.[Stage__c]
+      ,app.[Selection_Status__c]
+      ,app.[Primary_Interest_Grade_Level_General__c]
+FROM KIPPCareersMirror..Job_Position__c pos WITH(NOLOCK) 
+LEFT OUTER JOIN KIPP_NJ..AUTOLOAD$GDOCS_RECRUIT_jobpostingdrop post WITH(NOLOCK)
+  ON LEFT(pos.Job_Posting__c, (LEN(pos.Job_Posting__c) - 3)) = post.job_posting_id
+LEFT OUTER JOIN KIPPCareersMirror..Job_Application__c app WITH(NOLOCK)
+  ON post.job_posting_id = LEFT(app.Job_Posting__c, (LEN(app.Job_Posting__c) - 3)) 
+ --AND ((pos.Id = app.Job_Position__c) OR (app.Job_Position__c IS NULL))
+WHERE pos.Region__c = 'New Jersey'
