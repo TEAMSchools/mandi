bbc9489ca61193a3e36d3421f2ada72eac9fc486
Charlie <cbini87@gmail.com>
2016-05-10 15:27:48 -0400

    changes for assessment dashboard, new suspension compliance

--- TABLEAU/v_TABLEAU$assessment_data_blast.sql
+++ TABLEAU/v_TABLEAU$assessment_data_blast.sql
@@ -3,18 +3,18 @@ GO
 
 ALTER VIEW TABLEAU$assessment_data_blast AS
 
-SELECT sub.*
-      ,LOG(std_assessment_order) AS powerlaw_x -- ln of ordinal number
-      ,LOG(proficiency_band) AS powerlaw_y -- ln of score
-      ,LOG(proficiency_band) * LOG(std_assessment_order) AS powerlaw_xy -- ln(score) * ln(order)
-      ,POWER(LOG(std_assessment_order), 2) AS powerlaw_x2 -- ln(order)^2
+--SELECT sub.*
+--      ,LOG(std_assessment_order) AS powerlaw_x -- ln of ordinal number
+--      ,LOG(proficiency_band) AS powerlaw_y -- ln of score
+--      ,LOG(proficiency_band) * LOG(std_assessment_order) AS powerlaw_xy -- ln(score) * ln(order)
+--      ,POWER(LOG(std_assessment_order), 2) AS powerlaw_x2 -- ln(order)^2
 
-      ,SUM(LOG(std_assessment_order)) OVER(PARTITION BY sub.student_number, sub.standards_tested ORDER BY sub.date_taken) AS running_powerlaw_x -- ln of ordinal number
-      ,SUM(LOG(proficiency_band)) OVER(PARTITION BY sub.student_number, sub.standards_tested ORDER BY sub.date_taken) AS running_powerlaw_y -- ln of score
-      ,SUM(LOG(proficiency_band) * LOG(std_assessment_order)) OVER(PARTITION BY sub.student_number, sub.standards_tested ORDER BY sub.date_taken) AS running_powerlaw_xy -- ln(score) * ln(order)
-      ,SUM(POWER(LOG(std_assessment_order), 2)) OVER(PARTITION BY sub.student_number, sub.standards_tested ORDER BY sub.date_taken) AS running_powerlaw_x2 -- ln(order)^2
-FROM
-    (
+--      ,SUM(LOG(std_assessment_order)) OVER(PARTITION BY sub.student_number, sub.standards_tested ORDER BY sub.date_taken) AS running_powerlaw_x -- ln of ordinal number
+--      ,SUM(LOG(proficiency_band)) OVER(PARTITION BY sub.student_number, sub.standards_tested ORDER BY sub.date_taken) AS running_powerlaw_y -- ln of score
+--      ,SUM(LOG(proficiency_band) * LOG(std_assessment_order)) OVER(PARTITION BY sub.student_number, sub.standards_tested ORDER BY sub.date_taken) AS running_powerlaw_xy -- ln(score) * ln(order)
+--      ,SUM(POWER(LOG(std_assessment_order), 2)) OVER(PARTITION BY sub.student_number, sub.standards_tested ORDER BY sub.date_taken) AS running_powerlaw_x2 -- ln(order)^2
+--FROM
+--    (
      SELECT co.schoolid
            ,co.year AS academic_year
            ,co.grade_level
@@ -22,10 +22,7 @@ FROM
            ,co.student_number
            ,co.lastfirst
            ,co.spedlep  
-           ,co.enroll_status    
-           --,co.gender
-           --,co.retained_yr_flag
-           --,co.retained_ever_flag
+           ,co.enroll_status               
 
            ,a.assessment_id
            ,a.title
@@ -40,8 +37,8 @@ FROM
            ,std.custom_code AS standards_tested      
            ,std.description AS standard_descr      
 
-           ,ROUND(CONVERT(FLOAT,res.percent_correct),1) AS std_percent_correct
-           ,CONVERT(FLOAT,res.mastered) AS std_is_mastered
+           ,res.percent_correct AS std_percent_correct
+           ,res.mastered AS std_is_mastered
            ,res.performance_band_level AS proficiency_band
             
            ,enr.teacher_name      
@@ -54,8 +51,8 @@ FROM
            ,ROW_NUMBER() OVER(
               PARTITION BY co.student_number, astd.standard_id
                 ORDER BY ovr.date_taken ASC) AS std_assessment_order
-     FROM KIPP_NJ..COHORT$identifiers_long#static co WITH (NOLOCK)
-     JOIN KIPP_NJ..ILLUMINATE$assessments#static a WITH (NOLOCK)
+     FROM KIPP_NJ..COHORT$identifiers_long#static co WITH(NOLOCK)
+     JOIN KIPP_NJ..ILLUMINATE$assessments#static a WITH(NOLOCK)
        ON co.year = a.academic_year
       AND CHARINDEX(REPLACE(co.grade_level, 0, 'K'), a.tags) > 0 
       AND a.scope IN ('CMA - End-of-Module','CMA - Mid-Module')
@@ -76,27 +73,25 @@ FROM
      LEFT OUTER JOIN KIPP_NJ..ILLUMINATE$agg_student_responses#static ovr WITH(NOLOCK)
        ON co.student_number = ovr.local_student_id
       AND a.assessment_id = ovr.assessment_id  
-     LEFT OUTER JOIN KIPP_NJ..ILLUMINATE$agg_student_responses_standard res WITH (NOLOCK)
+     LEFT OUTER JOIN KIPP_NJ..ILLUMINATE$agg_student_responses_standard res WITH(NOLOCK)
        ON co.student_number = res.local_student_id
       AND a.assessment_id = res.assessment_id
       AND astd.standard_id = res.standard_id  
      WHERE co.year = KIPP_NJ.dbo.fn_Global_Academic_Year()
        AND co.enroll_status = 0  
-       AND co.rn = 1
+       AND co.rn = 1       
+       AND co.team NOT LIKE '%Pathways%'
 
      UNION ALL
 
-     SELECT co.schoolid
+     SELECT CASE WHEN co.team LIKE '%Pathways%' THEN 732570 ELSE co.schoolid END AS schoolid
            ,co.year AS academic_year
            ,co.grade_level
            ,co.team      
            ,co.student_number
            ,co.lastfirst
            ,co.spedlep  
-           ,co.enroll_status    
-           --,co.gender
-           --,co.retained_yr_flag
-           --,co.retained_ever_flag
+           ,co.enroll_status               
 
            ,a.assessment_id
            ,a.title
@@ -111,8 +106,8 @@ FROM
            ,std.custom_code AS standards_tested      
            ,std.description AS standard_descr      
 
-           ,ROUND(CONVERT(FLOAT,res.percent_correct),1) AS std_percent_correct
-           ,CONVERT(FLOAT,res.mastered) AS std_is_mastered
+           ,res.percent_correct AS std_percent_correct
+           ,res.mastered AS std_is_mastered
            ,res.performance_band_level AS proficiency_band
             
            ,enr.teacher_name      
@@ -125,10 +120,10 @@ FROM
            ,ROW_NUMBER() OVER(
               PARTITION BY co.student_number, astd.standard_id
                 ORDER BY ovr.date_taken ASC) AS std_assessment_order
-     FROM KIPP_NJ..ILLUMINATE$assessments#static a WITH (NOLOCK)
+     FROM KIPP_NJ..ILLUMINATE$assessments#static a WITH(NOLOCK)
      JOIN KIPP_NJ..ILLUMINATE$agg_student_responses#static ovr WITH(NOLOCK)
        ON a.assessment_id = ovr.assessment_id  
-     JOIN KIPP_NJ..COHORT$identifiers_long#static co WITH (NOLOCK) 
+     JOIN KIPP_NJ..COHORT$identifiers_long#static co WITH(NOLOCK) 
        ON ovr.local_student_id = co.student_number
       AND a.academic_year = co.year 
       AND ((CHARINDEX(REPLACE(co.grade_level, 0, 'K'), a.tags) = 0) OR ((a.title LIKE '%replacement%' OR a.title LIKE '%modified%')))
@@ -138,7 +133,7 @@ FROM
        ON a.assessment_id = astd.assessment_id
      LEFT OUTER JOIN KIPP_NJ..ILLUMINATE$standards#static std WITH(NOLOCK)
        ON astd.standard_id = std.standard_id
-     LEFT OUTER JOIN KIPP_NJ..ILLUMINATE$agg_student_responses_standard res WITH (NOLOCK)
+     LEFT OUTER JOIN KIPP_NJ..ILLUMINATE$agg_student_responses_standard res WITH(NOLOCK)
        ON ovr.local_student_id = res.local_student_id
       AND a.assessment_id = res.assessment_id
       AND astd.standard_id = res.standard_id  
@@ -152,4 +147,68 @@ FROM
      WHERE a.academic_year = KIPP_NJ.dbo.fn_Global_Academic_Year()    
        AND a.scope IN ('CMA - End-of-Module','CMA - Mid-Module')
        AND a.subject_area IN ('Text Study','Mathematics')
-    ) sub
+
+     UNION ALL
+
+     SELECT CASE WHEN co.team LIKE '%Pathways%' THEN 732570 ELSE co.schoolid END AS schoolid
+           ,co.year AS academic_year
+           ,co.grade_level
+           ,co.team      
+           ,co.student_number
+           ,co.lastfirst
+           ,co.spedlep  
+           ,co.enroll_status               
+
+           ,a.assessment_id
+           ,a.title
+           ,a.scope            
+           ,a.subject_area AS subject      
+           ,a.administered_at
+           ,0 AS is_replacement
+      
+           ,ovr.date_taken
+           ,ovr.percent_correct AS overall_pct_correct            
+
+           ,std.custom_code AS standards_tested      
+           ,std.description AS standard_descr      
+
+           ,res.percent_correct AS std_percent_correct
+           ,res.mastered AS std_is_mastered
+           ,res.performance_band_level AS proficiency_band
+            
+           ,enr.teacher_name      
+           ,enr.period
+           ,enr.section_number
+      
+           ,ROW_NUMBER() OVER(
+              PARTITION BY co.student_number, a.assessment_id
+                ORDER BY co.student_number) AS overall_rn      
+           ,ROW_NUMBER() OVER(
+              PARTITION BY co.student_number, astd.standard_id
+                ORDER BY ovr.date_taken ASC) AS std_assessment_order
+     FROM KIPP_NJ..ILLUMINATE$assessments#static a WITH(NOLOCK)
+     JOIN KIPP_NJ..ILLUMINATE$agg_student_responses#static ovr WITH(NOLOCK)
+       ON a.assessment_id = ovr.assessment_id  
+     JOIN KIPP_NJ..COHORT$identifiers_long#static co WITH(NOLOCK) 
+       ON ovr.local_student_id = co.student_number
+      AND a.academic_year = co.year       
+      --AND a.schoolid = co.schoolid
+      AND co.rn = 1
+     LEFT OUTER JOIN KIPP_NJ..ILLUMINATE$assessment_standards#static astd WITH(NOLOCK)
+       ON a.assessment_id = astd.assessment_id
+     LEFT OUTER JOIN KIPP_NJ..ILLUMINATE$standards#static std WITH(NOLOCK)
+       ON astd.standard_id = std.standard_id
+     LEFT OUTER JOIN KIPP_NJ..ILLUMINATE$agg_student_responses_standard res WITH(NOLOCK)
+       ON ovr.local_student_id = res.local_student_id
+      AND a.assessment_id = res.assessment_id
+      AND astd.standard_id = res.standard_id  
+     LEFT OUTER JOIN KIPP_NJ..PS$course_enrollments#static enr WITH(NOLOCK)
+       ON co.studentid = enr.studentid
+      AND co.year = enr.academic_year
+      /* ES and BOLD, JOIN to HR, otherwise JOIN to course */
+      AND (((co.grade_level <= 4 OR co.schoolid = 73258) AND enr.COURSE_NUMBER = 'HR') 
+               OR (co.schoolid != 73258 AND co.grade_level >= 5 AND a.subject_area = enr.illuminate_subject))
+      AND enr.drop_flags = 0
+     WHERE a.academic_year = KIPP_NJ.dbo.fn_Global_Academic_Year()           
+       AND a.scope NOT IN ('CMA - End-of-Module','CMA - Mid-Module')       
+--    ) sub
---
+++ TABLEAU/v_TABLEAU$compliance_suspensions.sql
@@ -0,0 +1,29 @@
+USE KIPP_NJ
+GO
+
+ALTER VIEW TABLEAU$compliance_suspensions AS
+
+SELECT co.SID
+      ,co.student_number
+      ,co.lastfirst
+      ,co.schoolid
+      ,co.grade_level
+      ,co.enroll_status
+      ,co.ETHNICITY
+      ,co.GENDER
+      ,co.SPEDLEP
+      ,co.SPED_code
+      
+      ,att.streak_id 
+      ,att.att_code
+      ,att.streak_length_membership
+      ,att.streak_start
+      ,att.streak_end
+FROM KIPP_NJ..COHORT$identifiers_long#static co WITH(NOLOCK)
+JOIN KIPP_NJ..ATT_MEM$attendance_streak att WITH(NOLOCK)
+  ON co.studentid = att.studentid
+ AND co.year = att.academic_year
+ AND att.att_code IN ('ISS','OSS')
+WHERE co.year = KIPP_NJ.dbo.fn_Global_Academic_Year()
+  AND co.rn = 1
+  AND co.grade_level != 99
