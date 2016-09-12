45cc7ba83f469deb1a25e944d6449ffc8802c26a
Charlie <cbini87@gmail.com>
2016-09-12 13:46:19 -0400

    various fixes

--- TABLEAU/v_TABLEAU$student_contact_info.sql
+++ TABLEAU/v_TABLEAU$student_contact_info.sql
@@ -5,12 +5,14 @@ ALTER VIEW TABLEAU$student_contact_info AS
 
 WITH contacts_unpivot AS (
   SELECT studentid        
+        ,FAMILY_IDENT
         ,LEFT(field, CHARINDEX('_',field) - 1) AS person
         ,RIGHT(field, LEN(field) - CHARINDEX('_',field)) AS type
         ,value      
   FROM
       (
-       SELECT con.STUDENTID
+       SELECT s.ID AS STUDENTID
+             ,s.FAMILY_IDENT
              ,CONVERT(VARCHAR(MAX),CONCAT(LTRIM(RTRIM(s.STREET)), ', ', LTRIM(RTRIM(s.CITY)), ' ', LTRIM(RTRIM(s.ZIP)))) AS HOME_NAME           
              ,CONVERT(VARCHAR(MAX),con.HOME_PHONE) AS HOME_HOME
              ,CONVERT(VARCHAR(MAX),blob.GUARDIANEMAIL) AS HOME_EMAIL
@@ -19,11 +21,13 @@ WITH contacts_unpivot AS (
              ,CONVERT(VARCHAR(MAX),con.MOTHER_HOME) AS PARENT1_HOME
              ,CONVERT(VARCHAR(MAX),con.MOTHER_CELL) AS PARENT1_CELL
              ,CONVERT(VARCHAR(MAX),con.MOTHER_DAY) AS PARENT1_DAY           
-             ,CONVERT(VARCHAR(MAX),con.FATHER) AS PARENT2_NAME
+             ,CONVERT(VARCHAR(MAX),cs.MOTHER_REGISTERED_TO_VOTE) AS PARENT1_REGISTEREDTOVOTE
+             ,CONVERT(VARCHAR(MAX),con.FATHER) AS PARENT2_NAME             
              ,CASE WHEN CONCAT(con.FATHER_HOME, con.FATHER_CELL, con.FATHER_DAY) != '' THEN CONVERT(VARCHAR(MAX),'Father') END AS PARENT2_RELATION
              ,CONVERT(VARCHAR(MAX),con.FATHER_HOME) AS PARENT2_HOME
              ,CONVERT(VARCHAR(MAX),con.FATHER_CELL) AS PARENT2_CELL
              ,CONVERT(VARCHAR(MAX),con.FATHER_DAY) AS PARENT2_DAY
+             ,CONVERT(VARCHAR(MAX),cs.FATHER_REGISTERED_TO_VOTE) AS PARENT2_REGISTEREDTOVOTE
              ,CONVERT(VARCHAR(MAX),con.DOCTOR_NAME) AS DOCTOR_NAME
              ,CASE WHEN CONCAT(con.DOCTOR_NAME, con.DOCTOR_PHONE) != '' THEN CONVERT(VARCHAR(MAX),'Doctor') END AS DOCTOR_RELATION
              ,CONVERT(VARCHAR(MAX),con.DOCTOR_PHONE) AS DOCTOR_CELL
@@ -57,11 +61,13 @@ WITH contacts_unpivot AS (
              ,CONVERT(VARCHAR(MAX),con.RELEASE_5_NAME) AS RELEASE5_NAME
              ,CONVERT(VARCHAR(MAX),con.RELEASE_5_RELATION) AS RELEASE5_RELATION
              ,CONVERT(VARCHAR(MAX),con.RELEASE_5_PHONE) AS RELEASE5_CELL
-       FROM KIPP_NJ..PS$STUDENTS_contact#static con WITH(NOLOCK)
-       JOIN KIPP_NJ..PS$STUDENTS_BLObs#static blob WITH(NOLOCK)
-         ON con.STUDENTID = blob.STUDENTID
-       JOIN KIPP_NJ..PS$STUDENTS#static s WITH(NOLOCK)
-         ON con.STUDENTID = s.ID
+       FROM KIPP_NJ..PS$STUDENTS#static s WITH(NOLOCK)
+       LEFT OUTER JOIN KIPP_NJ..PS$STUDENTS_custom#static cs WITH(NOLOCK)
+         ON s.ID = cs.STUDENTID
+       LEFT OUTER JOIN KIPP_NJ..PS$STUDENTS_contact#static con WITH(NOLOCK)
+         ON s.ID = con.STUDENTID
+       LEFT OUTER JOIN KIPP_NJ..PS$STUDENTS_BLObs#static blob WITH(NOLOCK)
+         ON s.ID = blob.STUDENTID
       ) sub
   UNPIVOT(
     value
@@ -73,11 +79,13 @@ WITH contacts_unpivot AS (
                  ,PARENT1_HOME
                  ,PARENT1_CELL
                  ,PARENT1_DAY
+                 ,PARENT1_REGISTEREDTOVOTE
                  ,PARENT2_NAME
                  ,PARENT2_RELATION
                  ,PARENT2_HOME
                  ,PARENT2_CELL
                  ,PARENT2_DAY
+                 ,PARENT2_REGISTEREDTOVOTE
                  ,DOCTOR_NAME
                  ,DOCTOR_RELATION
                  ,DOCTOR_CELL
@@ -115,7 +123,16 @@ WITH contacts_unpivot AS (
  )
 
 ,contacts_repivot AS (
-  SELECT *
+  SELECT STUDENTID
+        ,FAMILY_IDENT
+        ,person
+        ,NAME
+        ,RELATION
+        ,CELL
+        ,HOME
+        ,DAY
+        ,EMAIL
+        ,REGISTEREDTOVOTE
   FROM contacts_unpivot
   PIVOT(
     MAX(value)
@@ -124,8 +141,25 @@ WITH contacts_unpivot AS (
                 ,[CELL]
                 ,[HOME]              
                 ,[DAY]
-                ,[EMAIL])
-   ) p
+                ,[EMAIL]
+                ,[REGISTEREDTOVOTE])
+   ) p      
+ )
+
+,contacts_grouped AS (
+  SELECT FAMILY_IDENT
+        ,person
+        ,NAME
+        ,KIPP_NJ.dbo.GROUP_CONCAT_D(DISTINCT CELL, CHAR(10)) AS cell
+        ,KIPP_NJ.dbo.GROUP_CONCAT_D(DISTINCT HOME, CHAR(10)) AS home
+        ,KIPP_NJ.dbo.GROUP_CONCAT_D(DISTINCT DAY, CHAR(10)) AS day
+        ,KIPP_NJ.dbo.GROUP_CONCAT_D(DISTINCT EMAIL, CHAR(10)) AS email
+        ,MAX(REGISTEREDTOVOTE) AS REGISTEREDTOVOTE
+  FROM contacts_repivot
+  WHERE FAMILY_IDENT IS NOT NULL
+  GROUP BY FAMILY_IDENT
+          ,person
+          ,NAME
  )
 
 SELECT co.STUDENT_NUMBER
@@ -136,15 +170,20 @@ SELECT co.STUDENT_NUMBER
       ,co.ENROLL_STATUS
       
       ,s.FAMILY_IDENT
-      ,cs.INFOSNAP_OPT_IN
+      ,CASE 
+        WHEN s.FAMILY_IDENT IS NOT NULL THEN MAX(cs.INFOSNAP_OPT_IN) OVER(PARTITION BY s.FAMILY_IDENT)
+        ELSE cs.INFOSNAP_OPT_IN
+       END AS INFOSNAP_OPT_IN      
 
       ,c.person AS contact_type
       ,c.NAME AS contact_name
       ,ISNULL(c.RELATION, c.person) AS contact_relation
-      ,c.CELL AS contact_cell_phone
-      ,c.HOME AS contact_home_phone
-      ,c.DAY AS contact_day_phone
-      ,c.EMAIL AS contact_email      
+      
+      ,cg.CELL AS contact_cell_phone
+      ,cg.HOME AS contact_home_phone
+      ,cg.DAY AS contact_day_phone
+      ,cg.EMAIL AS contact_email      
+      ,cg.REGISTEREDTOVOTE AS contact_registered_to_vote
 FROM KIPP_NJ..COHORT$identifiers_long#static co WITH(NOLOCK)
 LEFT OUTER JOIN KIPP_NJ..PS$STUDENTS#static s WITH(NOLOCK)
   ON co.studentid = s.ID
@@ -152,5 +191,9 @@ LEFT OUTER JOIN KIPP_NJ..PS$STUDENTS_custom#static cs WITH(NOLOCK)
   ON co.studentid = cs.STUDENTID
 LEFT OUTER JOIN contacts_repivot c
   ON co.studentid = c.STUDENTID
+LEFT OUTER JOIN contacts_grouped cg
+  ON s.FAMILY_IDENT = cg.FAMILY_IDENT
+ AND c.person = cg.person
+ AND c.NAME = cg.NAME
 WHERE co.year = KIPP_NJ.dbo.fn_Global_Academic_Year()
   AND co.rn = 1
