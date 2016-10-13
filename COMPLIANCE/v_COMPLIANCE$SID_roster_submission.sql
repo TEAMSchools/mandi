0bae0d5a968cfd63b1cf27698e23553108d62ec6
Charlie <cbini87@gmail.com>
2016-10-13 08:51:50 -0400

    updates to SID

--- COMPLIANCE/v_COMPLIANCE$SID_roster_submission.sql
+++ COMPLIANCE/v_COMPLIANCE$SID_roster_submission.sql
@@ -6,6 +6,7 @@ ALTER VIEW COMPLIANCE$SID_roster_submission AS
 WITH attendance AS (
   SELECT STUDENTID
         ,academic_year
+        ,MIN(CASE WHEN ATTENDANCEVALUE = 1 THEN CONVERT(DATE,CALENDARDATE) END) AS min_attendance_date
         ,SUM(CONVERT(INT,MEMBERSHIPVALUE)) AS N_membership
         ,SUM(CONVERT(INT,ATTENDANCEVALUE)) AS N_present
         ,CASE
@@ -13,125 +14,174 @@ WITH attendance AS (
           ELSE SUM(CONVERT(INT,MEMBERSHIPVALUE)) - SUM(CONVERT(INT,ATTENDANCEVALUE)) - 10 
          END AS N_daystruant
   FROM KIPP_NJ..ATT_MEM$MEMBERSHIP WITH(NOLOCK)
-  WHERE MEMBERSHIPVALUE = 1
-    AND academic_year = (KIPP_NJ.dbo.fn_Global_Academic_Year() - 1)
+  WHERE MEMBERSHIPVALUE = 1    
   GROUP BY STUDENTID
           ,academic_year
  )
 
-SELECT s.student_number AS LocalIdentificationNumber
-      ,s.STATE_STUDENTNUMBER AS StateIdentificationNumber
-      ,s.first_name AS FirstName
-      ,cs.MIDDLE_NAME_CUSTOM AS MiddleName /* use custom field */
-      ,s.last_name AS LastName
-      ,NULL AS GenerationCodeSuffix
-      ,s.Gender
-      ,CASE
-        WHEN s.DOB = '1900-01-01' THEN NULL
-        ELSE REPLACE(CONVERT(DATE,s.dob),'-','') 
-       END AS DateOfBirth
-      ,cs.NJ_CityofBirth AS CityOfBirth      
-      ,CASE
-        WHEN cs.NJ_STATEOFBIRTH IS NULL THEN NULL
-        WHEN CHARINDEX(' ',cs.NJ_StateOfBirth) = 0 THEN UPPER(LEFT(cs.NJ_StateOfBirth,2))
-        ELSE CONCAT(LEFT(cs.NJ_StateOfBirth,1) , SUBSTRING(cs.NJ_StateOfBirth, CHARINDEX(' ',cs.NJ_StateOfBirth) + 1, 1))
-       END AS StateOfBirth
-      ,CASE WHEN cs.NJ_CountryOfBirth = 'UNITED STATES' THEN '2330' END AS CountryOfBirth
-      ,CASE WHEN s.ethnicity = 'H' THEN 'Y' ELSE 'N' END AS Ethnicity
-      ,CASE WHEN s.ethnicity = 'I' THEN 'Y' ELSE 'N' END AS RaceAmericanIndian
-      ,CASE WHEN s.ethnicity = 'A' THEN 'Y' ELSE 'N' END AS RaceAsian
-      ,CASE WHEN s.ethnicity = 'B' THEN 'Y' ELSE 'N' END AS RaceBlack
-      ,CASE WHEN s.ethnicity = 'P' THEN 'Y' ELSE 'N' END AS RacePacific
-      ,CASE WHEN s.ethnicity = 'W' THEN 'Y' ELSE 'N' END AS RaceWhite
-      ,CASE WHEN s.enroll_status = 0 THEN 'A' ELSE 'I' END AS Status /* we need to know which students are active in NJSMART but inactive on PS */
+SELECT co.student_number AS LocalIdentificationNumber
+      ,co.SID AS StateIdentificationNumber
+      ,ISNULL(nj.FirstName,co.first_name) AS FirstName
+      ,ISNULL(nj.MiddleName,cs.MIDDLE_NAME_CUSTOM) AS MiddleName /* use custom field */
+      ,ISNULL(nj.LastName,co.last_name) AS LastName
+      ,nj.GenerationCodeSuffix
+      ,co.Gender
+      ,REPLACE(CONVERT(DATE,co.dob),'-','') AS DateOfBirth
+      ,CASE WHEN nj.stateidentificationnumber IS NULL THEN cs.NJ_CITYOFBIRTH ELSE nj.CityOfBirth END AS CityOfBirth
+      ,CASE WHEN nj.stateidentificationnumber IS NULL THEN cs.NJ_STATEOFBIRTH ELSE nj.StateOfBirth END AS StateOfBirth
+      ,CASE 
+        WHEN nj.stateidentificationnumber IS NULL AND cs.NJ_COUNTRYOFBIRTH = 'UNITED STATES' THEN 2230
+        ELSE nj.CountryOfBirth 
+       END AS CountryOfBirth
+      ,CASE 
+        WHEN nj.stateidentificationnumber IS NOT NULL THEN nj.ethnicity
+        WHEN co.ethnicity = 'H' THEN 'Y' 
+        ELSE 'N' 
+       END AS Ethnicity
+      ,CASE 
+        WHEN nj.stateidentificationnumber IS NOT NULL THEN nj.raceamericanindian
+        WHEN co.ethnicity = 'I' THEN 'Y' 
+        ELSE 'N' 
+       END AS RaceAmericanIndian
+      ,CASE 
+        WHEN nj.stateidentificationnumber IS NOT NULL THEN nj.raceasian
+        WHEN co.ethnicity = 'A' THEN 'Y' 
+        ELSE 'N' 
+       END AS RaceAsian
+      ,CASE 
+        WHEN nj.stateidentificationnumber IS NOT NULL THEN nj.raceblack
+        WHEN co.ethnicity = 'B' THEN 'Y' 
+        ELSE 'N' 
+       END AS RaceBlack
+      ,CASE 
+        WHEN nj.stateidentificationnumber IS NOT NULL THEN nj.racepacific
+        WHEN co.ethnicity = 'P' THEN 'Y' 
+        ELSE 'N' 
+       END AS RacePacific
+      ,CASE 
+        WHEN nj.stateidentificationnumber IS NOT NULL THEN nj.racewhite
+        WHEN co.ethnicity = 'W' THEN 'Y' 
+        ELSE 'N' 
+       END AS RaceWhite
+      ,'A' AS Status /* we need to know which students are active in NJSMART but inactive on PS */
       ,'F' AS EnrollmentType
       
-      /* home district */
-      ,s.CITY AS CITY_NOIMPORT
-      ,CASE WHEN s.CITY = 'Newark' THEN '13' WHEN s.CITY = 'Camden' THEN '07' END AS CountyCodeResident
-      ,CASE WHEN s.CITY = 'Newark' THEN '3570' WHEN s.CITY = 'Camden' THEN '0680' END AS DistrictCodeResident
-      ,NULL AS SchoolCodeResident /* 050 for Newark, ? for Camden */
+      /* home district */      
+      ,CASE WHEN nj.stateidentificationnumber IS NULL THEN NULL ELSE RIGHT(CONCAT('0',nj.CountyCodeResident),2) END AS CountyCodeResident
+      ,CASE WHEN nj.stateidentificationnumber IS NULL THEN NULL ELSE RIGHT(CONCAT('0',nj.DistrictCodeResident),4) END AS DistrictCodeResident
+      ,CASE WHEN nj.stateidentificationnumber IS NULL THEN NULL ELSE RIGHT(CONCAT('0',nj.SchoolCodeResident),3) END AS SchoolCodeResident
+            
+      /* 1st attendance date for new students */
+      ,CASE WHEN nj.stateidentificationnumber IS NULL THEN REPLACE(att.min_attendance_date,'-','') ELSE nj.DistrictEntryDate END AS DistrictEntryDate
       
       /* always us */
-      ,CASE
-        WHEN s.DISTRICTENTRYDATE = '1900-01-01' THEN NULL
-        ELSE REPLACE(CONVERT(DATE,s.DistrictEntryDate),'-','') 
-       END AS DistrictEntryDate /* compare PS vs NJSMART and choose one */
-      ,CASE WHEN s.schoolid LIKE '1799%' THEN '07' ELSE '80' END AS CountyCodeReceiving      
-      ,CASE WHEN s.schoolid LIKE '1799%' THEN '1799' ELSE '7325' END AS DistrictCodeReceiving
-      ,CASE WHEN s.schoolid LIKE '1799%' THEN '111' ELSE '965' END AS SchoolCodeReceiving
+      ,CASE 
+        WHEN nj.stateidentificationnumber IS NOT NULL THEN RIGHT(CONCAT('0',nj.CountyCodeReceiving),2) 
+        WHEN co.schoolid LIKE '1799%' THEN '07'
+        ELSE '80'
+       END AS CountyCodeReceiving
+      ,CASE 
+        WHEN nj.stateidentificationnumber IS NOT NULL THEN RIGHT(CONCAT('0',nj.DistrictCodeReceiving),4) 
+        WHEN co.schoolid LIKE '1799%' THEN '1799'
+        ELSE '7325'
+       END AS DistrictCodeReceiving
+      ,CASE 
+        WHEN nj.stateidentificationnumber IS NOT NULL THEN RIGHT(CONCAT('0',nj.SchoolCodeReceiving),3) 
+        WHEN co.schoolid LIKE '1799%' THEN '111'
+        ELSE '965'
+       END AS SchoolCodeReceiving
       
       /* manual updates needed for out of district placements -- ASK KERRY, FIND BETTER SYSTEM OF TRACKING */
-      ,CASE WHEN s.schoolid LIKE '1799%' THEN '07' ELSE '80' END AS CountyCodeAttending      
-      ,CASE WHEN s.schoolid LIKE '1799%' THEN '1799' ELSE '7325' END AS DistrictCodeAttending
-      ,CASE WHEN s.schoolid LIKE '1799%' THEN '111' ELSE '965' END AS SchoolCodeAttending
+      ,CASE 
+        WHEN nj.stateidentificationnumber IS NOT NULL THEN RIGHT(CONCAT('0',nj.CountyCodeAttending),2) 
+        WHEN co.schoolid LIKE '1799%' THEN '07'
+        ELSE '80'
+       END AS CountyCodeAttending
+      ,CASE 
+        WHEN nj.stateidentificationnumber IS NOT NULL THEN RIGHT(CONCAT('0',nj.DistrictCodeAttending),4) 
+        WHEN co.schoolid LIKE '1799%' THEN '1799'
+        ELSE '7325'
+       END AS DistrictCodeAttending
+      ,CASE 
+        WHEN nj.stateidentificationnumber IS NOT NULL THEN RIGHT(CONCAT('0',nj.SchoolCodeAttending),3) 
+        WHEN co.schoolid LIKE '1799%' THEN '111'
+        ELSE '965'
+       END AS SchoolCodeAttending
       
       ,CASE 
-        WHEN s.GRADE_LEVEL = 99 THEN NULL
-        ELSE (KIPP_NJ.dbo.fn_Global_Academic_Year() + 1) + (12 - s.grade_level) 
+        WHEN co.highest_achieved = 99 THEN co.cohort
+        ELSE (KIPP_NJ.dbo.fn_Global_Academic_Year() + 1) + (12 - co.grade_level) 
        END AS YearOfGraduation
-      ,CASE
-        WHEN s.DISTRICTENTRYDATE = '1900-01-01' THEN NULL
-        ELSE REPLACE(CONVERT(DATE,s.DistrictEntryDate),'-','') 
-       END AS SchoolEntryDate
-      ,CASE WHEN s.enroll_status != 0 THEN REPLACE(CONVERT(DATE,s.exitdate),'-','') END AS SchoolExitDate
-      ,CASE WHEN s.enroll_status != 0 THEN s.exitcode END AS SchoolExitWithdrawalCode /* only for inactive students -- watch out for T2's */
+      ,CASE WHEN nj.stateidentificationnumber IS NULL THEN REPLACE(att.min_attendance_date,'-','') ELSE nj.SchoolEntryDate END AS SchoolEntryDate
+      ,nj.schoolexitdate AS SchoolExitDate
+      ,nj.SchoolExitWithdrawalCode /* only for inactive students -- watch out for T2's */
       
       /* SID = inactive; State Submission = active */
-      ,CASE WHEN s.enroll_status != 0 THEN att.N_membership END AS CumulativeDaysInMembership
-      ,CASE WHEN s.enroll_status != 0 THEN att.N_present END AS CumulativeDaysPresent
-      ,CASE WHEN s.enroll_status != 0 THEN att.N_daystruant END AS CumulativeDaysTowardsTruancy      
+      ,nj.CumulativeDaysInMembership
+      ,nj.CumulativeDaysPresent
+      ,nj.CumulativeDaysTowardsTruancy      
       
-      ,NULL AS Tuitioncode
-      ,CASE WHEN s.lunchstatus IN ('F','R') THEN s.lunchstatus ELSE 'N' END AS FreeAndReducedRateLunchStatus      
+      ,nj.Tuitioncode
+      ,CASE WHEN co.lunchstatus IN ('F','R') THEN co.lunchstatus ELSE 'N' END AS FreeAndReducedRateLunchStatus      
       ,CASE 
-        WHEN s.GRADE_LEVEL = 99 THEN NULL
-        WHEN s.GRADE_LEVEL = 0 THEN 'KF' 
-        ELSE RIGHT(CONCAT('0',CONVERT(VARCHAR,s.GRADE_LEVEL)),2) 
+        WHEN co.GRADE_LEVEL = 0 THEN 'KF' 
+        ELSE RIGHT(CONCAT('0',CONVERT(VARCHAR,co.GRADE_LEVEL)),2) 
        END AS GradeLevel
-      ,CASE 
-        WHEN s.GRADE_LEVEL = 99 THEN NULL
-        WHEN s.GRADE_LEVEL = 0 THEN 'KF' 
-        ELSE RIGHT(CONCAT('0',CONVERT(VARCHAR,s.GRADE_LEVEL)),2) 
+      ,CASE         
+        WHEN co.GRADE_LEVEL = 0 THEN 'KF' 
+        ELSE RIGHT(CONCAT('0',CONVERT(VARCHAR,co.GRADE_LEVEL)),2) 
        END AS ProgramTypeCode /* do we have the special ed classes? SPED code? */
       ,CASE WHEN co.retained_yr_flag = 1 THEN 'Y' ELSE 'N' END AS Retained
       ,CASE
-        WHEN iep.SPECIAL_EDUCATION = 'AI' THEN '01'
-        WHEN iep.SPECIAL_EDUCATION = 'AUT' THEN '02'
-        WHEN iep.SPECIAL_EDUCATION = 'CMI' THEN '03'
-        WHEN iep.SPECIAL_EDUCATION = 'CMO' THEN '04'
-        WHEN iep.SPECIAL_EDUCATION = 'CSE' THEN '05'
-        WHEN iep.SPECIAL_EDUCATION = 'CI' THEN '06'
-        WHEN iep.SPECIAL_EDUCATION = 'ED' THEN '07'
-        WHEN iep.SPECIAL_EDUCATION = 'MD' THEN '08'
-        WHEN iep.SPECIAL_EDUCATION = 'DB' THEN '09'
-        WHEN iep.SPECIAL_EDUCATION = 'OI' THEN '10'
-        WHEN iep.SPECIAL_EDUCATION = 'OHI' THEN '11'
-        WHEN iep.SPECIAL_EDUCATION = 'PSD' THEN '12'
-        WHEN iep.SPECIAL_EDUCATION = 'SM' THEN '13' /* no longer valid */
-        WHEN iep.SPECIAL_EDUCATION = 'SLD' THEN '14'
-        WHEN iep.SPECIAL_EDUCATION = 'TBI' THEN '15'
-        WHEN iep.SPECIAL_EDUCATION = 'VI' THEN '16'
-        WHEN iep.SPECIAL_EDUCATION = 'ESLS' THEN '17'
-        ELSE iep.SPECIAL_EDUCATION
+        WHEN co.SPED_code IS NULL THEN NULL
+        WHEN co.sped_code = 'AI' THEN '01'
+        WHEN co.sped_code = 'AUT' THEN '02'
+        WHEN co.sped_code = 'CMI' THEN '03'
+        WHEN co.sped_code = 'CMO' THEN '04'
+        WHEN co.sped_code = 'CSE' THEN '05'
+        WHEN co.sped_code = 'CI' THEN '06'
+        WHEN co.sped_code = 'ED' THEN '07'
+        WHEN co.sped_code = 'MD' THEN '08'
+        WHEN co.sped_code = 'DB' THEN '09'
+        WHEN co.sped_code = 'OI' THEN '10'
+        WHEN co.sped_code = 'OHI' THEN '11'
+        WHEN co.sped_code = 'PSD' THEN '12'
+        WHEN co.sped_code = 'SM' THEN '13' /* no longer valid */
+        WHEN co.sped_code = 'SLD' THEN '14'
+        WHEN co.sped_code = 'TBI' THEN '15'
+        WHEN co.sped_code = 'VI' THEN '16'
+        WHEN co.sped_code = 'ESLS' THEN '17'
+        ELSE RIGHT(CONCAT('0',co.sped_code),2)
        END AS SpecialEducationClassification /* audit 00, 99, 12 (PSD) students */
-      ,CASE WHEN cs.LEP_STATUS IS NOT NULL THEN '!' END AS LEPProgramStartDate /* hand entry -- SPED follow up */
-      ,CASE WHEN cs.LEP_STATUS IS NOT NULL THEN '!' END AS LEPProgramCompletionDate /* hand entry -- SPED follow up */
-      ,NULL AS NonPublic /* out-of-district placements? SPED follow up */
-      ,CASE 
-        WHEN s.city = 'Newark' THEN '0714'
-        WHEN s.city = 'Camden' THEN '0408'
+      ,CASE
+        WHEN co.LEP_STATUS IS NOT NULL AND nj.LEPProgramStartDate IS NULL THEN '!'
+        ELSE CONVERT(VARCHAR,nj.LEPProgramStartDate)
+       END AS LEPProgramStartDate /* hand entry -- SPED follow up */
+      ,CASE
+        WHEN co.LEP_STATUS IS NOT NULL AND nj.LEPProgramCompletionDate IS NULL THEN '!'
+        ELSE CONVERT(VARCHAR,nj.LEPProgramCompletionDate)
+       END AS LEPProgramCompletionDate  /* hand entry -- SPED follow up */
+      ,nj.NonPublic /* out-of-district placementa -- SPED follow up */
+      ,CASE       
+        WHEN nj.stateidentificationnumber IS NULL AND co.CITY = 'Newark' THEN '0714'
+        WHEN nj.stateidentificationnumber IS NULL AND co.CITY = 'Camden' THEN '0408'
+        WHEN nj.stateidentificationnumber IS NULL THEN NULL
+        ELSE RIGHT(CONCAT('0',nj.ResidentMunicipalCode),4) 
        END AS ResidentMunicipalCode
-      ,4 AS MilitaryConnectedStudentIndicator
-      ,s.SCHOOLID
-      ,CASE WHEN s.schoolid LIKE '1799%' THEN 'Camden' ELSE 'Newark' END AS region
-FROM KIPP_NJ..PS$STUDENTS#static s WITH(NOLOCK)  
+      ,COALESCE(nj.MilitaryConnectedStudentIndicator, 4) AS MilitaryConnectedStudentIndicator
+      ,CASE WHEN co.schoolid LIKE '1799%' THEN 'Camden' ELSE 'Newark' END AS region
+      ,co.schoolid
+      ,co.school_name      
+      ,co.CITY AS PS_CITY_NOIMPORT
+FROM KIPP_NJ..COHORT$identifiers_long#static co WITH(NOLOCK)  
+LEFT OUTER JOIN KIPP_NJ..PS$STUDENTS#static s WITH(NOLOCK)  
+  ON co.studentid = s.ID
 LEFT OUTER JOIN KIPP_NJ..PS$STUDENTS_custom#static cs WITH(NOLOCK)
-  ON s.id = cs.STUDENTID
-LEFT OUTER JOIN KIPP_NJ..PS$IEP_details#static iep
-  ON s.id = iep.STUDENTID
-LEFT OUTER JOIN KIPP_NJ..COHORT$identifiers_long#static co WITH(NOLOCK)
-  ON s.ID = co.studentid
- AND co.all_years_rn = 1
+  ON co.studentid = cs.STUDENTID
 LEFT OUTER JOIN attendance att
-  ON s.id = att.STUDENTID
+  ON co.studentid = att.STUDENTID
+ AND co.year = att.academic_year
+LEFT OUTER JOIN KIPP_NJ..AUTOLOAD$GDOCS_NJSMART_sid_export nj
+  ON co.SID = nj.stateidentificationnumber 
+ AND co.student_number = nj.localidentificationnumber
+WHERE CONVERT(DATE,GETDATE()) BETWEEN co.entrydate AND co.exitdate
