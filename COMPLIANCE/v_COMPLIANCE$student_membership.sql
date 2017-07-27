0bd2f64af911787e3b260082128188eae4b05bd2
Charlie <cbini87@gmail.com>
2017-07-27 16:32:52 -0400

    removed saturday logic

--- COMPLIANCE/v_COMPLIANCE$student_membership.sql
+++ COMPLIANCE/v_COMPLIANCE$student_membership.sql
@@ -3,36 +3,6 @@ GO
 
 ALTER VIEW COMPLIANCE$student_membership AS
 
-WITH saturdays AS (
-  SELECT STUDENTID      
-        ,academic_year
-        ,ATTENDANCEVALUE
-        ,MEMBERSHIPVALUE
-        ,ROW_NUMBER() OVER(
-          PARTITION BY studentid, DATEPART(MONTH, mem.CALENDARDATE)
-            ORDER BY calendardate DESC) AS rn
-  FROM KIPP_NJ..ATT_MEM$MEMBERSHIP mem WITH(NOLOCK)
-  WHERE DATEPART(DW, mem.CALENDARDATE) = 7 
- )
-
-,attmem AS (
-  SELECT STUDENTID      
-        ,academic_year
-        ,ATTENDANCEVALUE
-        ,MEMBERSHIPVALUE        
-  FROM KIPP_NJ..ATT_MEM$MEMBERSHIP mem WITH(NOLOCK)
-  WHERE DATEPART(DW, mem.CALENDARDATE) != 7
-
-  UNION ALL
-
-  SELECT STUDENTID
-        ,academic_year
-        ,ATTENDANCEVALUE
-        ,MEMBERSHIPVALUE
-  FROM saturdays
-  WHERE rn = 1
- )
-
 SELECT co.student_number      
       ,co.year AS academic_year
       ,co.school_name
@@ -41,7 +11,7 @@ SELECT co.student_number
       ,SUM(CONVERT(INT,attmem.MEMBERSHIPVALUE)) AS mem
       ,SUM(CONVERT(INT,attmem.MEMBERSHIPVALUE)) - SUM(CONVERT(INT,attmem.ATTENDANCEVALUE)) AS absences
 FROM KIPP_NJ..COHORT$identifiers_long#static co WITH(NOLOCK)
-JOIN attmem
+JOIN KIPP_NJ..ATT_MEM$MEMBERSHIP attmem WITH(NOLOCK)
   ON co.studentid = attmem.STUDENTID
  AND co.year = attmem.academic_year
 WHERE co.schoolid != 999999
